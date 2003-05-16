9f3297bb74c2e3ec681ab03ed77e3da148e5c4ca
Andrey Urusov <drobus@gmail.com>
2003-05-16 13:58:01 +0000

    cry out added to toolbar
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1334 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- trunk/resource.h
+++ trunk/resource.h
@@ -126,7 +126,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        194
-#define _APS_NEXT_COMMAND_VALUE         32825
+#define _APS_NEXT_COMMAND_VALUE         32826
 #define _APS_NEXT_CONTROL_VALUE         1053
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
