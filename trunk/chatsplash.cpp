94a66148b0dc17d1c19fa704d33bc0e878990bc0
Andrey Urusov <drobus@gmail.com>
2003-06-18 10:38:39 +0000

    redraw progressbar fixed
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1368 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- trunk/chatsplash.cpp
+++ trunk/chatsplash.cpp
@@ -68,7 +68,7 @@ int CChatSplash::OnCreate(LPCREATESTRUCT lpCreateStruct)
 
 	CenterWindow();
 
-	progress.Create( WS_CHILD | PBS_SMOOTH | WS_VISIBLE, CRect( 0, 0, 0, 0 ), this, 1 );
+	progress.Create( WS_CHILD | WS_VISIBLE, CRect( 0, 0, 0, 0 ), this, 1 );
 
 	return 0;
 }
@@ -117,20 +117,50 @@ void CChatSplash::OnPaint()
 	CRect clientRect;
 	GetClientRect( clientRect );
 	COLORREF bgcolor = ::GetSysColor( COLOR_BTNFACE );
-	dc.FillSolidRect( clientRect, bgcolor );
 
 	CRect textRect( 0, 0, 10, 1 );
+	CRect progressRect( clientRect.left + 4, textRect.bottom + 15, clientRect.right - 4, textRect.bottom + 30 );
+
+	if ( !initInfo.empty() ) {
+		CFont* prev_font = dc.SelectObject( &font );
+		dc.DrawText( format( IDS_SPLASH_INITINFO, initInfo.c_str() ).c_str(), -1, textRect, DT_SINGLELINE | DT_LEFT | DT_TOP | DT_CALCRECT );
+		dc.SelectObject( prev_font );
+		textRect.OffsetRect( 10, 10 );
+		progressRect.top    = textRect.bottom + 15;
+		progressRect.bottom = textRect.bottom + 30;
+	}
+
+	if ( useProgress ) {
+		CBrush brush( bgcolor );
+		CPen pen( PS_SOLID, 0, bgcolor );
+		CPen* prev_pen     = dc.SelectObject( &pen );
+		CBrush* prev_brush = dc.SelectObject( &brush );
+		CRect rect( clientRect.left, clientRect.top, clientRect.right, progressRect.top );
+		dc.Rectangle( rect );
+		rect.top    = progressRect.bottom;
+		rect.bottom = clientRect.bottom;
+		dc.Rectangle( rect );
+		rect.right  = progressRect.left;
+		rect.top    = progressRect.top;
+		dc.Rectangle( rect );
+		rect.left   = progressRect.right;
+		rect.right  = clientRect.right;
+		dc.Rectangle( rect );
+		dc.SelectObject( prev_pen );
+		dc.SelectObject( prev_brush );
+	} else {
+		dc.FillSolidRect( clientRect, bgcolor );
+	}
+
 	if ( !initInfo.empty() ) {
 		CFont* prev_font = dc.SelectObject( &font );
 		dc.SetBkMode( TRANSPARENT );
 		dc.SetTextColor( ::GetSysColor( COLOR_WINDOWTEXT ) );
-		std::string info = format( IDS_SPLASH_INITINFO, initInfo.c_str() );
-		dc.DrawText( info.c_str(), -1, textRect, DT_SINGLELINE | DT_LEFT | DT_TOP | DT_CALCRECT );
-		textRect.OffsetRect( 10, 10 );
-		dc.DrawText( info.c_str(), -1, textRect, DT_SINGLELINE | DT_LEFT | DT_TOP );
+		dc.DrawText( format( IDS_SPLASH_INITINFO, initInfo.c_str() ).c_str(), -1, textRect, DT_SINGLELINE | DT_LEFT | DT_TOP );
 		dc.SelectObject( prev_font );
 	}
+
 	if ( useProgress ) {
-		progress.MoveWindow( clientRect.left + 4, textRect.bottom + 15, clientRect.Width() - 8, 20 );
+		progress.MoveWindow( progressRect );
 	}
 }
