8e30e89d878ecae6248f3bc5f50e04e0f38b9084
Andrey Urusov <drobus@gmail.com>
2003-10-15 10:28:10 +0000

    debug preprocessor lines added
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@413 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- trunk/rdo_ctrls/rdotabctrl.cpp
+++ trunk/rdo_ctrls/rdotabctrl.cpp
@@ -1,6 +1,12 @@
 #include "rdo_ctrls_pch.h"
 #include "rdotabctrl.h"
 
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
 // ----------------------------------------------------------------------------
 // ---------- RDOTab
 // ----------------------------------------------------------------------------
--- trunk/rdo_ctrls/rdotreectrl.cpp
+++ trunk/rdo_ctrls/rdotreectrl.cpp
@@ -3,6 +3,8 @@
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
 #endif
 
 // ----------------------------------------------------------------------------
