cc938696c299733c25fef1f09088da829ec9c47c
Andrey Urusov <drobus@gmail.com>
2006-05-04 15:24:31 +0000

    no message
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@527 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- trunk/rdo_lib/ParserLib/rdoparser_base.cpp
+++ trunk/rdo_lib/ParserLib/rdoparser_base.cpp
@@ -61,7 +61,7 @@ void RDOParserList::reset()
 	insertParser( rdoModelObjects::obPMD, new RDOParserRDO( rdoModelObjects::PMD, pmdparse, pmderror, pmdlex ) );
 	insertParser( rdoModelObjects::obFRM, new RDOParserRDO( rdoModelObjects::FRM, frmparse, frmerror, frmlex ) );
 	insertParser( rdoModelObjects::obSMR, new RDOParserRDO( rdoModelObjects::SMR, smr2parse, smr2error, smr2lex ) );
-	insertParser( rdoModelObjects::obRSS, new RDOParserRSSPost() );
+	insertParser( rdoModelObjects::obPOST, new RDOParserRSSPost() );
 }
 
 int RDOParserList::insertParser( rdoModelObjects::RDOParseType type, RDOParserBase* parser )
