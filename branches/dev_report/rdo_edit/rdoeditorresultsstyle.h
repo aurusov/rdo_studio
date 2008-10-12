98ed0a2c526274a8442f4a2e2dde002c967d1014
Sergey Klochkov <serezha.klochkov@gmail.com>
2008-10-12 22:12:50 +0000

    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@924 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ branches/dev_report/rdo_edit/rdoeditorresultsstyle.h
@@ -0,0 +1,21 @@
+#ifndef RDOEDITORRESULTSSTYLE_H
+#define RDOEDITORRESULTSSTYLE_H
+#pragma once
+
+#include "rdoeditorbaseeditstyle.h"
+
+namespace rdoEditor {
+
+// ----------------------------------------------------------------------------
+// ---------- RDOEditorResultsStyle
+// ----------------------------------------------------------------------------
+class RDOEditorResultsStyle: public RDOEditorBaseEditStyle
+{
+public:
+	RDOEditorResultsStyle();
+	virtual ~RDOEditorResultsStyle();
+};
+
+}; // namespace rdoEditor
+
+#endif // RDOEDITORRESULTSSTYLE_H
