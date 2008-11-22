54e54269ace03aedbe74a3d1ebbead64733f99df
Andrey Urusov <drobus@gmail.com>
2008-11-22 19:36:00 +0000

    * пофиксен вывод лишней трассировки * форматинг RDORule * упрощен пример
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@949 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- branches/dev_rule/rdo_lib/rdo_runtime/rdo_rule.cpp
+++ branches/dev_rule/rdo_lib/rdo_runtime/rdo_rule.cpp
@@ -13,31 +13,34 @@ namespace rdoRuntime {
 // ----------------------------------------------------------------------------
 // ---------- RDORule
 // ----------------------------------------------------------------------------
-RDORule::RDORule( RDORuntime* runtime, RDOPatternRule* pattern, bool trace, const std::string& name ):
-	RDOActivityPattern<RDOPatternRule>( runtime, pattern, trace, name ),
-	RDORuntimeContainer( runtime ),
-	additionalCondition( NULL )
+RDORule::RDORule( RDORuntime* runtime, RDOPatternRule* pattern, bool trace, const std::string& name )
+	: RDOActivityPattern<RDOPatternRule>( runtime, pattern, trace, name )
+	, RDORuntimeContainer  (runtime)
+	, m_additionalCondition(NULL   )
 {
-	setTraceID( getRuntime()->getFreeActivityId() );
-	haveAdditionalCondition = false;
+	init();
 }
 
-RDORule::RDORule( RDORuntime* runtime, RDOPatternRule* pattern, bool trace, RDOCalc* condition, const std::string& name ):
-	RDOActivityPattern<RDOPatternRule>( runtime, pattern, trace, name ),
-	RDORuntimeContainer( runtime ),
-	additionalCondition( condition )
+RDORule::RDORule( RDORuntime* runtime, RDOPatternRule* pattern, bool trace, RDOCalc* condition, const std::string& name )
+	: RDOActivityPattern<RDOPatternRule>( runtime, pattern, trace, name )
+	, RDORuntimeContainer  (runtime  )
+	, m_additionalCondition(condition)
+{
+	init();
+}
+
+void RDORule::init()
 {
 	setTraceID( getRuntime()->getFreeActivityId() );
-	haveAdditionalCondition = true;
+	m_traceOFF = false;
 }
 
 bool RDORule::choiceFrom( RDORuntime* runtime )
 { 
 	runtime->setCurrentActivity( this );
-	if ( haveAdditionalCondition ) {
-		if ( !additionalCondition->calcValue( runtime ).getAsBool() ) {
-			return false;
-		}
+	if ( m_additionalCondition && !m_additionalCondition->calcValue( runtime ).getAsBool() )
+	{
+		return false;
 	}
 	return m_pattern->choiceFrom( runtime ); 
 }
@@ -51,36 +54,41 @@ void RDORule::convertRule( RDORuntime* runtime )
 void RDORule::onAfterRule( RDOSimulator* sim, bool inSearch )
 {
 	updateConvertStatus( sim, m_pattern->m_convertorStatus );
-	if ( !inSearch ) {
-		getRuntime()->getTracer()->writeRule( this, getRuntime() );
+	if ( !inSearch )
+	{
+		trace();
 	}
 	m_pattern->convertErase( static_cast<RDORuntime*>(sim) );
 	updateRelRes( sim );
 }
 
+void RDORule::trace()
+{
+	if ( !m_traceOFF )
+	{
+		getRuntime()->getTracer()->writeRule( this, getRuntime() );
+	}
+}
+
 bool RDORule::onCheckCondition( RDOSimulator* sim )
-/*
-Дата внесения изменений: 02.11.08
-Автор: Лущан Дмитрий
-Смысл изменений: не запускать патерн (рул), если после его выполнения состояние системы не изменилось	
-*/
 {
 	onBeforeChoiceFrom( sim );
 	sim->inc_cnt_choice_from();
 	bool result = choiceFrom( static_cast<RDORuntime*>(sim) );
 	if ( result )
 	{
-// Сохраняем состояние системы в копию clone и в ней запускаем соответствующий рул
-		RDOSimulator* clone = sim->clone();
-		onBeforeRule( clone );
-		convertRule( static_cast<RDORuntime*>(clone) );
-		onAfterRule( clone );
-// Сравниваем состояние системы до запуска рула и после
-		if ( *clone == *sim )
+		m_traceOFF = true;
+		std::auto_ptr<RDOSimulator> clone( sim->clone() );
+		if (onDoOperation(clone.get()) != RDOBaseOperation::BOR_done)
+		{
+			//! Реакция на плохой onDoOperation - это вообще-то спортный вопрос
+			return false;
+		}
+		if ( *clone.get() == *sim )
 		{
 			result = false;
 		}
-		delete clone;
+		m_traceOFF = false;
 	}
 	return result;
 }
