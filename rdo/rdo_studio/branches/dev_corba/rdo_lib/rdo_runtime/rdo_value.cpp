ca704eac7d34a2e3e5fd6793386a04e051f6853e
Andrey Urusov <drobus@gmail.com>
2009-03-18 01:54:16 +0000

    * бранч для корбы
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1510 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_corba/rdo_lib/rdo_runtime/rdo_value.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "rdo_value.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+// ----------------------------------------------------------------------------
+// ---------- RDOValue
+// ----------------------------------------------------------------------------
