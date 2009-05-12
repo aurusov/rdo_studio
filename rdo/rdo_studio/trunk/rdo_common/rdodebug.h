5e0464c406051e90e709d98dd2f411a741037c13
Andrey Urusov <drobus@gmail.com>
2009-05-12 00:09:34 +0000

    * исправлена ошибка в TRACE'ах
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1603 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_common/rdodebug.h
+++ rdo/rdo_studio/trunk/rdo_common/rdodebug.h
@@ -6,10 +6,10 @@
 // ====================================================================== SYNOPSIS
 // ===============================================================================
 
-#define  TRACE(A)              ::OutputDebugString(#A);
-#define TRACE1(A, P1)          ::OutputDebugString(rdo::format(_T("%s"), #A, P1).c_str());
-#define TRACE2(A, P1, P2)      ::OutputDebugString(rdo::format(_T("%s"), #A, P1, P2).c_str());
-#define TRACE3(A, P1, P2, P3)  ::OutputDebugString(rdo::format(_T("%s"), #A, P1, P2, P3).c_str());
+#define  TRACE(A)              ::OutputDebugString(A);
+#define TRACE1(A, P1)          ::OutputDebugString(rdo::format(A, P1).c_str());
+#define TRACE2(A, P1, P2)      ::OutputDebugString(rdo::format(A, P1, P2).c_str());
+#define TRACE3(A, P1, P2, P3)  ::OutputDebugString(rdo::format(A, P1, P2, P3).c_str());
 
 #define ASSERT(A) \
 	if (!(A)) \
