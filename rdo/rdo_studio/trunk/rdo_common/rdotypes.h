7a20e56762a85337ba51aa4c30e1edb5bad3b193
Andrey Urusov <drobus@gmail.com>
2009-06-12 07:46:51 +0000

    * добавлен rbyte
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1705 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_common/rdotypes.h
+++ rdo/rdo_studio/trunk/rdo_common/rdotypes.h
@@ -14,5 +14,6 @@ typedef  signed int                 rsint;
 typedef  unsigned int               ruint;
 typedef  signed long long int       rsint64;
 typedef  unsigned long long int     ruint64;
+typedef  unsigned char              rbyte;
 
 #endif //! RDOTYPES_H
