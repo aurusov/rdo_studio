84342e47d0697856b21893a8c5f43c7cd18a08b7
Andrey Urusov <drobus@gmail.com>
2009-07-10 11:35:19 +0000

    * добавлен запрос интерфейса по интерфейсу * удален запрос интерфейса через шаблонные методы (быстрый сверху-вниз)
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1757 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_common/test/rdo_interface_test/main.cpp
+++ rdo/rdo_studio/trunk/rdo_common/test/rdo_interface_test/main.cpp
@@ -1,42 +1,297 @@
-#include "../../rdocommon.h"
-#include "../../rdointerface.h"
+// ====================================================================== INCLUDES
+#include <string>
 #include <iostream>
+// ====================================================================== SYNOPSIS
+#include "../../rdocommon.h"
+#include "../../namespace.h"
+#include "../../rdomacros.h"
+#include "../../rdotypes.h"
+// ===============================================================================
+
+OPEN_RDO_NAMESPACE
+template <class T, ruint id>
+class InterfaceRegistrator
+{
+public:
+	typedef T Type;
+	enum { ID = id };
+};
+CLOSE_RDO_NAMESPACE
+
+#define INTERFACE_REGISTRATOR(I, ID) \
+typedef rdo::InterfaceRegistrator<I, ID> Registered##I; \
+class ___InterfaceRegistrator___DoubleID___##ID {};
+
+#define IID(I) \
+Registered##I::ID
+
+OPEN_RDO_NAMESPACE
+
+class IUnknow;
+typedef PTR(IUnknow) LPIUnknow;
+
+class UnknowPointer
+{
+public:
+	UnknowPointer ();
+	UnknowPointer (PTR(void) aInterface, LPIUnknow smt_ptr);
+	UnknowPointer (CREF(UnknowPointer) unknowPointer);
+	~UnknowPointer();
+
+	operator rbool() const;
+
+	UnknowPointer queryInterface(ruint id);
+	ruint         getInterfaceID() const;
+
+protected:
+	PTR(void)  m_interface;
+
+private:
+	LPIUnknow  m_smt_ptr;
+};
+
+template<class T>
+class Interface: public UnknowPointer
+{
+public:
+	Interface(CREF(UnknowPointer) unknowPointer)
+		: UnknowPointer(unknowPointer)
+	{}
+	PTR(T) operator-> ()
+	{
+		return static_cast<PTR(T)>(m_interface);
+	}
+};
+
+class IUnknow
+{
+public:
+	virtual void          AddRef () = 0;
+	virtual void          Release() = 0;
+	virtual UnknowPointer queryInterface(ruint id) = 0;
+};
+typedef PTR(IUnknow) LPIUnknow;
+
+inline UnknowPointer::UnknowPointer()
+	: m_interface(NULL)
+	, m_smt_ptr  (NULL)
+{}
+
+inline UnknowPointer::UnknowPointer(PTR(void) aInterface, LPIUnknow smt_ptr)
+	: m_interface(aInterface)
+	, m_smt_ptr  (smt_ptr   )
+{
+	m_smt_ptr->AddRef();
+}
+
+inline UnknowPointer::UnknowPointer(CREF(UnknowPointer) unknowPointer)
+	: m_interface(unknowPointer.m_interface)
+	, m_smt_ptr  (unknowPointer.m_smt_ptr  )
+{
+	m_smt_ptr->AddRef();
+}
+
+inline UnknowPointer::~UnknowPointer()
+{
+	if (m_smt_ptr)
+		m_smt_ptr->Release();
+}
+
+inline UnknowPointer::operator rbool () const
+{
+	return m_interface != NULL;
+}
+
+inline UnknowPointer UnknowPointer::queryInterface(ruint id)
+{
+	return m_smt_ptr ? m_smt_ptr->queryInterface(id) : UnknowPointer();
+}
+
+inline ruint UnknowPointer::getInterfaceID() const
+{
+}
+
+template<class T>
+class smart_ptr: public IUnknow
+{
+public:
+	typedef T            object_type;
+	typedef smart_ptr<T> this_type;
+
+	smart_ptr(PTR(T) obj)
+		: m_object(obj)
+	{
+		allocateCounter();
+		counter() = 1;
+	}
+	smart_ptr(CREF(this_type) sptr)
+		: m_counter(sptr.m_counter)
+		, m_object (sptr.m_object )
+	{
+		AddRef();
+	}
+	~smart_ptr()
+	{
+		Release();
+		if (counter() == 0)
+			deallocateCounter();
+	}
+	PTR(T) get()
+	{
+		return m_object;
+	}
+	operator rbool () const
+	{
+		return get() != NULL;
+	}
+	PTR(T) operator-> ()
+	{
+		return get();
+	}
+
+private:
+	PTR(unsigned int) m_counter;
+	PTR(T)            m_object;
+
+	void allocateCounter()
+	{
+		m_counter = new unsigned int;
+	}
+	void deallocateCounter()
+	{
+		delete m_counter;
+	}
+	REF(unsigned int) counter()
+	{
+		return *m_counter;
+	}
+	virtual void AddRef()
+	{
+		counter()++;
+	}
+	virtual void Release()
+	{
+		counter()--;
+		if (counter() == 0)
+		{
+			if (m_object)
+			{
+				Factory<T>::destroy(m_object);
+				m_object = NULL;
+			}
+		}
+	}
+	virtual UnknowPointer queryInterface(ruint id)
+	{
+		if (get() != NULL)
+		{
+			PTR(void) pInterface = get()->queryInterface(id);
+			if (pInterface)
+				return UnknowPointer(pInterface, this);
+		}
+		return UnknowPointer();
+	}
+};
+
+template <class T>
+class Factory
+{
+public:
+	static smart_ptr<T> create()
+	{
+		return smart_ptr<T>(new T());
+	}
+	template <typename P1>
+	static smart_ptr<T> create(P1 p1)
+	{
+		return smart_ptr<T>(new T(p1));
+	}
+	static void destroy(PTR(T) obj)
+	{
+		delete obj;
+	}
+};
+
+CLOSE_RDO_NAMESPACE
+
+#define DEFINE_THIS_TYPE(A)    typedef A this_type;
+#define DEFINE_IPARENT_TYPE(P) typedef P iparent_type;
+#define DEFINE_FACTORY(A)      friend class rdo::Factory<A>;
+#define DEFINE_CLASS_NAME(A)   static std::string className() { return #A; }
+
+#define RDO_IOBJECT(A, BASE) \
+protected: \
+DEFINE_THIS_TYPE(A) \
+DEFINE_FACTORY(A) \
+DEFINE_IPARENT_TYPE(BASE) \
+public: \
+DEFINE_CLASS_NAME(A)
+
+#define QUERY_INTERFACE_BEGIN \
+public: \
+PTR(void) queryInterface(ruint id) \
+{ \
+	PTR(void) pIterface = iparent_type::queryInterface(id); \
+	if (pIterface) \
+		return pIterface; \
+ \
+	switch (id) \
+	{
+
+#define QUERY_INTERFACE(A)        case IID(A): return static_cast<PTR(A)>(this);
+#define QUERY_INTERFACE_END \
+	} \
+	return NULL; \
+}
+
+OPEN_RDO_NAMESPACE
+class IObjectBase
+{
+public:
+	PTR(void) queryInterface(ruint id)
+	{
+		return NULL;
+	}
+};
+CLOSE_RDO_NAMESPACE
 
 class IMy1
 {
 public:
-	enum {EIMy1};
 	virtual void fun1() = 0;
 };
 
 class IMy2
 {
 public:
-	enum {EIMy2};
 	virtual void fun2() = 0;
 };
 
 class IMy3
 {
 public:
-	enum {EIMy3};
 	virtual void fun3() = 0;
 };
 
 class IMy4
 {
 public:
-	enum {EIMy4};
 	virtual void fun4() = 0;
 };
+
+INTERFACE_REGISTRATOR(IMy1, 1);
+INTERFACE_REGISTRATOR(IMy2, 2);
+INTERFACE_REGISTRATOR(IMy3, 3);
+INTERFACE_REGISTRATOR(IMy4, 4);
 
 class MyClass: public rdo::IObjectBase, public IMy1, public IMy2
 {
 RDO_IOBJECT(MyClass, IObjectBase);
 
-public:
-	QUERY_INTERFACE(IMy1);
-	QUERY_INTERFACE(IMy2);
+QUERY_INTERFACE_BEGIN
+	QUERY_INTERFACE(IMy1)
+	QUERY_INTERFACE(IMy2)
+QUERY_INTERFACE_END
 
 protected:
 	MyClass(int i)
@@ -48,6 +303,7 @@ protected:
 	{
 		std::cout << "~MyClass(): " << m_i << std::endl;
 	}
+
 private:
 	int m_i;
 
@@ -69,22 +325,19 @@ class MyClass2: public MyClass, public IMy3
 {
 RDO_IOBJECT(MyClass2, MyClass);
 
-public:
-	QUERY_INTERFACE(IMy3);
-
-	rdo::LocalInterface<void> queryInterface(ruint id)
-	{
-		switch (id)
-		{
-		case 3: return rdo::LocalInterface<void>(static_cast<IMy3*>(this));
-		}
-		return NULL;
-	}
+QUERY_INTERFACE_BEGIN
+	QUERY_INTERFACE(IMy3)
+QUERY_INTERFACE_END
 
 private:
-	MyClass2(int i)
-		: MyClass(i)
-	{}
+	MyClass2(int i): MyClass(i)
+	{
+		std::cout << "MyClass2(int " << i << ");" << std::endl;
+	}
+	~MyClass2()
+	{
+		std::cout << "~MyClass2()" << std::endl;
+	}
 	void fun3()
 	{
 		std::cout << "void fun3()" << std::endl;
@@ -96,10 +349,13 @@ void main()
 	std::vector< rdo::LPIUnknow > list;
 	rdo::smart_ptr<MyClass2> smptr = rdo::Factory<MyClass2>::create(1);
 	list.push_back(&smptr);
-	rdo::Interface<IMy3> ptr = list.back()->queryInterface(3);
+	rdo::Interface<IMy3> ptr = list.back()->queryInterface(IID(IMy3));
+	if (ptr)
+	{
+		ptr->fun3();
+		rdo::Interface<IMy1> ptr2 = ptr.queryInterface(IID(IMy2));
+		if (ptr2)
+			ptr2->fun1();
+	}
 	int i = 1;
-
-//	rdo::Interface<IMy2> lpIMy2 = list.back().query<IMy2>();
-//	if (lpIMy2)
-//		lpIMy2->fun2();
 }
