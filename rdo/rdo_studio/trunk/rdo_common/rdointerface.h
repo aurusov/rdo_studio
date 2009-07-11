d9bfd8c97a75fab8e12cf69694028474f5c6f0b6
Andrey Urusov <drobus@gmail.com>
2009-07-11 01:14:39 +0000

    * первая версия интерфейса, с которым можно пробовать работать
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1758 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_common/rdointerface.h
+++ rdo/rdo_studio/trunk/rdo_common/rdointerface.h
@@ -19,75 +19,118 @@
 // ===============================================================================
 
 OPEN_RDO_NAMESPACE
+template <class T, ruint id>
+class InterfaceRegistrator
+{
+public:
+	typedef T Type;
+	enum { ID = id };
+};
+
+template <class T> class GetInterface {};
+CLOSE_RDO_NAMESPACE
+
+#define INTERFACE_REGISTRATOR(I, ID) \
+typedef rdo::InterfaceRegistrator<I, ID> Registered##I; \
+template <> class rdo::GetInterface<I>: public Registered##I {}; \
+class ___InterfaceRegistrator___DoubleID___##ID {};
+
+#define IID(I) \
+Registered##I::ID
+
+OPEN_RDO_NAMESPACE
 
 class IUnknow;
 typedef PTR(IUnknow) LPIUnknow;
 
-template<class T>
-class Interface
+template<class T> class Interface;
+
+class UnknowPointer
 {
 public:
-	Interface()
-		: m_interface(NULL)
-		, m_counter  (NULL)
-	{}
-	Interface(PTR(T) aInterface, LPIUnknow counter)
-		: m_interface(aInterface)
-		, m_counter  (counter   )
-	{
-		m_counter->AddRef();
-	}
-	~Interface()
-	{
-		if (m_counter)
-			m_counter->Release();
-	}
-	operator rbool () const
-	{
-		return m_interface != NULL;
-	}
-	PTR(T) operator-> ()
-	{
-		return m_interface;
-	}
+	UnknowPointer ();
+	UnknowPointer (PTR(void) aInterface, LPIUnknow smt_ptr);
+	UnknowPointer (CREF(UnknowPointer) unknowPointer);
+	~UnknowPointer();
+
+	operator rbool() const;
+
+	UnknowPointer                   queryInterface(ruint id);
+	template<class I> Interface<I>  queryInterface();
+
+protected:
+	PTR(void)  m_interface;
 
 private:
-	PTR(T)     m_interface;
-	LPIUnknow  m_counter;
+	LPIUnknow  m_smt_ptr;
+};
+
+template<class T>
+class Interface: public UnknowPointer
+{
+public:
+	Interface(PTR(void) aInterface, LPIUnknow smt_ptr)
+		: UnknowPointer(aInterface, smt_ptr)
+	{}
+	Interface(CREF(Interface<T>) unknowPointer)
+		: UnknowPointer(unknowPointer)
+	{}
+	PTR(T) operator-> ()
+	{
+		return static_cast<PTR(T)>(m_interface);
+	}
 };
 
 class IUnknow
 {
 public:
-	virtual void  AddRef () = 0;
-	virtual void  Release() = 0;
-	virtual Interface<void> queryInterface(ruint id) = 0;
+	virtual void          AddRef () = 0;
+	virtual void          Release() = 0;
+	virtual UnknowPointer queryInterface(ruint id) = 0;
 };
 typedef PTR(IUnknow) LPIUnknow;
 
-template<class T>
-class LocalInterface
-{
-public:
-	LocalInterface(PTR(T) p)
-		: m_pInterface(p)
-	{}
-	operator rbool () const
-	{
-		return m_pInterface != NULL;
-	}
-	PTR(T) get()
-	{
-		return m_pInterface;
-	}
-	PTR(T) operator-> ()
-	{
-		return get();
-	}
+inline UnknowPointer::UnknowPointer()
+	: m_interface(NULL)
+	, m_smt_ptr  (NULL)
+{}
 
-public:
-	PTR(T) m_pInterface;
-};
+inline UnknowPointer::UnknowPointer(PTR(void) aInterface, LPIUnknow smt_ptr)
+	: m_interface(aInterface)
+	, m_smt_ptr  (smt_ptr   )
+{
+	m_smt_ptr->AddRef();
+}
+
+inline UnknowPointer::UnknowPointer(CREF(UnknowPointer) unknowPointer)
+	: m_interface(unknowPointer.m_interface)
+	, m_smt_ptr  (unknowPointer.m_smt_ptr  )
+{
+	m_smt_ptr->AddRef();
+}
+
+inline UnknowPointer::~UnknowPointer()
+{
+	if (m_smt_ptr)
+		m_smt_ptr->Release();
+}
+
+inline UnknowPointer::operator rbool () const
+{
+	return m_interface != NULL;
+}
+
+inline UnknowPointer UnknowPointer::queryInterface(ruint id)
+{
+	return m_smt_ptr ? m_smt_ptr->queryInterface(id) : UnknowPointer();
+}
+
+template<class I>
+inline Interface<I> UnknowPointer::queryInterface()
+{
+	UnknowPointer pointer = m_smt_ptr ? m_smt_ptr->queryInterface(GetInterface<I>::ID) : UnknowPointer();
+	return Interface<I>(pointer.m_interface, pointer.m_smt_ptr);
+}
 
 template<class T>
 class smart_ptr: public IUnknow
@@ -126,15 +169,19 @@ public:
 	{
 		return get();
 	}
-	template<class I> Interface<I> query()
+	operator UnknowPointer ()
+	{
+		return get() ? UnknowPointer(NULL, this) : UnknowPointer();
+	}
+	template<class I> Interface<I> queryInterface()
 	{
 		if (get() != NULL)
 		{
-			LocalInterface<I> localInterface = get()->query<I>();
-			if (localInterface)
-				return Interface<I>(localInterface.get(), this);
+			PTR(void) pInterface = get()->queryInterface(GetInterface<I>::ID);
+			if (pInterface)
+				return Interface<I>(pInterface, this);
 		}
-		return Interface<I>();
+		return Interface<I>(NULL, NULL);
 	}
 
 private:
@@ -169,15 +216,15 @@ private:
 			}
 		}
 	}
-	virtual Interface<void> queryInterface(ruint id)
+	virtual UnknowPointer queryInterface(ruint id)
 	{
 		if (get() != NULL)
 		{
-			LocalInterface<void> localInterface = get()->queryInterface(id);
-			if (localInterface)
-				return Interface<void>(localInterface.get(), this);
+			PTR(void) pInterface = get()->queryInterface(id);
+			if (pInterface)
+				return UnknowPointer(pInterface, this);
 		}
-		return Interface<void>();
+		return UnknowPointer();
 	}
 };
 
@@ -194,7 +241,7 @@ public:
 	{
 		return smart_ptr<T>(new T(p1));
 	}
-	static void destroy(T* obj)
+	static void destroy(PTR(T) obj)
 	{
 		delete obj;
 	}
@@ -202,28 +249,44 @@ public:
 
 CLOSE_RDO_NAMESPACE
 
-#define QUERY_INTERFACE_NULL         template<class T> rdo::LocalInterface<T> query() { return NULL;                  }
-#define QUERY_INTERFACE_PARENT(A)    template<class T> rdo::LocalInterface<T> query() { return A::query<T>();         }
-#define QUERY_INTERFACE_PARENT2(A,B) template<class T> rdo::LocalInterface<T> query() { rdo::LocalInterface<T> lpintA = A::query<T>(); return lpintA ? lpintA : B::query<T>(); }
-#define QUERY_INTERFACE(A)           template<>        rdo::LocalInterface<A> query() { return static_cast<A*>(this); }
-
-#define DEFINE_THIS_TYPE(A)  typedef A this_type;
-#define DEFINE_FACTORY(A)    friend class rdo::Factory<A>;
-#define DEFINE_CLASS_NAME(A) static std::string className() { return #A; }
+#define DEFINE_THIS_TYPE(A)    typedef A this_type;
+#define DEFINE_IPARENT_TYPE(P) typedef P iparent_type;
+#define DEFINE_FACTORY(A)      friend class rdo::Factory<A>;
+#define DEFINE_CLASS_NAME(A)   static std::string className() { return #A; }
 
 #define RDO_IOBJECT(A, BASE) \
 protected: \
 DEFINE_THIS_TYPE(A) \
 DEFINE_FACTORY(A) \
+DEFINE_IPARENT_TYPE(BASE) \
 public: \
-DEFINE_CLASS_NAME(A) \
-QUERY_INTERFACE_PARENT(BASE)
+DEFINE_CLASS_NAME(A)
+
+#define QUERY_INTERFACE_BEGIN \
+public: \
+PTR(void) queryInterface(ruint id) \
+{ \
+	PTR(void) pIterface = iparent_type::queryInterface(id); \
+	if (pIterface) \
+		return pIterface; \
+ \
+	switch (id) \
+	{
+
+#define QUERY_INTERFACE(A)        case IID(A): return static_cast<PTR(A)>(this);
+#define QUERY_INTERFACE_END \
+	} \
+	return NULL; \
+}
 
 OPEN_RDO_NAMESPACE
 class IObjectBase
 {
 public:
-	QUERY_INTERFACE_NULL;
+	PTR(void) queryInterface(ruint id)
+	{
+		return NULL;
+	}
 };
 CLOSE_RDO_NAMESPACE
 
