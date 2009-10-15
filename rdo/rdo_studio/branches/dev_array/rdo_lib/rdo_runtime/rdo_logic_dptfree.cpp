33c04382b63767dcb00aa8b9187590b8e7bdda7f
Andrey Urusov <drobus@gmail.com>
2009-10-15 13:31:54 +0000

    * бранч для добавления массивов
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1864 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_runtime/rdo_logic_dptfree.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "rdo_logic_dptfree.h"
+#include "rdo_runtime.h"
+
+namespace rdoRuntime
+{
+
+// ----------------------------------------------------------------------------
+// ---------- RDODPTFree
+// ----------------------------------------------------------------------------
+RDODPTFree::RDODPTFree( RDOSimulator* sim ):
+	RDOLogic( sim )
+{
+}
+
+RDODPTFree::~RDODPTFree()
+{
+}
+
+} // namespace rdoRuntime
