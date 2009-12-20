8eee2856c3390f2ce4102772a4b653281ba989d8
Mihail Chirkov <seivgard@gmail.com>
2009-12-20 18:10:49 +0000

    * revert changes, rev.[1947-1970]
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@1972 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_runtime/test/rdo_value_test/main.cpp
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_runtime/test/rdo_value_test/main.cpp
@@ -1,34 +1,24 @@
+/*
+ * copyright: (c) RDO-Team, 2009
+ * filename : main.cpp
+ * author   : Урусов Андрей
+ * date     : 13.07.2009
+ * bref     : тест для RDOValue
+ * indent   : 4T
+ */
+
 // ====================================================================== INCLUDES
 #include <iostream>
 // ====================================================================== SYNOPSIS
 #include "rdo_value.h"
-#include "rdo_enum.h"
-#include "rdo_array.h"
-#include "rdo_type.h"
 // ===============================================================================
 
 using namespace rdoRuntime;
 
 void main()
 {
-	RDOEnumType* enumType = new RDOEnumType(NULL, RDOEnumType::Enums("Red")("Green")("Type"));
-	RDOValue integer = 1;
-	RDOValue real    = 1.5;
-	RDOValue booling = true;
-	RDOValue string  = "string";
-	RDOArrayType* arrayType_int    = new RDOArrayType(NULL,&integer.type());
-	RDOArrayType* arrayType_real   = new RDOArrayType(NULL,&real.type());
-	RDOArrayType* arrayType_bool   = new RDOArrayType(NULL,&booling.type());
-	RDOArrayType* arrayType_string = new RDOArrayType(NULL,&string.type());
-	RDOArrayType* arrayType_enum   = new RDOArrayType(NULL,enumType);
-	RDOArrayType* arrayType1       = new RDOArrayType(NULL,arrayType_int);
-	RDOArrayType* arrayType2       = new RDOArrayType(NULL,arrayType_real);
-	RDOArrayType* arrayType3       = new RDOArrayType(NULL,arrayType_bool);
-	RDOArrayType* arrayType4       = new RDOArrayType(NULL,arrayType_string);
-	RDOArrayType* arrayType5       = new RDOArrayType(NULL,arrayType_enum);
-	RDOArrayType* arrayType6       = new RDOArrayType(NULL,arrayType5);
-	RDOArrayType* arrayType7       = new RDOArrayType(NULL,arrayType6);
-	std::string className = arrayType2->asString();
-
+	RDOValue value1("qqq");
+	RDOValue value2 = value1;
+	value2 += "aaa";
 	int i = 1;
 }
