af35438ac8ef891151e6f733df1f40b2ec3559fd
Andrey Urusov <drobus@gmail.com>
2010-02-10 19:57:43 +0000

    - добавлена рыба для класса ошибок
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2026 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdoparser_error.h
@@ -0,0 +1,21 @@
+/*
+ * copyright: (c) RDO-Team, 2009
+ * filename : rdoparser_error.h
+ * author   : Александ Барс, Урусов Андрей
+ * date     : 09.02.2010
+ * bref     : основан на rdoparser.h/cpp
+ * indent   : 4T
+ */
+
+#ifndef _RDOPARSER_ERROR_H_
+#define _RDOPARSER_ERROR_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif //! _RDOPARSER_ERROR_H_
