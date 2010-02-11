b9aedadf2eab0016407cfcad1df23b45154dd387
Andrey Urusov <drobus@gmail.com>
2010-02-11 15:03:22 +0000

    - форматирование
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2037 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/pch.h
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/pch.h
@@ -1,9 +1,22 @@
-#pragma warning(disable : 4786)  
+/*
+ * copyright: (c) RDO-Team, 2009
+ * filename : pch.h
+ * author   : Александ Барс, Урусов Андрей
+ * date     : 
+ * bref     : 
+ * indent   : 4T
+ */
 
-#ifndef WINVER               // Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501        // Change this to the appropriate value to target other versions of Windows.
+#ifndef _PARSER_PCH_H_
+#define _PARSER_PCH_H_
+
+#pragma warning(disable: 4786)  
+
+#ifndef WINVER               //! Allow use of features specific to Windows XP or later.
+#define WINVER 0x0501        //! Change this to the appropriate value to target other versions of Windows.
 #endif
 
+// ====================================================================== INCLUDES
 #include <algorithm>
 #include <vector>
 #include <map>
@@ -16,8 +29,11 @@
 #include <list>
 #include <math.h>
 #include <time.h>
-
+// ====================================================================== SYNOPSIS
 #include "rdo_common/rdocommon.h"
 #include "rdo_common/rdotypes.h"
 #include "rdo_common/rdomacros.h"
 #include "rdo_common/rdodebug.h"
+// ===============================================================================
+
+#endif //! _PARSER_PCH_H_
