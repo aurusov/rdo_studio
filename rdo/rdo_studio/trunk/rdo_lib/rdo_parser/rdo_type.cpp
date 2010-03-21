f30febf75bd153bfdf7d26b2c17b63420855a433
Andrey Urusov <drobus@gmail.com>
2010-03-21 23:46:30 +0000

    - добавлены сообщения об ошибках при кастинге  - статические классы генеряться через псевдо-фабрику  - name перенесен в cpp  - добавлен кастинг calc'ов
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2162 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type.cpp
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type.cpp
@@ -11,10 +11,10 @@
 #include "rdo_lib/rdo_parser/pch.h"
 // ====================================================================== INCLUDES
 // ====================================================================== SYNOPSIS
-#include "rdo_lib/rdo_parser/rdo_type.h"
-#include "rdo_lib/rdo_parser/rdo_type_range.h"
 #include "rdo_lib/rdo_parser/rdo_value.h"
-//#include "rdo_lib/rdo_parser/rdoparser.h"
+#include "rdo_lib/rdo_parser/rdo_type.h"
+#include "rdo_lib/rdo_parser/rdoparser_error.h"
+#include "rdo_lib/rdo_parser/rdoparser.h"
 #include "rdo_lib/rdo_runtime/rdo_exception.h"
 // ===============================================================================
 
@@ -23,33 +23,24 @@ OPEN_RDO_PARSER_NAMESPACE
 // ----------------------------------------------------------------------------
 // ---------- ATOM_TYPE_PARSER
 // ----------------------------------------------------------------------------
-#define DECLARE_ATOM_TYPE_PARSER(Type)              \
+#define DECLARE_ATOM_TYPE_PARSER(Type, TypeName)    \
+tstring RDOType__##Type::name() const               \
+{                                                   \
+	return TypeName;                                \
+}                                                   \
 RDOType__##Type g_##Type##_type;                    \
-rdo::smart_ptr<RDOType> g_##Type(&g_##Type##_type);
+rdo::smart_ptr<RDOType__##Type> g_##Type(&g_##Type##_type, true);
 
-DECLARE_ATOM_TYPE_PARSER(unknow       );
-DECLARE_ATOM_TYPE_PARSER(identificator);
-DECLARE_ATOM_TYPE_PARSER(int          );
-DECLARE_ATOM_TYPE_PARSER(real         );
-DECLARE_ATOM_TYPE_PARSER(bool         );
-DECLARE_ATOM_TYPE_PARSER(string       );
+DECLARE_ATOM_TYPE_PARSER(unknow,        _T("unknow")       );
+DECLARE_ATOM_TYPE_PARSER(identificator, _T("identificator"));
+DECLARE_ATOM_TYPE_PARSER(int,           _T("integer")      );
+DECLARE_ATOM_TYPE_PARSER(real,          _T("real")         );
+DECLARE_ATOM_TYPE_PARSER(bool,          _T("bool")         );
+DECLARE_ATOM_TYPE_PARSER(string,        _T("string")       );
 
 // ----------------------------------------------------------------------------
 // ---------- RDOType
 // ----------------------------------------------------------------------------
-LPRDOType RDOType::type_cast_throw(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
-{
-	LPRDOType toType = type_cast(from);
-	if (!toType)
-	{
-		rdoParse::g_error().push_only(src_info, rdo::format(_T("Несовместимые типы данных: %s и %s"), name().c_str(), from->name().c_str()));
-		rdoParse::g_error().push_only(to_src_info,   _T("См. первый тип"));
-		rdoParse::g_error().push_only(from_src_info, _T("См. второй тип"));
-		rdoParse::g_error().push_done();
-	}
-	return toType;
-}
-
 LPRDOType RDOType::getTypeByID(rdoRuntime::RDOType::TypeID typeID)
 {
 	switch (typeID)
@@ -65,9 +56,10 @@ LPRDOType RDOType::getTypeByID(rdoRuntime::RDOType::TypeID typeID)
 }
 
 //! RDOType__unknow
-LPRDOType RDOType__unknow::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__unknow::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
-	return NULL;
+	rdoParse::g_error().error(src_info, rdo::format(_T("Внутренная ошибка парсера. Невозможно преобразовать тип '%s' к неизвестному типу"), from_src_info.src_text().c_str()));
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__unknow::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -78,20 +70,33 @@ RDOValue RDOType__unknow::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo)
 	return RDOValue();
 }
 
+PTR(rdoRuntime::RDOCalc) RDOType__unknow::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return parent_type::calc_cast(pCalc, pType);
+}
+
 void RDOType__unknow::writeModelStructure(REF(std::ostream) stream) const
 {
+	rdoParse::g_error().error(RDOParserSrcInfo(), _T("Внутренная ошибка парсера. Невозможно записать неизвестный тип в отчет"));
 	NEVER_REACH_HERE;
 }
 
 //! RDOType__int
-LPRDOType RDOType__int::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__int::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
 	switch (from->type().typeID())
 	{
-		case rdoRuntime::RDOType::t_int : return g_int;
-		case rdoRuntime::RDOType::t_real: return g_real;
+		case rdoRuntime::RDOType::t_int :
+			return g_int;
+		case rdoRuntime::RDOType::t_real:
+			rdoParse::g_error().warning(src_info, _T("Перевод вещественного числа в целое, возможна потеря данных"));
+			return g_real;
+		default:
+			rdoParse::g_error().push_only(src_info,    rdo::format(_T("Ожидается целочисленное значение, найдено: %s"), from_src_info.src_text().c_str()));
+			rdoParse::g_error().push_only(to_src_info, rdo::format(_T("См. тип: %s"), to_src_info.src_text().c_str()));
+			rdoParse::g_error().push_done();
 	}
-	return NULL;
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__int::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -113,20 +118,34 @@ RDOValue RDOType__int::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to
 	return toValue;
 }
 
+PTR(rdoRuntime::RDOCalc) RDOType__int::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	if (pType->typeID() == rdoRuntime::RDOType::t_real)
+	{
+		return new rdoRuntime::RDOCalcDoubleToInt(RDOParser::s_parser()->runtime(), pCalc);
+	}
+	return pCalc;
+}
+
 void RDOType__int::writeModelStructure(REF(std::ostream) stream) const
 {
 	stream << _T("I") << std::endl;
 }
 
 //! RDOType__real
-LPRDOType RDOType__real::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__real::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
 	switch (from->type().typeID())
 	{
 		case rdoRuntime::RDOType::t_int :
-		case rdoRuntime::RDOType::t_real: return g_real;
+		case rdoRuntime::RDOType::t_real:
+			return g_real;
+		default:
+			rdoParse::g_error().push_only(src_info,    rdo::format(_T("Ожидается вещественное значение, найдено: %s"), from_src_info.src_text().c_str()));
+			rdoParse::g_error().push_only(to_src_info, rdo::format(_T("См. тип: %s"), to_src_info.src_text().c_str()));
+			rdoParse::g_error().push_done();
 	}
-	return NULL;
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__real::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -148,19 +167,29 @@ RDOValue RDOType__real::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) t
 	return toValue;
 }
 
+PTR(rdoRuntime::RDOCalc) RDOType__real::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return parent_type::calc_cast(pCalc, pType);
+}
+
 void RDOType__real::writeModelStructure(REF(std::ostream) stream) const
 {
 	stream << _T("R") << std::endl;
 }
 
 //! RDOType__string
-LPRDOType RDOType__string::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__string::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
 	switch (from->type().typeID())
 	{
-		case rdoRuntime::RDOType::t_string: return g_string;
+		case rdoRuntime::RDOType::t_string:
+			return g_string;
+		default:
+			rdoParse::g_error().push_only(src_info,    rdo::format(_T("Ожидается строковое значение, найдено: %s"), from_src_info.src_text().c_str()));
+			rdoParse::g_error().push_only(to_src_info, rdo::format(_T("См. тип: %s"), to_src_info.src_text().c_str()));
+			rdoParse::g_error().push_done();
 	}
-	return NULL;
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__string::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -182,15 +211,21 @@ RDOValue RDOType__string::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo)
 	return toValue;
 }
 
+PTR(rdoRuntime::RDOCalc) RDOType__string::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return parent_type::calc_cast(pCalc, pType);
+}
+
 void RDOType__string::writeModelStructure(REF(std::ostream) stream) const
 {
 	stream << _T("S") << std::endl;
 }
 
 //! RDOType__identificator
-LPRDOType RDOType__identificator::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__identificator::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
-	return NULL;
+	rdoParse::g_error().error(src_info, rdo::format(_T("Внутренная ошибка парсера. Невозможно преобразовать тип '%s' к идентификатору"), from_src_info.src_text().c_str()));
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__identificator::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -201,19 +236,30 @@ RDOValue RDOType__identificator::value_cast(CREF(RDOValue) from, CREF(RDOParserS
 	return RDOValue();
 }
 
+PTR(rdoRuntime::RDOCalc) RDOType__identificator::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return parent_type::calc_cast(pCalc, pType);
+}
+
 void RDOType__identificator::writeModelStructure(REF(std::ostream) stream) const
 {
+	rdoParse::g_error().error(RDOParserSrcInfo(), _T("Внутренная ошибка парсера. Невозможно записать тип идектификатор в отчет"));
 	NEVER_REACH_HERE;
 }
 
 //! RDOType__bool
-LPRDOType RDOType__bool::type_cast(CREF(LPRDOType) from) const
+LPRDOType RDOType__bool::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
 {
 	switch (from->type().typeID())
 	{
-		case rdoRuntime::RDOType::t_bool: return g_bool;
+		case rdoRuntime::RDOType::t_bool:
+			return g_bool;
+		default:
+			rdoParse::g_error().push_only(src_info,    rdo::format(_T("Ожидается булевское значение, найдено: %s"), from_src_info.src_text().c_str()));
+			rdoParse::g_error().push_only(to_src_info, rdo::format(_T("См. тип: %s"), to_src_info.src_text().c_str()));
+			rdoParse::g_error().push_done();
 	}
-	return NULL;
+	return rdo::smart_ptr_null();
 }
 
 RDOValue RDOType__bool::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
@@ -228,11 +274,16 @@ RDOValue RDOType__bool::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) t
 
 	if (toValue.typeID() == rdoRuntime::RDOType::t_unknow)
 	{
-		rdoParse::g_error().push_only(src_info,    rdo::format(_T("Невозможно преобразовать значение '%s' к строковому типу"), from.src_info().src_text().c_str()));
+		rdoParse::g_error().push_only(src_info,    rdo::format(_T("Невозможно преобразовать значение '%s' к булевскому типу"), from.src_info().src_text().c_str()));
 		rdoParse::g_error().push_only(to_src_info, rdo::format(_T("См. тип: %s"), to_src_info.src_text().c_str()));
 		rdoParse::g_error().push_done();
 	}
 	return toValue;
+}
+
+PTR(rdoRuntime::RDOCalc) RDOType__bool::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return parent_type::calc_cast(pCalc, pType);
 }
 
 void RDOType__bool::writeModelStructure(REF(std::ostream) stream) const
--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type.h
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type.h
@@ -16,39 +16,64 @@
 #include "rdo_lib/rdo_runtime/rdo_type.h"
 #include "rdo_lib/rdo_runtime/rdo_value.h"
 #include "rdo_lib/rdo_runtime/rdo_model_interface.h"
+#include "rdo_lib/rdo_runtime/rdocalc.h"
 #include "rdo_common/rdosmart_ptr.h"
 // ===============================================================================
 
 OPEN_RDO_PARSER_NAMESPACE
 
 // ----------------------------------------------------------------------------
-// ---------- RDOType
+// ---------- IType
 // ----------------------------------------------------------------------------
 class RDOValue;
 class RDOType;
 DECLARE_POINTER(RDOType);
 
-class RDOType: public IModelStructure
+class IType
 {
-friend class rdo::Factory<RDOType>;
+public:
+	virtual tstring   name() const = 0;
+	virtual LPRDOType type_cast(
+		CREF(LPRDOType)        from,
+		CREF(RDOParserSrcInfo) from_src_info,
+		CREF(RDOParserSrcInfo) to_src_info,
+		CREF(RDOParserSrcInfo) src_info
+	) const = 0;
+	virtual RDOValue  value_cast(
+		CREF(RDOValue)         from,
+		CREF(RDOParserSrcInfo) to_src_info,
+		CREF(RDOParserSrcInfo) src_info
+	) const = 0;
+	//! calc_cast вызывается строго после type_cast, поэтому никаких RDOParserSrcInfo не передается
+	virtual PTR(rdoRuntime::RDOCalc) calc_cast(
+		PTR(rdoRuntime::RDOCalc) pCalc,
+		CREF(LPRDOType)          pType
+	) const = 0;
+};
+#define DECLARE_IType                                                                                                                                                                   \
+	virtual tstring                  name      () const;                                                                                                                                \
+	virtual LPRDOType                type_cast (CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const; \
+	virtual RDOValue                 value_cast(CREF(RDOValue)  from, CREF(RDOParserSrcInfo) to_src_info,   CREF(RDOParserSrcInfo) src_info)                                     const; \
+	virtual PTR(rdoRuntime::RDOCalc) calc_cast (PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const;
+
+// ----------------------------------------------------------------------------
+// ---------- RDOType
+// ----------------------------------------------------------------------------
+class RDOType: public IType, public IModelStructure
+{
+DECLARE_FACTORY(RDOType)
 public:
 	CREF(rdoRuntime::RDOType)        type() const { return *m_type;         }
 	CPTR(rdoRuntime::RDOType) operator-> () const { return  m_type;         }
 
 	rdoRuntime::RDOType::TypeID    typeID() const { return type().typeID(); }
 
-	LPRDOType type_cast_throw(
-		CREF(LPRDOType)        from,
-		CREF(RDOParserSrcInfo) from_src_info,
-		CREF(RDOParserSrcInfo) to_src_info,
-		CREF(RDOParserSrcInfo) src_info
-	) const;
-
-	virtual tstring   name      ()                     const = 0;
-	virtual LPRDOType type_cast (CREF(LPRDOType) from) const = 0;
-	virtual RDOValue  value_cast(CREF(RDOValue)  from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const = 0;
-
 	static LPRDOType getTypeByID(rdoRuntime::RDOType::TypeID typeID);
+
+	virtual PTR(rdoRuntime::RDOCalc) calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+	{
+		return pCalc;
+	}
 
 protected:
 	RDOType(CPTR(rdoRuntime::RDOType) type)
@@ -61,32 +86,33 @@ protected:
 
 	CPTR(rdoRuntime::RDOType) m_type;
 };
+DECLARE_POINTER(RDOType);
 
 // ----------------------------------------------------------------------------
 // ---------- ATOM_TYPE_PARSER
 // ----------------------------------------------------------------------------
-#define DEFINE_ATOM_TYPE_PARSER(Type, TypeName)                                   \
-class RDOType__##Type: public RDOType                                             \
-{                                                                                 \
-public:                                                                           \
-	RDOType__##Type():                                                            \
-		RDOType(&rdoRuntime::g_##Type)                                            \
-	{}                                                                            \
-	~RDOType__##Type()                                                            \
-	{}                                                                            \
-	virtual tstring   name      ()                     const { return TypeName; } \
-	virtual LPRDOType type_cast (CREF(LPRDOType) from) const;                     \
-	virtual RDOValue  value_cast(CREF(RDOValue)  from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const; \
-	DECLARE_IModelStructure;                                                      \
-};                                                                                \
-extern rdo::smart_ptr<RDOType> g_##Type;
+#define DEFINE_ATOM_TYPE_PARSER(Type)    \
+class RDOType__##Type: public RDOType    \
+{                                        \
+private:                                 \
+typedef RDOType parent_type;             \
+public:                                  \
+	RDOType__##Type():                   \
+		RDOType(&rdoRuntime::g_##Type)   \
+	{}                                   \
+	~RDOType__##Type()                   \
+	{}                                   \
+	DECLARE_IType;                       \
+	DECLARE_IModelStructure;             \
+};                                       \
+extern rdo::smart_ptr<RDOType__##Type> g_##Type;
 
-DEFINE_ATOM_TYPE_PARSER(unknow,        _T("unknow")       );
-DEFINE_ATOM_TYPE_PARSER(identificator, _T("identificator"));
-DEFINE_ATOM_TYPE_PARSER(int,           _T("integer")      );
-DEFINE_ATOM_TYPE_PARSER(real,          _T("real")         );
-DEFINE_ATOM_TYPE_PARSER(bool,          _T("bool")         );
-DEFINE_ATOM_TYPE_PARSER(string,        _T("string")       );
+DEFINE_ATOM_TYPE_PARSER(unknow       );
+DEFINE_ATOM_TYPE_PARSER(identificator);
+DEFINE_ATOM_TYPE_PARSER(int          );
+DEFINE_ATOM_TYPE_PARSER(real         );
+DEFINE_ATOM_TYPE_PARSER(bool         );
+DEFINE_ATOM_TYPE_PARSER(string       );
 
 CLOSE_RDO_PARSER_NAMESPACE
 
