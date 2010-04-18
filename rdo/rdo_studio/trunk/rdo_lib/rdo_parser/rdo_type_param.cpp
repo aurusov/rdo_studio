42d1b2a2fdd9f3b5ac34d0438172262adf812f7e
Andrey Urusov <drobus@gmail.com>
2010-04-18 18:04:14 +0000

    - формирование src_text для RDOTypeParam
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2213 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type_param.cpp
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type_param.cpp
@@ -26,10 +26,13 @@ RDOTypeParam::RDOTypeParam(CREF(LPRDOType) type, CREF(RDOValue) default, CREF(RD
 	, m_default       (default )
 {
 	ASSERT(m_type);
+	setSrcText(m_type->name());
+
 	if (m_default.defined())
 	{
 		m_type->type_cast(m_default.type(), m_default.src_info(), src_info, m_default.src_info());
 		m_default = m_type->value_cast(m_default, src_info, m_default.src_info());
+		setSrcText(rdo::format(_T("%s = %s"), src_text().c_str(), m_default.src_text().c_str()));
 	}
 	switch (m_type->type().typeID())
 	{
