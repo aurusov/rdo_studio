77f39c1a8c972c64b8b9f4662517db6033aa52a9
Andrey Urusov <drobus@gmail.com>
2010-04-18 18:50:48 +0000

    - формирование сообщения об ошибке
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2216 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type_param.cpp
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/rdo_type_param.cpp
@@ -30,8 +30,8 @@ RDOTypeParam::RDOTypeParam(CREF(LPRDOType) type, CREF(RDOValue) default, CREF(RD
 
 	if (m_default.defined())
 	{
-		m_type->type_cast(m_default.type(), m_default.src_info(), src_info, m_default.src_info());
-		m_default = m_type->value_cast(m_default, src_info, m_default.src_info());
+		m_type->type_cast(m_default.type(), m_default.src_info(), this->src_info(), m_default.src_info());
+		m_default = m_type->value_cast(m_default, this->src_info(), m_default.src_info());
 		setSrcText(rdo::format(_T("%s = %s"), src_text().c_str(), m_default.src_text().c_str()));
 	}
 	switch (m_type->type().typeID())
