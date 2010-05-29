d7bb1f1dabdee69d2824374852df1e71efb0ed75
Andrey Urusov <drobus@gmail.com>
2010-05-29 15:07:07 +0000

    - добавлен пункт меню circle
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2340 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_animation/rdo_studio/resource.h
+++ rdo/rdo_studio/branches/dev_animation/rdo_studio/resource.h
@@ -380,6 +380,7 @@
 #define ID_INSERT_RTP_BOOL              33014
 #define ID_INSERT_RTP_TRUE              33015
 #define ID_INSERT_RTP_FALSE             33016
+#define ID_INSERT_FRM_CIRCLE            33017
 #define ID_INSERT_BUFFER1_PASTE         33110
 #define ID_INSERT_BUFFER1_EDIT          33111
 #define ID_INSERT_BUFFER2_PASTE         33112
