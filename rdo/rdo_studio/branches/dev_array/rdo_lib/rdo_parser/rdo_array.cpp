15cb537a4d9c7b265acc1525d99d8a4096dc5358
Mihail Chirkov <seivgard@gmail.com>
2010-05-30 15:16:26 +0000

    - добавленны необходимые методы для RDOArrayType в парсере.
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2349 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/rdo_array.cpp
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/rdo_array.cpp
@@ -14,6 +14,7 @@
 #include "rdo_lib/rdo_parser/rdo_array.h"
 #include "rdo_lib/rdo_parser/rdoparser.h"
 #include "rdo_lib/rdo_parser/rdoparser_error.h"
+#include "rdo_lib/rdo_parser/rdoparser_lexer.h"
 // ===============================================================================
 
 OPEN_RDO_PARSER_NAMESPACE
@@ -29,6 +30,92 @@ RDOArrayType::RDOArrayType()
 
 RDOArrayType::~RDOArrayType()
 {
+}
+inline tstring RDOArrayType::name() const
+{
+	CPTR(RDOArrayType) pArray(const_cast<PTR(RDOArrayType)>(this));
+	switch(pArray->typeID())
+	{
+	case rdoRuntime::RDOType::t_array:
+	{
+		return rdo::format(_T("array<%s>"), (*pArray).name()); break;
+	}
+	default: return rdo::format(_T("unknown type"));break;
+	}
+}
+LPRDOType RDOArrayType::type_cast(CREF(LPRDOType) from, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
+{
+	switch (from->typeID())
+	{
+	case rdoRuntime::RDOType::t_array:
+		{
+			LPRDOArrayType pArray(const_cast<PTR(RDOArrayType)>(this));
+			//! Ёто один и тот же тип
+			if (pArray == from)
+				return pArray;
+
+			else
+			{
+				rdoParse::g_error().push_only(src_info,     _T("Ќесоответствие несоответствие размерности массивов"));
+				rdoParse::g_error().push_only(to_src_info,   to_src_info.src_text());
+				rdoParse::g_error().push_only(src_info,     _T("и"));
+				rdoParse::g_error().push_only(from_src_info, from_src_info.src_text());
+			}
+			rdoParse::g_error().push_done();
+			break;
+		}
+	default:
+		rdoParse::g_error().push_only(src_info,    rdo::format(_T("ќжидаетс€ значение типа массив, найдено: %s"), from_src_info.src_text().c_str()));
+		rdoParse::g_error().push_only(to_src_info, rdo::format(_T("—м. тип: %s"), to_src_info.src_text().c_str()));
+		rdoParse::g_error().push_done();
+		break;
+	}
+	return rdo::smart_ptr_null();
+}
+RDOValue RDOArrayType::value_cast(CREF(RDOValue) from, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const
+{
+	RDOValue toValue;
+	LPRDOArrayType pArray(const_cast<PTR(RDOArrayType)>(this));
+	try
+	{
+		if (from.typeID() == rdoRuntime::RDOType::t_array)
+			if (m_type == &from.type()->type())
+				toValue = from;
+
+		//case rdoRuntime::RDOType::t_int:			toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getInt()),			pArray, from.src_info()); break;
+		//case rdoRuntime::RDOType::t_real:			toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getDouble()),			pArray, from.src_info()); break;
+		//case rdoRuntime::RDOType::t_bool:			toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getBool()),			pArray, from.src_info()); break;
+		//case rdoRuntime::RDOType::t_string:			toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getString()),			pArray, from.src_info()); break;
+		//case rdoRuntime::RDOType::t_enum:			toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getEnum()),			pArray, from.src_info()); break;
+		//case rdoRuntime::RDOType::t_identificator:	toValue = RDOValue(rdoRuntime::RDOValue(getArray(), from->getIdentificator()),	pArray, from.src_info()); break;
+	}
+	catch (CREF(rdoRuntime::RDOValueException))
+	{}
+
+	if (toValue.typeID() == rdoRuntime::RDOType::t_unknow)
+	{
+		rdoParse::g_error().push_only(src_info,    rdo::format(_T("Ќеверное значение параметра дл€ массива: %s"), from.src_info().src_text().c_str()));
+		rdoParse::g_error().push_only(to_src_info, rdo::format(_T(": %s"), name().c_str()));
+		rdoParse::g_error().push_done();
+	}
+	return toValue;
+}
+
+PTR(rdoRuntime::RDOCalc) RDOArrayType::calc_cast(PTR(rdoRuntime::RDOCalc) pCalc, CREF(LPRDOType) pType) const
+{
+	return RDOType::calc_cast(pCalc, pType);
+}
+
+RDOValue RDOArrayType::get_default() const
+{
+	LPRDOArrayType pArray(const_cast<PTR(RDOArrayType)>(this));
+	return RDOValue(rdoRuntime::RDOValue(getArray()), pArray, RDOParserSrcInfo());
+}
+
+void RDOArrayType::writeModelStructure(REF(std::ostream) stream) const
+{
+	CPTR(RDOArrayType) pArray(const_cast<PTR(RDOArrayType)>(this));
+	stream << (*pArray).name()<< std::endl;
 }
 
 CLOSE_RDO_PARSER_NAMESPACE
