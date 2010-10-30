67cbef76234f2bf83791250d303ca956b9cdcd87
Andrey Urusov <drobus@gmail.com>
2010-10-30 10:25:13 +0000

    - добавлена рыба контекста типа
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2884 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/context/type.cpp
@@ -0,0 +1,49 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : type.cpp
+ * author   : Урусов Андрей
+ * date     : 30.10.2010
+ * bref     : 
+ * indent   : 4T
+ */
+
+// ====================================================================== PCH
+#include "rdo_lib/rdo_parser/pch.h"
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_lib/rdo_parser/context/type.h"
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- TypeContext
+// ----------------------------------------------------------------------------
+TypeContext::TypeContext(CREF(LPRDOTypeParam) pTypeParam)
+	: m_pTypeParam(pTypeParam)
+{}
+
+LPRDOTypeParam TypeContext::getType() const
+{
+	return m_pTypeParam;
+}
+
+LPContext TypeContext::parser(PTR(RDOParser) pParser, CREF(RDOValue) value)
+{
+	NEVER_REACH_HERE;
+	return NULL;
+}
+
+rdoRuntime::LPRDOCalc TypeContext::getCalc()
+{
+	NEVER_REACH_HERE;
+	return NULL;
+}
+
+LPLocalVariableList TypeContext::getLocalMemory()
+{
+	NEVER_REACH_HERE;
+	return NULL;
+}
+
+CLOSE_RDO_PARSER_NAMESPACE
---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/context/type.h
@@ -0,0 +1,53 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : type.h
+ * author   : Урусов Андрей
+ * date     : 30.10.2010
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _RDOPARSER_CONTEXT_TYPE_H_
+#define _RDOPARSER_CONTEXT_TYPE_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_common/rdomacros.h"
+
+#include "rdo_lib/rdo_parser/namespace.h"
+#include "rdo_lib/rdo_parser/rdo_type_param.h"
+#include "rdo_lib/rdo_parser/context/context.h"
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- ITypeContext
+// ----------------------------------------------------------------------------
+S_INTERFACE(ITypeContext)
+{
+	virtual LPRDOTypeParam getType() const = 0;
+};
+#define DECLARE_ITypeContext        \
+	LPRDOTypeParam getType() const;
+
+// ----------------------------------------------------------------------------
+// ---------- TypeContext
+// ----------------------------------------------------------------------------
+OBJECT(TypeContext)
+	IS  IMPLEMENTATION_OF(IContext    )
+	AND IMPLEMENTATION_OF(ITypeContext)
+{
+DECLARE_FACTORY(TypeContext);
+private:
+	TypeContext(CREF(LPRDOTypeParam) pTypeParam);
+
+	LPRDOTypeParam m_pTypeParam;
+
+	DECLARE_IContext;
+	DECLARE_ITypeContext;
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif //! _RDOPARSER_CONTEXT_TYPE_H_
