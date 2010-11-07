9db04e101d255807ece11397648bae5461c99dc5
Andrey Urusov <drobus@gmail.com>
2010-11-07 15:32:33 +0000

    - добавлено получение имени временного файла  - добавлена удаление пустоты в начале файла  - добавлен rdofile.cpp  - File::splitpath перестал быть inline
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2928 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_convertor/rdo_common/rdofile.h
+++ rdo/rdo_studio/branches/dev_convertor/rdo_common/rdofile.h
@@ -22,10 +22,13 @@ OPEN_RDO_NAMESPACE
 class File
 {
 public:
-	static rbool exist    (CREF(tstring) name);
-	static rbool read_only(CREF(tstring) name);
-	static rbool unlink   (CREF(tstring) name);
-	static rbool splitpath(CREF(tstring) name, REF(tstring) fileDir, REF(tstring) fileName, REF(tstring) fileExt);
+	static rbool   exist          (CREF(tstring) name);
+	static rbool   read_only      (CREF(tstring) name);
+	static rbool   unlink         (CREF(tstring) name);
+	static rbool   splitpath      (CREF(tstring) name, REF(tstring) fileDir, REF(tstring) fileName, REF(tstring) fileExt);
+
+	static tstring getTempFileName();
+	static rbool   trimLeft       (CREF(tstring) name);
 };
 
 CLOSE_RDO_NAMESPACE
