574068c8c8407c2a3f7fc519bd22c9a077ee5604
Andrey Urusov <drobus@gmail.com>
2010-12-04 10:10:41 +0000

    * revert changes from rev.2977
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@2992 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/context/pattern.cpp
@@ -0,0 +1,33 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : pattern.cpp
+ * author   : Урусов Андрей
+ * date     : 16.10.2010
+ * bref     : 
+ * indent   : 4T
+ */
+
+// ====================================================================== PCH
+#include "rdo_lib/rdo_parser/pch.h"
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_lib/rdo_parser/context/pattern.h"
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- ContextPattern
+// ----------------------------------------------------------------------------
+ContextPattern::ContextPattern()
+{
+	m_pLocalVariableList = rdo::Factory<LocalVariableList>::create();
+	ASSERT(m_pLocalVariableList);
+}
+
+LPLocalVariableList ContextPattern::getLocalMemory()
+{
+	return m_pLocalVariableList;
+}
+
+CLOSE_RDO_PARSER_NAMESPACE
---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_parser/context/pattern.h
@@ -0,0 +1,51 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : pattern.h
+ * author   : Урусов Андрей
+ * date     : 16.10.2010
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _CONEXT_PATTERN_H_
+#define _CONEXT_PATTERN_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_lib/rdo_parser/local_variable.h"
+#include "rdo_lib/rdo_parser/context/context.h"
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- IContextPattern
+// ----------------------------------------------------------------------------
+S_INTERFACE(IContextPattern)
+{
+	virtual LPLocalVariableList getLocalMemory() = 0;
+};
+#define DECLARE_IContextPattern           \
+public:                                   \
+	LPLocalVariableList getLocalMemory();
+
+// ----------------------------------------------------------------------------
+// ---------- ContextPattern
+// ----------------------------------------------------------------------------
+CLASS(ContextPattern):
+	    INSTANCE_OF      (Context        )
+	AND IMPLEMENTATION_OF(IContextPattern)
+{
+DECLARE_FACTORY(ContextPattern);
+private:
+	ContextPattern();
+
+	LPLocalVariableList m_pLocalVariableList;
+
+	DECLARE_IContextPattern;
+};
+DECLARE_POINTER(ContextPattern);
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif //! _CONEXT_PATTERN_H_
