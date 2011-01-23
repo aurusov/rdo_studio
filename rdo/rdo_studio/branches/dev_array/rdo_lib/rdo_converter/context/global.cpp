8d8da7ae2daf5e795bfece7687db1c10b4480273
Andrey Urusov <drobus@gmail.com>
2011-01-23 11:49:14 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3211 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_converter/context/global.cpp
@@ -0,0 +1,64 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : global.cpp
+ * author   : Урусов Андрей
+ * date     : 06.06.2010
+ * bref     : 
+ * indent   : 4T
+ */
+
+// ====================================================================== PCH
+#include "rdo_lib/rdo_converter/pch.h"
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_lib/rdo_converter/context/global.h"
+#include "rdo_lib/rdo_converter/context/timenow.h"
+// ===============================================================================
+
+OPEN_RDO_CONVERTER_NAMESPACE
+
+ContextGlobal::ContextGlobal()
+{
+	m_timeNow = rdo::Factory<ContextTimeNow>::create();
+	ASSERT(m_timeNow);
+}
+
+ContextGlobal::~ContextGlobal()
+{}
+
+LPContext ContextGlobal::parser(PTR(Converter) pParser, CREF(RDOValue) value)
+{
+	ASSERT(value.typeID() == rdoRuntime::RDOType::t_identificator);
+
+	if (value->getIdentificator() == _T("Time_now") || value->getIdentificator() == _T("time_now") || value->getIdentificator() == _T("Системное_время") || value->getIdentificator() == _T("системное_время"))
+	{
+		return m_timeNow;
+	}
+
+	NEVER_REACH_HERE;
+
+	return LPContext();
+
+	//else if ( value->getIdentificator() == "Terminate_counter" || value->getIdentificator() == "terminate_counter" )
+	//{
+	//	m_value = rdo::Factory<RDOType__int>::create().cast<RDOType>();
+	//	m_calc = new rdoRuntime::RDOCalcGetTermNow( converter()->runtime() );
+	//	m_calc->setSrcInfo( src_info() );
+	//	return;
+	//}
+	//else if ( value->getIdentificator() == "Seconds" || value->getIdentificator() == "seconds" )
+	//{
+	//	m_value = rdo::Factory<RDOType__real>::create().cast<RDOType>();
+	//	m_calc = new rdoRuntime::RDOCalcGetSeconds( converter()->runtime() );
+	//	m_calc->setSrcInfo( src_info() );
+	//	return;
+	//}
+}
+
+rdoRuntime::LPRDOCalc ContextGlobal::getCalc()
+{
+	NEVER_REACH_HERE;
+	return rdoRuntime::LPRDOCalc();
+}
+
+CLOSE_RDO_CONVERTER_NAMESPACE
---
+++ rdo/rdo_studio/branches/dev_array/rdo_lib/rdo_converter/rdo_common/model_objects_convertor.h
@@ -0,0 +1,92 @@
+/*
+ * copyright: (c) RDO-Team, 2009
+ * filename : model_objects_convertor.h
+ * author   : Александ Барс, Урусов Андрей
+ * date     : 13.06.2009
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _RDOCOMMON_MODEL_OBJECTS_CONVERTOR_H_
+#define _RDOCOMMON_MODEL_OBJECTS_CONVERTOR_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+// ===============================================================================
+
+#define OPEN_RDO_MODEL_OBJECTS_CONVERTOR_NAMESPACE	namespace rdoModelObjectsConvertor {
+#define CLOSE_RDO_MODEL_OBJECTS_CONVERTOR_NAMESPACE	};
+
+OPEN_RDO_MODEL_OBJECTS_CONVERTOR_NAMESPACE
+	struct RDOSMRFileInfo
+	{
+		tstring  m_model_name;
+		tstring  m_resource_file;
+		tstring  m_oprIev_file;
+		tstring  m_frame_file;
+		tstring  m_statistic_file;
+		tstring  m_results_file;
+		tstring  m_trace_file;
+
+		RDOSMRFileInfo()
+			: m_model_name    (_T(""))
+			, m_resource_file (_T(""))
+			, m_oprIev_file   (_T(""))
+			, m_frame_file    (_T(""))
+			, m_statistic_file(_T(""))
+			, m_results_file  (_T(""))
+			, m_trace_file    (_T(""))
+		{}
+	};
+
+	enum RDOFileTypeIn
+	{
+		UNDEFINED_IN = 0,
+		PAT_IN,
+		RTP_IN,
+		RSS_IN,
+		OPR_IN,
+		FRM_IN,
+		FUN_IN,
+		DPT_IN,
+		SMR_IN,
+		PMD_IN,
+		PMV_IN,
+		TRC_IN 
+	};
+	enum RDOFileTypeOut
+	{
+		UNDEFINED_OUT = 0,
+		PAT_OUT,
+		RTP_OUT,
+		RSS_OUT,
+		FRM_OUT,
+		FUN_OUT,
+		DPT_OUT,
+		SMR_OUT,
+		PMD_OUT,
+		PMV_OUT,
+		TRC_OUT,
+		EVN_OUT,
+		PRC_OUT
+	};
+
+	enum RDOParseType
+	{
+		obNONE = 0x0000,
+		obPRE  = 0x0001,
+		obRTP  = 0x0002,
+		obRSS  = 0x0004,
+		obFUN  = 0x0008,
+		obPAT  = 0x0010,
+		obOPR  = 0x0020,
+		obDPT  = 0x0040,
+		obPMD  = 0x0080,
+		obFRM  = 0x0100,
+		obSMR  = 0x0200,
+		obPOST = 0x0400,
+		obALL  = 0x07FF
+	};
+CLOSE_RDO_MODEL_OBJECTS_CONVERTOR_NAMESPACE
+
+#endif //! _RDOCOMMON_MODEL_OBJECTS_CONVERTOR_H_
