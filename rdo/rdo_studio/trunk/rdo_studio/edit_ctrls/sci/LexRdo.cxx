f3a5ed9172039a45a786f485e669e8a6cd7637ca
Dmitry Luschan <dluschan@gmail.com>
2011-02-21 02:20:15 +0000

    - $Process теперь тоже сворачивается и разворачивается в редакторе до $End
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3273 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_studio/edit_ctrls/sci/LexRdo.cxx
+++ rdo/rdo_studio/trunk/rdo_studio/edit_ctrls/sci/LexRdo.cxx
@@ -127,8 +127,8 @@ static void FoldRdoDoc( unsigned int startPos, int length, int initStyle, WordLi
 		} else {
 			if ( str.length() ) {
 				if (
-					str == "$Pattern" || str == "$Resource_type" || str == "$Resources" || str == "$Operations" || str == "$Frame" || str == "$Constant" || str == "$Sequence"  || str == "$Function" || str == "$Decision_point" || str == "$Results" ||
-					str == "$pattern" || str == "$resource_type" || str == "$resources" || str == "$operations" || str == "$frame" || str == "$constant" || str == "$sequence"  || str == "$function" || str == "$decision_point" || str == "$results" )
+					str == "$Pattern" || str == "$Resource_type" || str == "$Resources" || str == "$Operations" || str == "$Frame" || str == "$Constant" || str == "$Sequence"  || str == "$Function" || str == "$Decision_point" || str == "$Process" || str == "$Results" ||
+					str == "$pattern" || str == "$resource_type" || str == "$resources" || str == "$operations" || str == "$frame" || str == "$constant" || str == "$sequence"  || str == "$function" || str == "$decision_point" || str == "$Process" || str == "$results" )
 				{
 					levelCurrent++;
 				} else if ( str == "$End" ) {
