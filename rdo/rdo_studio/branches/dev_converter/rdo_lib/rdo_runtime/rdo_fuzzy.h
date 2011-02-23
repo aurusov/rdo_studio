c6497246c182457f7070ae44a392433657d9947c
Andrey Urusov <drobus@gmail.com>
2011-02-23 17:24:36 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3314 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_converter/rdo_lib/rdo_runtime/rdo_fuzzy.h
+++
@@ -1,8 +0,0 @@
-#ifndef RDO_FUZZY_H
-#define RDO_FUZZY_H
-
-#include "rdo_lib/rdo_runtime/rdo_value.h"
-#include "rdo_lib/rdo_runtime/rdo_fuzzy_def.h"
-#include "rdo_lib/rdo_runtime/rdo_fuzzy.inl"
-
-#endif // RDO_FUZZY_H
