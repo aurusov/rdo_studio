f286c4fa58ef40cc3c3199740e26bf906a866c59
Andrey Urusov <drobus@gmail.com>
2011-02-23 11:44:26 +0000

    - надо увеличивать !!!
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3285 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_studio/resource.h
+++ rdo/rdo_studio/trunk/rdo_studio/resource.h
@@ -688,7 +688,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        234
+#define _APS_NEXT_RESOURCE_VALUE        252
 #define _APS_NEXT_COMMAND_VALUE         33293
 #define _APS_NEXT_CONTROL_VALUE         1134
 #define _APS_NEXT_SYMED_VALUE           101
