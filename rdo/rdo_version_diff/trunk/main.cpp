3e356404c98d7cc540bc2aa3cc3a390e74f42be1
Andrey Urusov <drobus@gmail.com>
2011-02-26 18:24:43 +0000

    - import
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3386 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_version_diff/trunk/main.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+typedef std::string  tstring;
+typedef unsigned int ruint;
+
+void main(int paramCount, char* params[])
+{
+	//if (paramCount != 2)
+	//{
+	//	std::cerr << "using version.exe <rev.number>" << std::endl;
+	//	return;
+	//}
+
+	//std::stringstream revStr;
+	//revStr << params[1];
+	//ruint rev;
+	//revStr >> rev;
+	//const tstring versionFileName = "C:/rdo/rdo_studio/rdo_studio/res/version.h";
+
+	ruint rev;
+	std::cin >> rev;
+	const tstring versionFileName = "../rdo_studio/res/version.h";
+
+	std::ifstream ifile(versionFileName.c_str());
+	if (ifile.is_open())
+	{
+		std::stringstream revStr;
+		revStr << rev;
+		std::stringstream ifileStr;
+		while (true)
+		{
+			tstring str;
+			ifile >> str;
+			if (str.find(revStr.str()) != tstring::npos)
+			{
+				return;
+			}
+			if (ifile.eof())
+			{
+				break;
+			}
+		}
+	}
+
+	std::ofstream ofile;
+	ofile.open(versionFileName.c_str());
+	ofile << "#define FILEVER        1,1,0," << rev << std::endl;
+	ofile << "#define PRODUCTVER     FILEVER" << std::endl;
+	ofile << "#define STRCONVERT(x)  #x" << std::endl;
+	ofile << "#define STR(x)         STRCONVERT(x)" << std::endl;
+	ofile.close();
+}
