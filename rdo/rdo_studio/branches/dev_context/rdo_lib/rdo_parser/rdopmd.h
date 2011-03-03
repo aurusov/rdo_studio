740359bb528b166e8a3267a2756cb1668861d394
Andrey Urusov <drobus@gmail.com>
2011-03-03 14:15:03 +0000

    - бранч контекста
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3459 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_context/rdo_lib/rdo_parser/rdopmd.h
@@ -0,0 +1,160 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : rdopmd.h
+ * author   : Александ Барс, Урусов Андрей
+ * date     : 
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _RDOPMD_PMD_H_
+#define _RDOPMD_PMD_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_common/smart_ptr/intrusive_ptr.h"
+#include "rdo_lib/rdo_runtime/rdopokaz.h"
+#include "rdo_lib/rdo_parser/rdo_object.h"
+#include "rdo_lib/rdo_parser/rdofun.h"
+#include "rdo_lib/rdo_parser/context/context.h"
+// ===============================================================================
+
+OPEN_RDO_PARSER_NAMESPACE
+
+int  pmdparse(PTR(void) lexer);
+int  pmdlex  (PTR(YYSTYPE) lpval, PTR(YYLTYPE) llocp, PTR(void) lexer);
+void pmderror(PTR(char) mes);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDPokaz
+// ----------------------------------------------------------------------------
+OBJECT(RDOPMDPokaz) IS INSTANCE_OF(RDOParserSrcInfo)
+{
+DECLARE_FACTORY(RDOPMDPokaz);
+public:
+	CREF(tstring) name() const { return src_text(); }
+
+protected:
+	RDOPMDPokaz(CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOPMDPokaz();
+
+	void endOfCreation(CREF(LPIPokaz) pPokaz);
+
+	LPIPokaz m_pPokaz;
+};
+
+// ----------------------------------------------------------------------------
+// ---------- RDOResultGroup
+// ----------------------------------------------------------------------------
+CLASS(RDOResultGroup):
+	    INSTANCE_OF(RDOParserSrcInfo)
+	AND INSTANCE_OF(Context         )
+{
+DECLARE_FACTORY(RDOResultGroup);
+public:
+	CREF(tstring) name  () const;
+	void          append(CREF(LPRDOPMDPokaz) pResult   );
+	LPRDOPMDPokaz find  (CREF(tstring)       resultName) const;
+
+private:
+	RDOResultGroup(CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOResultGroup();
+
+	typedef std::list<LPRDOPMDPokaz> ResultList;
+
+	ResultList m_resultList;
+};
+DECLARE_POINTER(RDOResultGroup);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDWatchPar
+// ----------------------------------------------------------------------------
+class RDOPMDWatchPar: public RDOPMDPokaz
+{
+DECLARE_FACTORY(RDOPMDWatchPar);
+public:
+	void init(rbool trace, CREF(RDOParserSrcInfo) res_src_info, CREF(RDOParserSrcInfo) par_src_info);
+
+private:
+	RDOPMDWatchPar(CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOPMDWatchPar();
+};
+DECLARE_POINTER(RDOPMDWatchPar);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDWatchState
+// ----------------------------------------------------------------------------
+class RDOPMDWatchState: public RDOPMDPokaz
+{
+DECLARE_FACTORY(RDOPMDWatchState);
+public:
+	void init(rbool trace, LPRDOFUNLogic pLogic);
+
+private:
+	RDOPMDWatchState(CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOPMDWatchState();
+};
+DECLARE_POINTER(RDOPMDWatchState);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDWatchTemp
+// ----------------------------------------------------------------------------
+class RDOPMDWatchTemp: public RDOPMDPokaz
+{
+protected:
+	RDOPMDWatchTemp(CREF(RDOParserSrcInfo) src_info, CREF(RDOParserSrcInfo) res_type_src_info);
+	virtual ~RDOPMDWatchTemp();
+};
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDWatchQuant
+// ----------------------------------------------------------------------------
+class RDOPMDWatchQuant: public RDOPMDWatchTemp
+{
+DECLARE_FACTORY(RDOPMDWatchQuant);
+public:
+	void init           (rbool trace, CREF(RDOParserSrcInfo) res_type_src_info);
+	void setLogic       (REF(LPRDOFUNLogic) pLogic);
+	void setLogicNoCheck();
+
+private:
+	RDOPMDWatchQuant(CREF(RDOParserSrcInfo) src_info, CREF(RDOParserSrcInfo) res_type_src_info);
+	virtual ~RDOPMDWatchQuant();
+};
+DECLARE_POINTER(RDOPMDWatchQuant);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDWatchValue
+// ----------------------------------------------------------------------------
+class RDOPMDWatchValue: public RDOPMDWatchTemp
+{
+DECLARE_FACTORY(RDOPMDWatchValue);
+public:
+	void init           (rbool trace, CREF(RDOParserSrcInfo) res_type_src_info);
+	void setLogic       (REF(LPRDOFUNLogic) pLogic, REF(LPRDOFUNArithm) pArithm);
+	void setLogicNoCheck(REF(LPRDOFUNArithm) pArithm);
+
+private:
+	RDOPMDWatchValue(CREF(RDOParserSrcInfo) src_info, CREF(RDOParserSrcInfo) res_type_src_info);
+	virtual ~RDOPMDWatchValue();
+};
+DECLARE_POINTER(RDOPMDWatchValue);
+
+// ----------------------------------------------------------------------------
+// ---------- RDOPMDGetValue
+// ----------------------------------------------------------------------------
+class RDOPMDGetValue: public RDOPMDPokaz
+{
+DECLARE_FACTORY(RDOPMDGetValue);
+public:
+	void init(LPRDOFUNArithm pArithm);
+
+private:
+	RDOPMDGetValue(CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOPMDGetValue();
+};
+DECLARE_POINTER(RDOPMDGetValue);
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif //! _RDOPMD_PMD_H_
---
+++ rdo/rdo_studio/branches/dev_context/rdo_studio/rdo_tracer/rdotracer.cpp
@@ -0,0 +1,108 @@
+#include "rdo_studio/stdafx.h"
+#include "rdo_studio/rdo_tracer/rdotracer.h"
+#include "rdo_studio/rdostudiomodel.h"
+#include "rdo_studio/rdostudiomainfrm.h"
+#include "rdo_studio/rdostudioapp.h"
+#include "rdo_studio/rdostudiothread.h"
+#include "rdo_kernel/rdokernel.h"
+#include "rdo_repository/rdorepository.h"
+#include "rdo_lib/rdo_simulator/rdosimwin.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+
+using namespace rdoRepository;
+using namespace rdoSimulator;
+using namespace rdoTracer;
+
+RDOTracer* tracer = NULL;
+
+static bool clear_after_stop = false;
+
+// ----------------------------------------------------------------------------
+// ---------- RDOTracer
+// ----------------------------------------------------------------------------
+RDOTracer::RDOTracer(): RDOTracerBase( "RDOStudioTracerGUI", static_cast<RDOKernelGUI*>(studioApp.studioGUI) )
+{
+	clear_after_stop = false;
+
+	tracer = this;
+
+	notifies.push_back( RT_REPOSITORY_MODEL_CLOSE );
+	notifies.push_back( RT_SIMULATOR_MODEL_STOP_OK );
+	notifies.push_back( RT_SIMULATOR_MODEL_STOP_BY_USER );
+	notifies.push_back( RT_SIMULATOR_MODEL_STOP_RUNTIME_ERROR );
+	notifies.push_back( RT_RUNTIME_MODEL_START_BEFORE );
+	notifies.push_back( RT_RUNTIME_MODEL_START_AFTER );
+	notifies.push_back( RT_RUNTIME_TRACE_STRING );
+
+	after_constructor();
+}
+
+RDOTracer::~RDOTracer()
+{
+	tracer = NULL;
+}
+
+void RDOTracer::proc( RDOThread::RDOMessageInfo& msg )
+{
+	switch ( msg.message ) {
+		case RDOThread::RT_REPOSITORY_MODEL_CLOSE: {
+			clear_after_stop = model->isRunning();
+			if ( !clear_after_stop ) {
+				clear();
+			}
+			break;
+		}
+		case RDOThread::RT_RUNTIME_MODEL_START_BEFORE: {
+			clear();
+			try {
+				setModelName( model->getName() );
+				studioApp.mainFrame->output.appendStringToDebug( rdo::format( IDS_TRACER_GETTING_MODEL_STRUCTURE ) );
+				rdo::textstream model_structure;
+				sendMessage( kernel->simulator(), RT_SIMULATOR_GET_MODEL_STRUCTURE, &model_structure );
+				getModelStructure( model_structure );
+				studioApp.mainFrame->output.appendStringToDebug( rdo::format( IDS_MODEL_RESOURCE_LOADING_NAME_OK ) );
+			} catch ( ... ) {
+				studioApp.mainFrame->output.appendStringToDebug( rdo::format( IDS_MODEL_RESOURCE_LOADING_NAME_FAILED ) );
+			}
+			break;
+		}
+		case RDOThread::RT_RUNTIME_MODEL_START_AFTER: {
+			rdoRuntime::RunTimeMode runtimeMode;
+			sendMessage( kernel->runtime(), RT_RUNTIME_GET_MODE, &runtimeMode );
+			setRuntimeMode( runtimeMode );
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_MODEL_STOP_OK           :
+		case RDOThread::RT_SIMULATOR_MODEL_STOP_BY_USER      :
+		case RDOThread::RT_SIMULATOR_MODEL_STOP_RUNTIME_ERROR: {
+			if ( clear_after_stop ) {
+				clear();
+				clear_after_stop = false;
+			}
+			setDrawTrace( true );
+			break;
+		}
+		case RDOThread::RT_RUNTIME_TRACE_STRING: {
+			msg.lock();
+			plugins->traceProc( *static_cast<std::string*>(msg.param) );
+			getTraceString( *static_cast<std::string*>(msg.param) );
+			msg.unlock();
+			break;
+		}
+	}
+}
+
+void RDOTracer::setRuntimeMode( const rdoRuntime::RunTimeMode value )
+{
+	if ( value == rdoRuntime::RTM_MaxSpeed ) {
+		setDrawTrace( false );
+	} else {
+		setDrawTrace( true );
+	}
+}
