90bf3d9d641d6d3c55db4ecf1a623b349f03998e
Andrey Urusov <drobus@gmail.com>
2011-03-15 06:06:35 +0000

    - добавлен caller
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3610 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_caller/fun/main.cpp
+++ rdo/rdo_caller/fun/main.cpp
@@ -80,6 +80,39 @@ private:
 	}
 };
 
+class Sender
+{
+public:
+	template <class T, class F>
+	void dispatch(T* pObject, F pFunction)
+	{
+		CallerAdapter0<T, F>* pAdapter = new CallerAdapter0<T, F>(pObject, pFunction);
+		m_callerList.push_back(pAdapter);
+	}
+
+	template <class T, class F, class P1, class P2>
+	void dispatch(T* pObject, F pFunction, const P1& p1, const P2& p2)
+	{
+		CallerAdapter2<T, F, P1, P2>* pAdapter = new CallerAdapter2<T, F, P1, P2>(pObject, pFunction, p1, p2);
+		m_callerList.push_back(pAdapter);
+	}
+
+	void call()
+	{
+		CallerList::const_iterator it = m_callerList.begin();
+		while (it != m_callerList.end())
+		{
+			(*it)->call();
+			delete *it;
+			++it;
+		}
+	}
+
+private:
+	typedef std::list<LPICaller> CallerList;
+	CallerList m_callerList;
+};
+
 void main()
 {
 	A a;
@@ -87,17 +120,10 @@ void main()
 	B b;
 	CallerAdapter2<B, BFun, double, int> adapter2(&b, &B::fun2, 10, 20);
 
-	typedef std::list<LPICaller> CallerList;
-	CallerList callerList;
-	callerList.push_back(&adapter);
-	callerList.push_back(&adapter2);
-
-	CallerList::const_iterator it = callerList.begin();
-	while (it != callerList.end())
-	{
-		(*it)->call();
-		++it;
-	}
+	Sender sender;
+	sender.dispatch(&a, &A::fun);
+	sender.dispatch(&b, &B::fun2, 10, 20);
+	sender.call();
 
 	int i = 1;
 }
