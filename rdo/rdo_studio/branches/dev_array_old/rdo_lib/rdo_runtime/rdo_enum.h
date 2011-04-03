0ba734b5cf4fb8ff31eec54c1e128f93311c8c52
Andrey Urusov <drobus@gmail.com>
2011-04-03 16:41:21 +0000

    - старая версия
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3769 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_array_old/rdo_lib/rdo_runtime/rdo_enum.h
@@ -0,0 +1,56 @@
+/*
+ * copyright: (c) RDO-Team, 2009
+ * filename : rdo_enum.h
+ * author   : Урусов Андрей
+ * date     : 
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _RDO_ENUM_H_
+#define _RDO_ENUM_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_common/rdocommon.h"
+#include "rdo_lib/rdo_runtime/rdo_type.h"
+// ===============================================================================
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- RDOEnumType
+// ----------------------------------------------------------------------------
+class RDOEnumType: public RDOType
+{
+DECLARE_FACTORY(RDOEnumType);
+public:
+	typedef tstring                EnumItem;
+	typedef rdo::vector<EnumItem>  Enums;
+	typedef Enums::const_iterator  CIterator;
+	typedef Enums::const_iterator  const_iterator;
+
+	enum { END = ~0 };
+
+	void  add     (CREF(tstring) next);
+	ruint findEnum(CREF(tstring) val) const;
+	rbool exist   (CREF(tstring) val) const;
+
+	rbool           empty    () const;
+	const CIterator begin    () const;
+	const CIterator end      () const;
+	CREF(Enums)     getValues() const;
+
+private:
+	RDOEnumType();
+	RDOEnumType(CREF(Enums) enums);
+
+	Enums m_enum;
+};
+DECLARE_POINTER(RDOEnumType);
+
+CLOSE_RDO_RUNTIME_NAMESPACE
+
+#include "rdo_lib/rdo_runtime/rdo_enum.inl"
+
+#endif //! _RDO_ENUM_H_
