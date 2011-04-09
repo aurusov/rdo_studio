0cf21b2729b3aaeccb6ab5f8826ac9d05044168a
Andrey Urusov <drobus@gmail.com>
2011-04-09 14:10:11 +0000

    - атомарные типы вынесены в отдельный модуль
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3794 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_parser/type/type.h
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_parser/type/type.h
@@ -88,33 +88,6 @@ protected:
 	rdoRuntime::LPRDOType m_pType;
 };
 
-// ----------------------------------------------------------------------------
-// ---------- ATOM_TYPE_PARSER
-// ----------------------------------------------------------------------------
-#define DEFINE_ATOM_TYPE_PARSER(Type)    \
-class RDOType__##Type: public RDOType    \
-{                                        \
-private:                                 \
-typedef RDOType parent_type;             \
-public:                                  \
-	RDOType__##Type():                   \
-		RDOType(rdoRuntime::g_##Type)    \
-	{}                                   \
-	~RDOType__##Type()                   \
-	{}                                   \
-	DECLARE_IType;                       \
-	DECLARE_IModelStructure;             \
-};                                       \
-DECLARE_POINTER(RDOType__##Type);
-
-DEFINE_ATOM_TYPE_PARSER(unknow       );
-DEFINE_ATOM_TYPE_PARSER(identificator);
-DEFINE_ATOM_TYPE_PARSER(int          );
-DEFINE_ATOM_TYPE_PARSER(real         );
-DEFINE_ATOM_TYPE_PARSER(bool         );
-DEFINE_ATOM_TYPE_PARSER(string       );
-DEFINE_ATOM_TYPE_PARSER(void         );
-
 CLOSE_RDO_PARSER_NAMESPACE
 
 #endif //! _RDOPARSER_TYPE_H_
