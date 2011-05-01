fbba3a2f6fa5e2c9e0d9270a1a0fb18323bf3534
Andrey Urusov <drobus@gmail.com>
2011-05-01 18:37:54 +0000

    - после переименования
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@3960 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_lib/rdo_simulator/procgui.cpp
+++ rdo/rdo_studio/trunk/rdo_lib/rdo_simulator/procgui.cpp
@@ -54,12 +54,12 @@ void ProcGUIBlock::Create(std::vector <double>  pParams)
 		{
 			m_pParser->error().error(rdoParse::RDOParserSrcInfo(), rdo::format(_T("Ошибка создания типа ресурса: %s"), rtp_name.c_str()));
 		}
-		rdoRuntime::RDOPROCTransact::typeID = rtp.id();
+		rdoRuntime::RDOPROCTransact::s_typeID = rtp.id();
 	}
 	else
 	{
 		CREF(rdoMBuilder::RDOResType) rtp = rtpList[rtp_name];
-		rdoRuntime::RDOPROCTransact::typeID = rtp.id();
+		rdoRuntime::RDOPROCTransact::s_typeID = rtp.id();
 	}
 
 	//! GENERATE
