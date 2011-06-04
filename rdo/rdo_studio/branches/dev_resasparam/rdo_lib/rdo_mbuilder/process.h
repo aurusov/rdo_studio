64270bbee2955a3265c013046ccc339bc3745467
Andrey Urusov <drobus@gmail.com>
2011-06-04 12:08:20 +0000

    - бранч для передачи ресурсов в качестве параметров
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4077 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/branches/dev_resasparam/rdo_lib/rdo_mbuilder/process.h
@@ -0,0 +1,46 @@
+/*
+ * copyright: (c) RDO-Team, 2010
+ * filename : process.h
+ * author   : Урусов Андрей
+ * date     : 
+ * bref     : 
+ * indent   : 4T
+ */
+
+#ifndef _MBUILDER_PROCESS_H_
+#define _MBUILDER_PROCESS_H_
+
+// ====================================================================== INCLUDES
+// ====================================================================== SYNOPSIS
+#include "rdo_lib/rdo_mbuilder/namespace.h"
+#include "rdo_lib/rdo_mbuilder/rdo_resources.h"
+// ===============================================================================
+
+OPEN_MBUILDER_NAMESPACE
+
+// ----------------------------------------------------------------------------
+// ---------- BlockForQueue
+// ----------------------------------------------------------------------------
+class BlockForQueue
+{
+public:
+	static rbool      checkType (RDOResType rtp,         CREF(rdoParse::RDOParserSrcInfo) info    );
+	static void       createRes (RDOResType rtp,         CREF(tstring)                    res_name);
+	static RDOResType createType(CREF(tstring) rtp_name, CREF(rdoParse::RDOParserSrcInfo) info    );
+};
+
+// ----------------------------------------------------------------------------
+// ---------- BlockForSeize
+// ----------------------------------------------------------------------------
+class BlockForSeize
+{
+public:
+	static rbool      checkType  (RDOResType    rtp,      CREF(rdoParse::RDOParserSrcInfo) info    );
+	static void       createRes  (RDOResType    rtp,      CREF(tstring)                    res_name);
+	static void       reobjectRes(RDOResType    rtp,      CREF(tstring)                    res_name);
+	static RDOResType createType (CREF(tstring) rtp_name, CREF(rdoParse::RDOParserSrcInfo) info    );
+};
+
+CLOSE_MBUILDER_NAMESPACE
+
+#endif //! _MBUILDER_PROCESS_H_
