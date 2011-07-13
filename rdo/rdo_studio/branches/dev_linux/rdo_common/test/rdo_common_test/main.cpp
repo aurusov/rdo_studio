7f3c3ee9e39c9a822418462f64d5bb2da2ddbdc2
Andrey Urusov <drobus@gmail.com>
2011-07-13 18:41:38 +0000

    - библиотечные инклюды располагаются в INCLUDES  - инклюды проекта располагаются SYNOPSIS  - инклюды проекта указываются от корня сорсов
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4315 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_linux/rdo_common/test/rdo_common_test/main.cpp
+++ rdo/rdo_studio/branches/dev_linux/rdo_common/test/rdo_common_test/main.cpp
@@ -11,17 +11,15 @@
 // ====================================================================== PCH
 // ====================================================================== INCLUDES
 #include <boost/regex.hpp>
+#define BOOST_TEST_MODULE test_rdo_common
+#include <boost/test/included/unit_test.hpp>
 // ====================================================================== SYNOPSIS
 #include "rdo_common/rdocommon.h"
 #include "rdo_common/rdofile.h"
 #include "rdo_common/rdotime.h"
 #include "rdo_common/test/rdo_common_test/resource.h"
-
-#include <config-tiles.h>
+#include "rdo_common/test/rdo_common_test/config-tiles.h"
 // ===============================================================================
-
-#define BOOST_TEST_MODULE test_rdo_common
-#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_rdo_common)
 
