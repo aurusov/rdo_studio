e5a9363cb73cf5b931f1e31ada14185fad88bd3b
Evgeny Proydakov <lord.tiran@gmail.com>
2011-08-06 16:08:50 +0000

    - updated rdodebug.h cross platform TRACE and ASSERT
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4428 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_linux/rdo_common/rdodebug.h
+++ rdo/rdo_studio/branches/dev_linux/rdo_common/rdodebug.h
@@ -11,10 +11,15 @@
 #define _RDODEBUG_H_
 
 // ====================================================================== INCLUDES
+#ifdef WIN32
 #ifdef RDO_MT
 #include <afxwin.h>
 #else
 #include <windows.h>
+#endif
+#else
+#include <iostream>
+#include <assert.h>
 #endif
 // ====================================================================== SYNOPSIS
 #include "rdo_common/rdocommon.h"
@@ -36,10 +41,11 @@
 #undef ASSERT
 #endif
 
-
 #ifdef _DEBUG
 
-#define  TRACE(A)              ::OutputDebugString(A);
+#ifdef WIN32
+
+#define TRACE(A)               ::OutputDebugString(A);
 #define TRACE1(A, P1)          ::OutputDebugString(rdo::format(A, P1).c_str());
 #define TRACE2(A, P1, P2)      ::OutputDebugString(rdo::format(A, P1, P2).c_str());
 #define TRACE3(A, P1, P2, P3)  ::OutputDebugString(rdo::format(A, P1, P2, P3).c_str());
@@ -52,6 +58,21 @@
 	}
 
 #define NEVER_REACH_HERE ASSERT(false)
+
+#else // WIN32
+
+void NewOutputDebugString(CREF(tstring) A)
+{
+	std::cout << A;
+}
+
+#define TRACE(A)				NewOutputDebugString(A);
+#define TRACE1(A, P1)           NewOutputDebugString(rdo::format(A, P1).c_str());
+#define TRACE2(A, P1, P2)       NewOutputDebugString(rdo::format(A, P1, P2).c_str());
+#define TRACE3(A, P1, P2, P3)   NewOutputDebugString(rdo::format(A, P1, P2, P3).c_str());
+#define ASSERT(A)				assert(A);
+
+#endif // WIN32
 
 #else //! _DEBUG
 
