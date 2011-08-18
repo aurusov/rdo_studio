7d6561dd8396d2b6a122fe241bea7677f3054668
Andrey Urusov <drobus@gmail.com>
2011-08-18 16:43:44 +0000

    - добавлен help
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4533 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/trunk/rdo_common/static_assert.h
+++ rdo/rdo_studio/trunk/rdo_common/static_assert.h
@@ -3,12 +3,12 @@
   \file      static_assert.h
   \author    Урусов Андрей (rdo@rk9.bmstu.ru)
   \date      08.03.2010
-  \brief     основан на классе CompileTimeError из Александреску
+  \brief     Основан на классе CompileTimeError из <a href="http://lib.mexmat.ru/books/7498">Александреску</a>
   \indent    4T
 */
 
-#ifndef _STATIC_ASSERT_H_
-#define _STATIC_ASSERT_H_
+#ifndef _LIB_COMMON_STATIC_ASSERT_H_
+#define _LIB_COMMON_STATIC_ASSERT_H_
 
 // ----------------------------------------------------------------------- INCLUDES
 // ----------------------------------------------------------------------- SYNOPSIS
@@ -19,15 +19,25 @@
 
 OPEN_RDO_NAMESPACE
 
+//! \struct rdo::CompileTimeError
+//! \brief   Шаблонный класс для генерации ошибки компиляции
+//! \details Используется через макрос \ref STATIC_ASSERT
+//! \tparam rbool - если \b true, ошибки нет, если \b false - ошибка есть
 template <rbool>
 struct CompileTimeError;
 
+//! Специализация шаблонного класса \ref rdo::CompileTimeError для описание ситуации \b отсутствия ошибки компиляции
 template <>
 struct CompileTimeError<true> {};
 
+//! Выбрасывает ASSERT на этапе компиляции, когда компилятор доходит до этого макроса, т.е. формирует ошибку компиляции. Пример:
+//! \code
+//! STATIC_ASSERT(Some_error);
+//! \endcode
+//! \param expr - текст ошибки, указывается без пробелов
 #define STATIC_ASSERT(expr)                 \
-	rdo::CompileTimeError<((expr) != 0)>(); \
+	rdo::CompileTimeError<((expr) != 0)>();
 
 CLOSE_RDO_NAMESPACE
 
-#endif //! _STATIC_ASSERT_H_
+#endif //! _LIB_COMMON_STATIC_ASSERT_H_
