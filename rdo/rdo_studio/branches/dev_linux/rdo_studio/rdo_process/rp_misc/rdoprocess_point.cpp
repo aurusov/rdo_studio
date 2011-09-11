a89246646f3138070361a03e4019462ad545f688
Andrey Urusov <drobus@gmail.com>
2011-09-11 15:09:20 +0000

    - удаление перед пересозданием
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4806 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_linux/rdo_studio/rdo_process/rp_misc/rdoprocess_point.cpp
+++
@@ -1,8 +0,0 @@
-#include "rdo_studio/rdo_process/rp_misc/stdafx.h"
-#include "rdo_studio/rdo_process/rp_misc/rdoprocess_point.h"
-
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
--- rdo/rdo_studio/branches/dev_linux/rdo_studio/rdo_tracer/tracer_ctrls/rdotracertreeitem.cpp
+++
@@ -1,8 +0,0 @@
-#include "rdo_studio/pch/pch.h"
-#include "rdo_studio/rdo_tracer/tracer_ctrls/rdotracertreeitem.h"
-
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
