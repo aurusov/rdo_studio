48c08929f8b831290abd0feac52d3318bf2faa8b
Andrey Urusov <drobus@gmail.com>
2011-09-11 20:00:06 +0000

    - создание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4891 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_linux/rdopatpreparse.h
@@ -0,0 +1,58 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdopatpreparse.h
+  \author    Ћущан ƒмитрий (dluschan@rk9.bmstu.ru)
+  \date      22.05.10
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPATPREPARSE_H_
+#define _RDOPATPREPARSE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/rdomacros.h"
+#include "utils/rdotypes.h"
+#include "utils/smart_ptr/intrusive_ptr.h"
+#include "simulator/runtime/calc/event_plan.h"
+#include "simulator/runtime/rdo_event.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOEvent
+// --------------------------------------------------------------------------------
+OBJECT(RDOEvent)
+{
+DECLARE_FACTORY(RDOEvent);
+public:
+	typedef std::list<rdoRuntime::LPRDOCalcEvent> CalcList;
+
+	CREF(tstring)          name           () const;
+	void                   attachCalc     (CREF(rdoRuntime::LPRDOCalcEventStop) pCalc);
+	void                   attachCalc     (CREF(rdoRuntime::LPRDOCalcEventPlan) pCalc);
+	REF(CalcList)          getCalcList    ();
+	LPIBaseOperation       getRuntimeEvent() const;
+	void                   setRuntimeEvent(LPIBaseOperation pRuntimeEvent);
+	rbool                  getRegular     () const;
+	rdoRuntime::LPRDOCalc  getInitCalc    () const;
+	void                   setInitCalc    (CREF(rdoRuntime::LPRDOCalc) pCalc);
+
+private:
+	RDOEvent(CREF(tstring) name, rbool regular);
+	virtual ~RDOEvent();
+
+	tstring                m_name;
+	rbool                  m_regullar;
+	CalcList               m_calcList;
+	LPIBaseOperation       m_runtimeEvent;
+	rdoRuntime::LPRDOCalc  m_pInitCalc;
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#include "simulator/compiler/parser/rdopatpreparse.inl"
+
+#endif // _RDOPATPREPARSE_H_
