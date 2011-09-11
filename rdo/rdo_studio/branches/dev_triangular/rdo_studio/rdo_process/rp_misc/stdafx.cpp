849205e1cd1195ecc13720535cc4ad2be387924f
Andrey Urusov <drobus@gmail.com>
2011-09-11 18:31:55 +0000

    - удаление перед пересозданием
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4850 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_triangular/rdo_studio/rdo_process/rp_misc/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	rdo_process.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "rdo_studio/rdo_process/rp_misc/stdafx.h"
