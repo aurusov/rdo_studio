4c3016e7b2347667950315c153e88c95eac41244
Andrey Urusov <drobus@gmail.com>
2011-09-11 20:00:53 +0000

    - создание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4893 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_resasparam/rdo_array.cpp
@@ -0,0 +1,82 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdo_array.cpp
+  \author    Чирков Михаил
+  \date      
+  \brief     
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/compiler/parser/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/rdo_array.h"
+#include "simulator/compiler/parser/rdoparser.h"
+#include "simulator/compiler/parser/rdoparser_error.h"
+#include "simulator/compiler/parser/rdoparser_lexer.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+//----------------------------------------------------------------------------
+//---------- RDOArrayValue
+//----------------------------------------------------------------------------
+RDOArrayValue::RDOArrayValue(CREF(LPRDOArrayType) pArrayType)
+	: m_pArrayType(pArrayType)
+{}
+
+RDOArrayValue::~RDOArrayValue()
+{}
+
+void RDOArrayValue::insertItem(CREF(RDOValue) value)
+{
+	m_pArrayType->getItemType()->type()->type_cast(value.typeInfo()->type(), value.src_info(), m_pArrayType->src_info(), value.src_info());
+	RDOValue ItemValue = m_pArrayType->getItemType()->type()->value_cast(value, m_pArrayType->src_info(), value.src_info());
+	m_container.push_back(ItemValue);
+}
+
+CREF(LPRDOArrayType) RDOArrayValue::getArrayType() const
+{
+	return m_pArrayType;
+}
+
+REF(LPRDOArrayType) RDOArrayValue::getArrayType()
+{
+	return m_pArrayType;
+}
+
+rdoRuntime::RDOValue RDOArrayValue::getRArray() const
+{
+	rdoRuntime::RDOArrayValue arrayValue(m_pArrayType->getRuntimeArrayType());
+	STL_FOR_ALL_CONST(m_container, it)
+	{
+		arrayValue.insertItem(it->value());
+	}
+	rdoRuntime::RDOValue value(arrayValue);
+	return value;
+}
+
+tstring RDOArrayValue::getAsString() const
+{
+	tstring arrayValue;
+	STL_FOR_ALL_CONST(m_container, it)
+	{
+		if (it == m_container.begin())
+		{
+			arrayValue = rdo::format(_T("[%s"), it->value().getAsString().c_str());
+		}
+		else
+		{
+			arrayValue = rdo::format(_T("%s, %s"), arrayValue.c_str(), it->value().getAsString().c_str());
+		}
+	}
+	return rdo::format(_T("%s]"), arrayValue.c_str());
+}
+
+CREF(RDOArrayValue::Container) RDOArrayValue::getContainer() const
+{
+	return m_container;
+}
+
+CLOSE_RDO_PARSER_NAMESPACE
---
+++ rdo/packages/compiler/parser/branches/dev_resasparam/type/type.h
@@ -0,0 +1,93 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      type.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPARSER_TYPE_H_
+#define _RDOPARSER_TYPE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/smart_ptr/intrusive_ptr.h"
+#include "simulator/runtime/rdo_type.h"
+#include "simulator/runtime/rdo_value.h"
+#include "simulator/runtime/rdo_model_i.h"
+#include "simulator/runtime/rdocalc.h"
+#include "simulator/compiler/parser/rdo_object.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- IType
+// --------------------------------------------------------------------------------
+class RDOValue;
+PREDECLARE_POINTER(RDOType);
+
+class IType
+{
+public:
+	virtual tstring   name() const = 0;
+	virtual LPRDOType type_cast(
+		CREF(LPRDOType)        pFrom,
+		CREF(RDOParserSrcInfo) from_src_info,
+		CREF(RDOParserSrcInfo) to_src_info,
+		CREF(RDOParserSrcInfo) src_info
+	) const = 0;
+	virtual RDOValue  value_cast(
+		CREF(RDOValue)         from,
+		CREF(RDOParserSrcInfo) to_src_info,
+		CREF(RDOParserSrcInfo) src_info
+	) const = 0;
+	//! calc_cast вызывается строго после type_cast, поэтому никаких RDOParserSrcInfo не передается
+	virtual rdoRuntime::LPRDOCalc calc_cast(
+		CREF(rdoRuntime::LPRDOCalc) pCalc,
+		CREF(LPRDOType)             pType
+	) const = 0;
+	virtual rdoRuntime::RDOValue get_default() const = 0;
+};
+#define DECLARE_IType                                                                                                                                                                   \
+	virtual tstring                name       () const;                                                                                                                                 \
+	virtual LPRDOType              type_cast  (CREF(LPRDOType) pFrom, CREF(RDOParserSrcInfo) from_src_info, CREF(RDOParserSrcInfo) to_src_info, CREF(RDOParserSrcInfo) src_info) const; \
+	virtual RDOValue               value_cast (CREF(RDOValue)  from, CREF(RDOParserSrcInfo) to_src_info,   CREF(RDOParserSrcInfo) src_info)                                     const;  \
+	virtual rdoRuntime::LPRDOCalc  calc_cast  (CREF(rdoRuntime::LPRDOCalc) pCalc, CREF(LPRDOType) pType) const;                                                                         \
+	virtual rdoRuntime::RDOValue   get_default() const;
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOType
+// --------------------------------------------------------------------------------
+OBJECT(RDOType)
+	IS  IMPLEMENTATION_OF(IType          )
+	AND IMPLEMENTATION_OF(IModelStructure)
+{
+DECLARE_FACTORY(RDOType)
+public:
+	CREF(rdoRuntime::LPRDOType)        type() const { return m_pType;           }
+	CREF(rdoRuntime::LPRDOType) operator-> () const { return m_pType;           }
+
+	rdoRuntime::RDOType::TypeID      typeID() const { return m_pType->typeID(); }
+
+	virtual rdoRuntime::LPRDOCalc calc_cast(CREF(rdoRuntime::LPRDOCalc) pCalc, CREF(LPRDOType) pType) const
+	{
+		return pCalc;
+	}
+
+protected:
+	RDOType(CREF(rdoRuntime::LPRDOType) pType)
+		: m_pType(pType)
+	{
+		ASSERT(m_pType);
+	}
+	virtual ~RDOType()
+	{}
+
+	rdoRuntime::LPRDOType m_pType;
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif // _RDOPARSER_TYPE_H_
