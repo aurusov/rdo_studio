27dc2cdc9ed1b00a37438d2f6498ea2140356c7f
Igor Klevanec <cerevra@gmail.com>
2011-09-12 17:13:02 +0000

    - добавлен солюшн автотеста для треугольного закона
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4915 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_triangular/test/rdo_sequences_triangular_test/main.cpp
@@ -0,0 +1,24 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      main.cpp
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Клеванец Игорь (impus@hotbox.ru)
+  \date      12.09.2011
+  \brief     Тест треугольного закона
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- INCLUDES
+#include "stdafx.h"
+#define BOOST_TEST_MODULE RDOTriangularTest
+#include <boost/test/included/unit_test.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/rdo_random_distribution.h"
+// --------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(RDOTriangularTest)
+{
+
+}
+
