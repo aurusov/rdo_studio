c19f4af534ecc6f69710c846a51b0e6e0a0111a3
Evgeny Proydakov <lord.tiran@gmail.com>
2011-09-14 08:55:40 +0000

    - added copyright in bison.sh scripts
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4937 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_linux/bison.sh
+++ rdo/rdo_studio/branches/dev_linux/bison.sh
@@ -1,3 +1,8 @@
+###############################################################################
+# Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
+###############################################################################
+# bison files generator shell script
+###############################################################################
 
 cd simulator/compiler/parser/
 bison rdortp.y -g -v --defines=rdogramma.h -prtp -ordogramrtp.cpp
