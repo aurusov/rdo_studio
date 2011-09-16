04674a22bfa2639ab45cbae01c6a8d362144022f
Andrey Urusov <drobus@gmail.com>
2011-09-16 09:47:50 +0000

    - переход на Visual Studio 2008  - форматирование  - namespace не открывается
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@4968 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_linux/test/rdo_array_test/main.cpp
+++ rdo/packages/runtime/branches/dev_linux/test/rdo_array_test/main.cpp
@@ -1,15 +1,22 @@
-// ====================================================================== INCLUDES
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      main.cpp
+  \authors   Чирков Михаил
+  \date      13.12.2009
+  \brief     Тест массивов
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- INCLUDES
 #include <iostream>
 #include <stdio.h>
-// ====================================================================== SYNOPSIS
+// ----------------------------------------------------------------------- SYNOPSIS
 #include "simulator/runtime/rdo_value.h"
 #include "simulator/runtime/rdo_array.h"
 #include "simulator/runtime/rdo_type.h"
-// ===============================================================================
-
-using namespace rdoRuntime;
-
 #include "simulator/runtime/test/rdo_array_test/arrayTestFunction.inl"
+// --------------------------------------------------------------------------------
 
 void main()
 {
