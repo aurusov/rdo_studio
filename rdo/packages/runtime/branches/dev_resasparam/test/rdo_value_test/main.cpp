d513dd0f28d1c45a3a80524d8070c99b3ceecc83
Andrey Urusov <drobus@gmail.com>
2011-09-18 19:08:03 +0000

    - создание автотеста на основе ручного
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5013 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_resasparam/test/rdo_value_test/main.cpp
+++ rdo/packages/runtime/branches/dev_resasparam/test/rdo_value_test/main.cpp
@@ -10,16 +10,31 @@
 // ---------------------------------------------------------------------------- PCH
 // ----------------------------------------------------------------------- INCLUDES
 #include <iostream>
+#include <boost/regex.hpp>
+#define BOOST_TEST_MODULE RDOValue_Test
+#include <boost/test/included/unit_test.hpp>
 // ----------------------------------------------------------------------- SYNOPSIS
 #include "simulator/runtime/rdo_value.h"
 // --------------------------------------------------------------------------------
 
-using namespace rdoRuntime;
+BOOST_AUTO_TEST_SUITE(RDOValue_Test)
 
-void main()
+BOOST_AUTO_TEST_CASE(RDOValue_String)
 {
-	RDOValue value1("qqq");
-	RDOValue value2 = value1;
-	value2 += "aaa";
-	int i = 1;
+	const tstring str1 = _T("qqq");
+	rdoRuntime::RDOValue value1(str1);
+	BOOST_CHECK(value1.getString  () == str1);
+	BOOST_CHECK(value1.getAsString() == str1);
+
+	rdoRuntime::RDOValue value2 = value1;
+	BOOST_CHECK(value2.getString  () == str1);
+	BOOST_CHECK(value2.getAsString() == str1);
+	BOOST_CHECK(value2 == value1);
+
+	const tstring str2 = _T("aaa");
+	value2 += str2;
+	BOOST_CHECK(value2.getString  () == str1 + str2);
+	BOOST_CHECK(value2.getAsString() == str1 + str2);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
