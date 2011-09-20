60e73ab00e2fb72ce55fd8b489d737d2aa808118
Igor Klevanec <cerevra@gmail.com>
2011-09-20 13:36:51 +0000

    - автотесты. создается контейнер с данными
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5024 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_triangular/test/rdo_sequences_triangular_test/main.cpp
+++ rdo/packages/runtime/branches/dev_triangular/test/rdo_sequences_triangular_test/main.cpp
@@ -11,11 +11,53 @@
 // ---------------------------------------------------------------------------- PCH
 // ----------------------------------------------------------------------- INCLUDES
 #define BOOST_TEST_MODULE RDOTriangularTest
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <map>
 #include <boost/test/included/unit_test.hpp>
+#include <boost/bind.hpp>
+#include <boost/assign/std/map.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/serialization/map.hpp>
 // ----------------------------------------------------------------------- SYNOPSIS
 #include "simulator/runtime/rdo_random_distribution.h"
 // --------------------------------------------------------------------------------
+using namespace rdoRuntime;
 
 BOOST_AUTO_TEST_CASE(RDOTriangularTest)
 {
+	RandGeneratorTriangular triang(123456789);				//создаю объект, генерящий числа по треугольному закону
+
+	typedef std::map<int, double> Container;				//даю более красивое название контейнеру map
+	Container m_base, m_forTest;							//создаю два контейнера
+
+	std::ofstream  testFile("Test.txt", std::ios::binary);	//открываю файл, котором будут лежать образцовые данные.
+
+	for (int i = 0; i < 100000; i++)						//контейнер забивается числами, нагенеренными тестируемым объектом
+	{
+		std::pair<Container::iterator, bool> result =
+			m_forTest.insert(Container::value_type(i, triang.next(1,5,7)));	//в контейнере значения следуют парами (порядковый номер и сгенеренное число)
+	}
+	
+	
+	
+	//Этот блок планировалось использовать для записи эталонного файла. Уперся в незнание. Понятия не имею, как
+	//записать данные в файл
+/*
+	Container::const_iterator it = m_forTest.begin();
+	while (it != m_forTest.end())
+	{
+	//char aaa[] = std::cout << it->first << ";" << it->second <<std::endl;
+		testFile.write(aaa,sizeof(aaa));
+	}
+*/
+	testFile.close();
+	int i = 1;
 }
+
+
