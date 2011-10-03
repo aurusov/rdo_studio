4381275eaaf058289cc00d6f0fd3455fac64ffc8
Andrey Urusov <drobus@gmail.com>
2011-10-03 19:08:33 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5267 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/utils/branches/dev_linux/rdoindexedstack.h
@@ -0,0 +1,58 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdoindexedstack.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      23.02.2010
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _UTILS_RDOINDEXEDSTACK_H_
+#define _UTILS_RDOINDEXEDSTACK_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+#include <map>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/namespace.h"
+#include "utils/rdomacros.h"
+#include "utils/rdotypes.h"
+#include "utils/rdoidgenerator.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_NAMESPACE
+
+template<class T>
+class IndexedStack
+{
+public:
+	typedef IDGenerator::ID ID;
+
+	IndexedStack()
+	{}
+
+	ID push(CREF(T) object)
+	{
+                std::pair<typename Stack::iterator, rbool> result = m_stack.insert(
+                        std::pair<typename Stack::key_type, typename Stack::mapped_type>(m_generator.get(), object)
+		);
+		ASSERT(result.second);
+		return result.first->first;
+	}
+	T pop(ID index)
+	{
+                typename Stack::iterator it = m_stack.find(index);
+		ASSERT(it != m_stack.end());
+		T object = it->second;
+		m_stack.erase(it);
+		return object;
+	}
+
+protected:
+	typedef std::map<IDGenerator::ID, T> Stack;
+	Stack       m_stack;
+	IDGenerator m_generator;
+};
+
+CLOSE_RDO_NAMESPACE
+
+#endif // _UTILS_RDOINDEXEDSTACK_H_
---
+++ rdo/packages/utils/branches/dev_linux/rdotime.h
@@ -0,0 +1,49 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdotime.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      10.05.2009
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _UTILS_RDOTIME_H_
+#define _UTILS_RDOTIME_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/namespace.h"
+#include "utils/rdotypes.h"
+#include "utils/rdomacros.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_NAMESPACE
+
+class Time
+{
+public:
+	typedef  ruint64  Value;
+
+	Time();
+	Time(CREF(Value) value);
+
+	rbool operator== (CREF(Time) time);
+	operator Value() const;
+
+	tstring asString() const;
+
+	CREF(Value) value() const;
+
+	static Time utc    ();
+	static Time local  ();
+	static Time invalid();
+
+private:
+	Value  m_value;
+};
+
+CLOSE_RDO_NAMESPACE
+
+#include "utils/rdotime.inl"
+
+#endif // _UTILS_RDOTIME_H_
