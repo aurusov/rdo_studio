40a9ad095af0027e04ecc35e239a0b98d388231f
Igor Klevanec <cerevra@gmail.com>
2011-10-12 19:19:48 +0000

    - рефакторинг тестов. Выделены шаблоны в отдельный файл. работает все, кроме теста экспоненциального закона
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5310 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_triangular/test/sequences_test/sequences/main.cpp
@@ -0,0 +1,102 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      main.cpp
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Клеванец Игорь (impus@hotbox.ru)
+  \date      2.10.2011
+  \brief     Тест законов распределения
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- INCLUDES
+#define BOOST_TEST_MODULE RDOSequencesTest
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/rdofile.h"
+#include "simulator/runtime/rdo_random_distribution.h"
+#include "simulator/runtime/test/sequences_test/sequences/templ.cpp"
+// --------------------------------------------------------------------------------
+
+typedef std::list<double> Container;
+typedef const tstring contstr;
+
+const long int g_seed                 = 123456789;                  //!< база генератора
+contstr        g_fileNormalName       = _T("data_normal.txt");      //!< файл данных
+contstr        g_fileUniformName      = _T("data_uniform.txt");     //!< файл данных
+contstr        g_fileExponentialName  = _T("data_exponential.txt"); //!< файл данных
+contstr        g_fileTriangularName   = _T("data_trinagular.txt");  //!< файл данных
+const ruint    g_count                = 100000;                     //!< количество генерируемых данных
+const double   g_main                 = 10.0;                       //!< параметр закона
+const double   g_var                  = 1.0;                        //!< параметр закона
+const double   g_from                 = 1.0;                        //!< параметр закона
+const double   g_to                   = 7.0;                        //!< параметр закона
+const double   g_top                  = 5.0;                        //!< параметр закона
+const ruint    g_precision            = 20;                         //!< точность вещественного числа при выводе в поток
+
+
+
+BOOST_AUTO_TEST_SUITE(RDOSequencesTest)
+
+// --------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(RDONormalTestCreate)
+{
+	onGenerateData<rdoRuntime::RandGeneratorNormal>
+		(boost::bind(&rdoRuntime::RandGeneratorNormal::next, _1, g_main, g_var), g_fileNormalName);
+}
+
+BOOST_AUTO_TEST_CASE(RDONormalTestCheck)
+{
+	onCheckData<rdoRuntime::RandGeneratorNormal>
+		(boost::bind(&rdoRuntime::RandGeneratorNormal::next, _1, g_main, g_var), g_fileNormalName);
+}
+// --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(RDOUniformTestCreate)
+{
+	onGenerateData<rdoRuntime::RandGeneratorUniform>
+		(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
+}
+
+BOOST_AUTO_TEST_CASE(RDOUniformTestCheck)
+{
+	onCheckData<rdoRuntime::RandGeneratorUniform>
+		(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
+}
+// --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(RDOExponentialTestCreate)
+{
+	onGenerateData<rdoRuntime::RandGeneratorExponential>
+		(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
+}
+
+BOOST_AUTO_TEST_CASE(RDOExponentialTestCheck)
+{
+	onCheckData<rdoRuntime::RandGeneratorExponential>
+		(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
+}
+// --------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(RDOTriangularTestCreate)
+{
+	onGenerateData<rdoRuntime::RandGeneratorTriangular>
+		(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
+}
+
+BOOST_AUTO_TEST_CASE(RDOTriangularTestCheck)
+{
+	onCheckData<rdoRuntime::RandGeneratorTriangular>
+		(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
+}
+// --------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END()
