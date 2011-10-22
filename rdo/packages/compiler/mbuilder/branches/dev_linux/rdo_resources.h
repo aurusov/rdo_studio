454162c4499b7995541813cb00f5eb5ffb180036
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-22 19:50:58 +0000

    - reworked #ifndef macro and added include in mbuilder library file
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5374 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/compiler/mbuilder/branches/dev_linux/rdo_resources.h
+++ rdo/packages/compiler/mbuilder/branches/dev_linux/rdo_resources.h
@@ -10,11 +10,15 @@
 #ifndef _MBUILDER_RESOURCES_H_
 #define _MBUILDER_RESOURCES_H_
 
-#if _MSC_VER > 1000
-#pragma once
+// ----------------------------------------------------------------------- PLATFORM
+#include "utils/platform.h"
+// ----------------------------------------------------------------------- INCLUDES
+#ifdef COMPILER_VISUAL_STUDIO
+	#if _MSC_VER > 1000
+		#pragma once
+	#endif
 #endif
 
-// ----------------------------------------------------------------------- INCLUDES
 #include <list>
 // ----------------------------------------------------------------------- SYNOPSIS
 #include "utils/rdodebug.h"
