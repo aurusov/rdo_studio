8801936e2b7947baa8bcdb043cc1b096fa9fda92
Alex Cyferov <pinometr@gmail.com>
2011-10-27 12:07:40 +0000

    - версия №3 для консультации 27.10.11
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5406 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/branches/dev_processgui/rdo_process/rp_method/rdoprocess_object_pixmap.cpp
+++ rdo/app/rdo_studio/branches/dev_processgui/rdo_process/rp_method/rdoprocess_object_pixmap.cpp
@@ -49,7 +49,7 @@ rp::RPXMLNode* RPObjectPixmap::save( rp::RPXMLNode* parent_node )
 
 void RPObjectPixmap::save_To_XML(pugi::xml_document &doc, std::list< RPObject* >& all_child)
 {
-	pugi::xml_node node_new = doc.append_child(getClassName().c_str());
+	//pugi::xml_node node_new = doc.append_child(getClassName().c_str());
 }
 
 void RPObjectPixmap::setPosition( double posx, double posy )
