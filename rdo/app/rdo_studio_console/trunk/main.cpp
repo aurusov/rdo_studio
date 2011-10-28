bc5ca5199a6077386f5dc4496f7a446f19bcb5c4
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-28 10:02:16 +0000

    - fixed code style error in rdo_studio_console
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5421 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio_console/trunk/main.cpp
+++ rdo/app/rdo_studio_console/trunk/main.cpp
@@ -23,17 +23,17 @@ const tstring SYSTEM_OS = _T("Windows");
 const tstring SYSTEM_OS = _T("Linux");
 #endif
 
-const tstring HELP_COMMANDS = _T("help");
+const tstring HELP_COMMANDS     = _T("help");
 const tstring LANGUAGE_COMMANDS = _T("language");
-const tstring VERSION_COMMANDS = _T("version");
+const tstring VERSION_COMMANDS  = _T("version");
 
-const tstring HELP_COMMANDS_SHORT = _T("h");
+const tstring HELP_COMMANDS_SHORT     = _T("h");
 const tstring LANGUAGE_COMMANDS_SHORT = _T("l");
-const tstring VERSION_COMMANDS_SHORT = _T("v");
+const tstring VERSION_COMMANDS_SHORT  = _T("v");
 
-const tstring HELP_COMMENT = _T("display help message");
+const tstring HELP_COMMENT     = _T("display help message");
 const tstring LANGUAGE_COMMENT = _T("display language version of RDO");
-const tstring VERSION_COMMENT = _T("display program version");
+const tstring VERSION_COMMENT  = _T("display program version");
 
 const tstring RDO_STUDIO_CONSOLE_VERSION = _T("0.1.0");
 const tstring RDO_LANGUAGE_VERSION       = _T("1.5");
