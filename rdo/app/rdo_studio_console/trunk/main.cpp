34351d4da717908d2766330175fd1443b5652bc0
Evgeny Proydakov <lord.tiran@gmail.com>
2011-10-28 09:42:34 +0000

    - updated rdo_studio_console : added create description and create_desctiption_message function
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5420 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio_console/trunk/main.cpp
+++ rdo/app/rdo_studio_console/trunk/main.cpp
@@ -23,20 +23,31 @@ const tstring SYSTEM_OS = _T("Windows");
 const tstring SYSTEM_OS = _T("Linux");
 #endif
 
-namespace po = boost::program_options;
+const tstring HELP_COMMANDS = _T("help");
+const tstring LANGUAGE_COMMANDS = _T("language");
+const tstring VERSION_COMMANDS = _T("version");
+
+const tstring HELP_COMMANDS_SHORT = _T("h");
+const tstring LANGUAGE_COMMANDS_SHORT = _T("l");
+const tstring VERSION_COMMANDS_SHORT = _T("v");
+
+const tstring HELP_COMMENT = _T("display help message");
+const tstring LANGUAGE_COMMENT = _T("display language version of RDO");
+const tstring VERSION_COMMENT = _T("display program version");
 
 const tstring RDO_STUDIO_CONSOLE_VERSION = _T("0.1.0");
 const tstring RDO_LANGUAGE_VERSION       = _T("1.5");
 
+namespace po = boost::program_options;
+
+tstring create_description_message();
+void create_description(po::options_description& description);
+
 int main(int argc, char *argv[])
 {
-	po::options_description description(_T("rdo console studio v") + 
-	                                    RDO_STUDIO_CONSOLE_VERSION + " " + 
-	                                    SYSTEM_OS + _T(" ( http://rdo.rk9.bmstu.ru )"));
-	description.add_options()
-	        _T(("help,h", "display help message"))
-	        _T(("language,l", "display language version of RDO"))
-	        _T(("version,v", "display program version"));
+	po::options_description description(create_description_message().c_str());
+	create_description(description);
+	
 	po::variables_map options;
 	try
 	{
@@ -46,15 +57,15 @@ int main(int argc, char *argv[])
 	{
 		std::cout << _T("command line error: ") << e.what() << std::endl;
 	}
-	if (options.empty() || options.count(_T("help")))
+	if (options.empty() || options.count(HELP_COMMANDS))
 	{
 		std::cout << description << std::endl;
 	}
-	else if (options.count(_T("language")))
+	else if (options.count(LANGUAGE_COMMANDS))
 	{
 		std::cout << _T("rdo language v") + RDO_LANGUAGE_VERSION << std::endl;
 	}
-	else if (options.count(_T("version")))
+	else if (options.count(VERSION_COMMANDS))
 	{
 		std::cout << _T("rdo console studio v") + RDO_STUDIO_CONSOLE_VERSION << std::endl;
 	}
@@ -63,4 +74,19 @@ int main(int argc, char *argv[])
 		// main code slot
 	}
 	return 0;
+}
+
+tstring create_description_message()
+{
+	return _T("rdo console studio v") + 
+			RDO_STUDIO_CONSOLE_VERSION + " " + 
+			SYSTEM_OS + _T(" ( http://rdo.rk9.bmstu.ru )");
+}
+
+void create_description(po::options_description& description)
+{
+	description.add_options()
+			((HELP_COMMANDS + "," + HELP_COMMANDS_SHORT).c_str(), HELP_COMMENT.c_str())
+			((LANGUAGE_COMMANDS + "," + LANGUAGE_COMMANDS_SHORT).c_str(), LANGUAGE_COMMENT.c_str())
+			((VERSION_COMMANDS + "," + VERSION_COMMANDS_SHORT).c_str(), VERSION_COMMENT.c_str());	
 }
