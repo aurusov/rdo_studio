5330b1aa0ec23274ea6a351bf305d622c2ba04d4
Andrey Urusov <drobus@gmail.com>
2011-10-30 08:49:50 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5465 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/utils/branches/dev_linux/rdotypes.h
@@ -0,0 +1,66 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdotypes.h
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Пройдаков Евгений (lord.tiran@gmail.com)
+  \date      18.04.2009
+  \brief     Объявление базовый типов
+  \indent    4T
+*/
+
+#ifndef _UTILS_RDOTYPES_H_
+#define _UTILS_RDOTYPES_H_
+
+// ----------------------------------------------------------------------- PLATFORM
+#include "utils/platform.h"
+// ----------------------------------------------------------------------- INCLUDES
+#ifdef OST_WINDOWS
+	#include <tchar.h>
+#else
+	#include <stdint.h>
+	#define _T(x) x
+#endif
+
+#include <string>
+// ----------------------------------------------------------------------- SYNOPSIS
+// --------------------------------------------------------------------------------
+
+//! Тип символа
+typedef  char                       tchar;
+
+//! Тип строки
+typedef  std::basic_string<tchar>   tstring;
+
+//! Булевский тип
+typedef  bool                       rbool;
+
+//! Целый знаковый тип
+typedef  signed int                 rsint;
+
+//! Целый беззнаковый тип
+typedef  unsigned int               ruint;
+
+//! Целый знаковый 32-х битный тип
+#ifdef OST_WINDOWS
+	typedef  long                   rsint32;
+#else
+	typedef  int32_t                rsint32;
+#endif
+
+//! Целый беззнаковый 32-х битный тип
+#ifdef OST_WINDOWS
+	typedef  unsigned long          ruint32;
+#else
+	typedef  uint32_t               ruint32;
+#endif
+
+//! Целый знаковый 64-х битный тип
+typedef  signed long long int       rsint64;
+
+//! Целый беззнаковый 64-х битный тип
+typedef  unsigned long long int     ruint64;
+
+//! Тип байта
+typedef  unsigned char              rbyte;
+
+#endif // _UTILS_RDOTYPES_H_
---
+++ rdo/packages/utils/branches/dev_linux/smart_ptr/counter_reference.h
@@ -0,0 +1,38 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      counter_reference.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      12.06.2010
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _UTILS_SMART_PTR_COUNTER_REFERENCE_H_
+#define _UTILS_SMART_PTR_COUNTER_REFERENCE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/namespace.h"
+#include "utils/rdomacros.h"
+#include "utils/rdotypes.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_NAMESPACE
+
+class counter_reference
+{
+template<class T> friend class intrusive_ptr;
+template<class T> friend class RefCounter;
+public:
+	//! Инициализиреут счетчик нулём
+	counter_reference();
+
+private:
+	ruint m_intrusive_counter;
+};
+
+CLOSE_RDO_NAMESPACE
+
+#include "utils/smart_ptr/counter_reference.inl"
+
+#endif // _UTILS_SMART_PTR_COUNTER_REFERENCE_H_
