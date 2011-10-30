1267824f6f634b28b7400c830aa37d0faac0f8ca
Andrey Urusov <drobus@gmail.com>
2011-10-30 08:34:55 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5450 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_linux/test/sequences_test/normal_test/main.cpp
@@ -0,0 +1,35 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      main.cpp
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Клеванец Игорь (impus@hotbox.ru)
+  \date      2.10.2011
+  \brief     Тест нормального закона
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- INCLUDES
+#define BOOST_TEST_MODULE RDOTriangularTest
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <boost/test/included/unit_test.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/rdofile.h"
+#include "simulator/runtime/rdo_random_distribution.h"
+// --------------------------------------------------------------------------------
+
+const long int g_seed     = 123456789;                  //!< база генератора
+const tstring  g_fileName = _T("data_normal.txt");  //!< файл данных
+const ruint    g_count    = 100000;                     //!< количество генерируемых данных
+const double   g_from     = 1.0;                        //!< параметр закона
+const double   g_to       = 7.0;                        //!< параметр закона
+
+typedef std::list <double> Container;
+
+BOOST_AUTO_TEST_SUITE(RDOTriangularTest)
+
+BOOST_AUTO_TEST_CASE(RDOTriangularTestCreate)
+{
+}
