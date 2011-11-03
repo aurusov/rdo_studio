f89acbc171ccdd0f76bdaecc536fdd7b8f9d9463
Andrey Urusov <drobus@gmail.com>
2011-11-03 21:44:58 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5490 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_linux/context/context.h
@@ -0,0 +1,51 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      context.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      06.06.2010
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPARSER_CONTEXT_H_
+#define _RDOPARSER_CONTEXT_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/rdo_value.h"
+#include "simulator/compiler/parser/expression.h"
+#include "simulator/compiler/parser/context/stack.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- Context
+// --------------------------------------------------------------------------------
+OBJECT(Context)
+{
+DECLARE_FACTORY(Context);
+friend void ContextStack::push(LPContext pContext);
+
+public:
+	template <class T>
+	rdo::intrusive_ptr<T> cast();
+
+	LPContext    find  (CREF(LPRDOValue) pValue) const;
+	LPExpression create(CREF(LPRDOValue) pValue);
+
+protected:
+	Context();
+	virtual ~Context();
+
+private:
+	LPContextStack m_pContextStack;
+
+	void setContextStack(CREF(LPContextStack) pContextStack);
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#include "simulator/compiler/parser/context/context.inl"
+
+#endif // _RDOPARSER_CONTEXT_H_
