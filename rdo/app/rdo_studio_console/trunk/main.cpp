7076d33ea5f47f3d6184a45202ed20360d7438a6
Evgeny Proydakov <lord.tiran@gmail.com>
2011-11-12 15:46:46 +0000

    - added new input parameters and a description of them for the console version of rdo
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5622 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio_console/trunk/main.cpp
+++ rdo/app/rdo_studio_console/trunk/main.cpp
@@ -15,6 +15,9 @@
 #include <boost/program_options.hpp>
 // ----------------------------------------------------------------------- SYNOPSIS
 #include "utils/rdocommon.h"
+//#include "kernel/rdothread.h"
+//#include "repository/rdorepository.h"
+//#include "simulator/service/rdosimwin.h"
 // --------------------------------------------------------------------------------
 
 #ifdef COMPILER_VISUAL_STUDIO
@@ -28,19 +31,35 @@ const tstring SYSTEM_OS = _T("Windows");
 const tstring SYSTEM_OS = _T("Linux");
 #endif // OST_LINUX
 
-const tstring HELP_COMMANDS     = _T("help");
-const tstring LANGUAGE_COMMANDS = _T("language");
-const tstring VERSION_COMMANDS  = _T("version");
+// Input param constant
+const tstring MODEL_COMMAND                 = _T("model");
+const tstring HELP_COMMAND                  = _T("help");
+const tstring VERSION_COMMAND               = _T("version");
+const tstring LANGUAGE_COMMAND              = _T("language");
+const tstring AUTO_RUN_COMMAND              = _T("autorun");
+const tstring AUTO_EXIT_COMMAND             = _T("autoexit");
+const tstring DONT_CLOSE_IF_ERROR_COMMAND	= _T("dont_close_if_error");
+const tstring PLUGIN_START_COMMAND			= _T("pluginstart");
+const tstring PLUGIN_AUTO_EXIT_COMMAND      = _T("pluginautoexit");
 
-const tstring HELP_COMMANDS_SHORT     = _T("h");
-const tstring LANGUAGE_COMMANDS_SHORT = _T("l");
-const tstring VERSION_COMMANDS_SHORT  = _T("v");
+const tstring MODEL_COMMAND_SHORT           = _T("m");
+const tstring HELP_COMMAND_SHORT			= _T("h");
+const tstring VERSION_COMMAND_SHORT			= _T("v");
+const tstring LANGUAGE_COMMAND_SHORT		= _T("l");
 
-const tstring HELP_COMMENT     = _T("display help message");
-const tstring LANGUAGE_COMMENT = _T("display language version of RDO");
-const tstring VERSION_COMMENT  = _T("display program version");
+// Input param comment
+const tstring MODEL_COMMENT                 = _T("path to RDO model");
+const tstring HELP_COMMENT					= _T("display help message");
+const tstring VERSION_COMMENT				= _T("display program version");
+const tstring LANGUAGE_COMMENT				= _T("display language version of RDO");
+const tstring AUTO_RUN_COMMENT				= _T("autostart program, skipped in console version");
+const tstring AUTO_EXIT_COMMENT				= _T("autoexit program, skipped in console version");
+const tstring DONT_CLOSE_IF_ERROR_COMMENT	= _T("dont close if error, skipped in console version");
+const tstring PLUGIN_START_COMMENT			= _T("start plugin, skipped in console version");
+const tstring PLUGIN_AUTO_EXIT_COMMENT		= _T("plugin auto exit, skipped in console version");
 
-const tstring RDO_STUDIO_CONSOLE_VERSION = _T("0.1.0");
+// Auxiliary constants
+const tstring RDO_STUDIO_CONSOLE_VERSION = _T("0.1.5");
 const tstring RDO_LANGUAGE_VERSION       = _T("1.5");
 
 const tstring RDO_SITE                         = _T("( http://rdo.rk9.bmstu.ru )");
@@ -53,6 +72,26 @@ namespace po = boost::program_options;
 
 tstring create_description_message();
 void create_description(po::options_description& description);
+
+struct rdo_studio_console_param
+{
+	rdo_studio_console_param() :
+		m_model_file_name(""),
+		autorun(true),
+		autoexit(true),
+		dont_close_if_error(false),
+		pluginstart(false),
+		pluginautoexit(false)
+	{
+	}
+	
+	tstring m_model_file_name;
+	rbool autorun;
+	rbool autoexit;
+	rbool dont_close_if_error;
+	rbool pluginstart;
+	rbool pluginautoexit;
+};
 
 int main(int argc, char *argv[])
 {
@@ -68,28 +107,34 @@ int main(int argc, char *argv[])
 	{
 		std::cout << _T("command line error: ") << e.what() << std::endl;
 	}
-	if (options.empty() || options.count(HELP_COMMANDS))
+	if (options.empty() || options.count(HELP_COMMAND))
 	{
 		std::cout << description << std::endl;
 	}
-	else if (options.count(LANGUAGE_COMMANDS))
+	else if (options.count(LANGUAGE_COMMAND))
 	{
 		std::cout << _T("rdo language v") + RDO_LANGUAGE_VERSION << std::endl;
 	}
-	else if (options.count(VERSION_COMMANDS))
+	else if (options.count(VERSION_COMMAND))
 	{
-		std::cout << RDO_STUDIO_CONSOLE_PROGRAMM_NAME + NOP_STRING + VERSION_COMMANDS_SHORT + RDO_STUDIO_CONSOLE_VERSION << std::endl;
+		std::cout << RDO_STUDIO_CONSOLE_PROGRAMM_NAME + NOP_STRING + VERSION_COMMAND_SHORT + RDO_STUDIO_CONSOLE_VERSION << std::endl;
 	}
 	else
 	{
-		// main code slot
+		// Init
+		//RDOKernel::init();
+		//new rdoSimulator::RDOThreadSimulator();
+		//new rdoRepository::RDOThreadRepository();
+		
+		// Close
+		//RDOKernel::close();
 	}
 	return 0;
 }
 
 tstring create_description_message()
 {
-	return  RDO_STUDIO_CONSOLE_PROGRAMM_NAME + NOP_STRING + VERSION_COMMANDS_SHORT +
+	return  RDO_STUDIO_CONSOLE_PROGRAMM_NAME + NOP_STRING + VERSION_COMMAND_SHORT +
 			RDO_STUDIO_CONSOLE_VERSION + NOP_STRING + 
 			SYSTEM_OS + RDO_SITE;
 }
@@ -97,7 +142,13 @@ tstring create_description_message()
 void create_description(po::options_description& description)
 {
 	description.add_options()
-			((HELP_COMMANDS + COMMA_STRING + HELP_COMMANDS_SHORT).c_str(), HELP_COMMENT.c_str())
-			((LANGUAGE_COMMANDS + COMMA_STRING + LANGUAGE_COMMANDS_SHORT).c_str(), LANGUAGE_COMMENT.c_str())
-			((VERSION_COMMANDS + COMMA_STRING + VERSION_COMMANDS_SHORT).c_str(), VERSION_COMMENT.c_str());	
+			((MODEL_COMMAND + COMMA_STRING + MODEL_COMMAND_SHORT).c_str(), MODEL_COMMENT.c_str())
+			((VERSION_COMMAND + COMMA_STRING + VERSION_COMMAND_SHORT).c_str(), VERSION_COMMENT.c_str())
+			((HELP_COMMAND + COMMA_STRING + HELP_COMMAND_SHORT).c_str(), HELP_COMMENT.c_str())
+			((LANGUAGE_COMMAND + COMMA_STRING + LANGUAGE_COMMAND_SHORT).c_str(), LANGUAGE_COMMENT.c_str())
+			(AUTO_RUN_COMMAND.c_str(), AUTO_RUN_COMMENT.c_str())
+			(AUTO_EXIT_COMMAND.c_str(), AUTO_EXIT_COMMENT.c_str())
+			(DONT_CLOSE_IF_ERROR_COMMAND.c_str(), DONT_CLOSE_IF_ERROR_COMMENT.c_str())
+			(PLUGIN_START_COMMAND.c_str(), PLUGIN_START_COMMENT.c_str())
+			(PLUGIN_AUTO_EXIT_COMMAND.c_str(), PLUGIN_AUTO_EXIT_COMMENT.c_str());	
 }
