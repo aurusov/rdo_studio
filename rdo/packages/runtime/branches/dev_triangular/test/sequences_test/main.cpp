99b69d998ab335c38227b37a4dd1517272a3c477
Andrey Urusov <drobus@gmail.com>
2011-11-21 19:39:29 +0000

    - подозрительно работающий вариант проверки законов на кси-квадрат. теперь ширина отрезков одинаковая. методически неверно, но чуть лучше
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5702 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_triangular/test/sequences_test/main.cpp
+++ rdo/packages/runtime/branches/dev_triangular/test/sequences_test/main.cpp
@@ -24,6 +24,7 @@
 // --------------------------------------------------------------------------------
 
 typedef std::vector<double> Container;
+typedef std::vector<ruint>  ContainerInt;
 typedef const tstring contstr;
 
 const long int g_seed                 = 123456789;                  //!< база генератора
@@ -41,7 +42,7 @@ const double   g_top                  = 5.0;                        //!< парамет
 const ruint    g_precision            = 20;                         //!< точность вещественного числа при выводе в поток
 
 const ruint    g_countOfExamples      = 2000;                       //!< количество чисел в выборке
-const ruint    g_countOfFree          = 39;                         //!< число разрядов
+const ruint    g_countOfR          = 39;                         //!< число разрядов
 const double   pi                     = 3.141592653;                //!< фундаментальная константа
 const double   g_ksiEtalon            = 50.9985;                    //!< табличное значение. 95% вероятность того, что это действительно тот самый закон распределения
 
@@ -55,7 +56,8 @@ void onGenerateData(F binder, contstr g_fileName)
 		return;
 
 	T sequence(g_seed);
-	Container test(g_count);
+	Container test;
+	test.reserve(g_count);
 
 	for (ruint i = 0; i < g_count; ++i)
 	{
@@ -76,7 +78,8 @@ void onCheckData(F binder, contstr g_fileName)
 	std::ifstream stream(g_fileName.c_str());
 	BOOST_CHECK(stream.good());
 
-	Container test(g_count);
+	Container test;
+	test.reserve(g_count);
 	T sequence(g_seed);
 	for (ruint i = 0; i < g_count; ++i)
 	{
@@ -129,50 +132,27 @@ double  area (F binder, double n, double m)
 template <class T, class G, class F, class S>
 void onCheckKsi(F binder, S binderSeq, double left, double right)
 {
-	const ruint  countOfxI  = g_countOfFree*1000;	//количество отрезков, на 1 меньше числа точек
-	const double areaOfI    = 1/(g_countOfFree*1.0);			//площадь на отрезке
+	Container x(g_countOfR + 1);
+	//x.reserve(g_countOfR + 1);
+	double elem = (right-left)/(g_countOfR*1.0);	//расстояние между точками на прямой
 
-	Container xTemp(countOfxI + 1);			//создаю временный контейнер значений, на основе которых потом буду считать границы участка
-	double elem = (right-left)/(countOfxI);	//расстояние между точками на прямой, между временными значениями
-
-	for (ruint i = 0; i < countOfxI + 1; ++i)//загоняю элементарные значения в контейнер. решил, что на три порядка больше точек (чем колчество участков) даст достаточную точность при вычислениях
+	for (ruint i = 0; i < g_countOfR + 1; ++i)
 	{
-		xTemp[i] = left + elem*i;
+		x[i] = left + elem*i;
 	}
 
-	Container x;							//контейнер для границ интервалов, которые будут участововать в вычислениях
-	x.push_back(left);						//левая граница будет в начале списка, правая граница или близкая к ней точка - right
+	Container vb(g_countOfExamples);				//контейнер для хранения выборки
+	//vb.reserve(g_countOfExamples);
 
-	for (ruint i = 1; i < countOfxI; ++i)
-	{
-		if (fabs(area<T>(binder, x.back(), xTemp[i]) - areaOfI) < fabs(area<T>(binder, x.back(), xTemp[i+1]) - areaOfI))
-		{
-			x.push_back(xTemp[i]);
-		}
-	}
-	if(x.size() == g_countOfFree)			//обрабатывается случай, когда площадь на последнем интервале окажется чуть меньше чем отведенная ему доля площади. погрешность ожидаестся очень малая из-за этого. проблема могла возникнуть при посчете количества попаданий в последний интервал. а его бы не было
-	{
-		x.push_back(right);
-	}
-	else if(x.back() < right)				//случай, когда последний интервал заканчивается раньше конца заданного интервала
-	{
-		x.pop_back();
-		x.push_back(right);
-	}
-
-	//итак, теперь у нас есть границы интервалов
-
-	Container vb(g_countOfExamples);		//контейнер для хранения выборки
-
-	G sequence(g_seed);						//выборка
+	G sequence(g_seed);								//выборка
 	for (ruint i = 0; i < g_countOfExamples; ++i)
 	{
 		vb[i] = binderSeq.operator()(&sequence);
 	}
 
-	Container fI(g_countOfFree);			//контейнер для храниения количества попаданий на интервал
-	
-	for(ruint i = 0; i < g_countOfFree; ++i)	//нахождение количества попаданий на интервал
+	Container f_vb(g_countOfR);						//контейнер для храниения количества попаданий на интервал
+
+	for(ruint i = 0; i < g_countOfR; ++i)			//нахождение количества попаданий на интервал
 	{
 		ruint freq = 0;
 		for(ruint k = 0; k < g_countOfExamples; ++k)
@@ -182,14 +162,29 @@ void onCheckKsi(F binder, S binderSeq, double left, double right)
 				++freq;
 			}
 		}
-		fI[i] = freq;
+		f_vb[i] = freq;
+	}
+
+	ContainerInt F_etalon(g_countOfR);
+	//Fi.reserve(g_countOfR);
+
+	for (ruint i = 0; i < g_countOfR; ++i)
+	{
+		F_etalon[i] = g_countOfExamples*area<T>(binder, x[i], x[i+1]);
+	}
+
+	double sum = 0;
+	double summ= 0;
+	for(ruint i = 0; i < g_countOfR; ++i)
+	{
+		sum += F_etalon[i];
+		summ += f_vb[i];
 	}
 
 	double ksi = 0;
-	double Fi = g_countOfExamples/g_countOfFree;		//вычисление самой формулы
-	for(ruint i = 0; i < g_countOfFree; ++i)
+	for(ruint i = 0; i < g_countOfR; ++i)
 	{
-		ksi += ((fI[i] - Fi)*(fI[i] - Fi))/Fi;
+		ksi += (f_vb[i] - F_etalon[i])*(f_vb[i] - F_etalon[i])/F_etalon[i];
 	}
 
 	BOOST_CHECK(ksi <= g_ksiEtalon);
@@ -298,10 +293,10 @@ BOOST_AUTO_TEST_CASE(RDONormalTestCheck)
 	onCheckKsi<SequenceNormal, rdoRuntime::RandGeneratorNormal>
 		(boost::bind(&SequenceNormal::get, normal, _1),
 		boost::bind(&rdoRuntime::RandGeneratorNormal::next, _1, g_main, g_var),
-		g_main-4*sqrt(g_var),
-		g_main+4*sqrt(g_var));
+		g_main-3.2*sqrt(g_var),
+		g_main+3.2*sqrt(g_var));
 }
-/*
+
 // --------------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------------
@@ -309,14 +304,14 @@ BOOST_AUTO_TEST_CASE(RDONormalTestCheck)
 // --------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(RDOUniformTestCreate)
 {
-	onGenerateData<rdoRuntime::RandGeneratorUniform>
-		(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
+	//onGenerateData<rdoRuntime::RandGeneratorUniform>
+	//	(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
 }
 
 BOOST_AUTO_TEST_CASE(RDOUniformTestCheck)
 {
-	onCheckData<rdoRuntime::RandGeneratorUniform>
-		(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
+	//onCheckData<rdoRuntime::RandGeneratorUniform>
+	//	(boost::bind(&rdoRuntime::RandGeneratorUniform::next, _1, g_from, g_to), g_fileUniformName);
 
 	SequenceUniform uniform(g_from, g_to);
 	onCheckKsi<SequenceUniform, rdoRuntime::RandGeneratorUniform>
@@ -332,21 +327,21 @@ BOOST_AUTO_TEST_CASE(RDOUniformTestCheck)
 // --------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(RDOExponentialTestCreate)
 {
-	onGenerateData<rdoRuntime::RandGeneratorExponential>
-		(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
+	//onGenerateData<rdoRuntime::RandGeneratorExponential>
+	//	(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
 }
 
 BOOST_AUTO_TEST_CASE(RDOExponentialTestCheck)
 {
-	onCheckData<rdoRuntime::RandGeneratorExponential>
-		(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
+	//onCheckData<rdoRuntime::RandGeneratorExponential>
+	//	(boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main), g_fileExponentialName);
 
 	SequenceExponential exponential(g_main);
 	onCheckKsi<SequenceExponential, rdoRuntime::RandGeneratorExponential>
 		(boost::bind(&SequenceExponential::get, exponential, _1),
 		boost::bind(&rdoRuntime::RandGeneratorExponential::next, _1, g_main),
 		0,
-		7*g_main);
+		5*g_main);
 }
 // --------------------------------------------------------------------------------
 
@@ -355,14 +350,14 @@ BOOST_AUTO_TEST_CASE(RDOExponentialTestCheck)
 // --------------------------------------------------------------------------------
 BOOST_AUTO_TEST_CASE(RDOTriangularTestCreate)
 {
-	onGenerateData<rdoRuntime::RandGeneratorTriangular>
-		(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
+	//onGenerateData<rdoRuntime::RandGeneratorTriangular>
+	//	(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
 }
 
 BOOST_AUTO_TEST_CASE(RDOTriangularTestCheck)
 {
-	onCheckData<rdoRuntime::RandGeneratorTriangular>
-		(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
+	//onCheckData<rdoRuntime::RandGeneratorTriangular>
+	//	(boost::bind(&rdoRuntime::RandGeneratorTriangular::next, _1, g_from, g_top, g_to), g_fileTriangularName);
 
 	SequenceTriangular triangular(g_from, g_top, g_to);
 	onCheckKsi<SequenceTriangular, rdoRuntime::RandGeneratorTriangular>
@@ -372,5 +367,5 @@ BOOST_AUTO_TEST_CASE(RDOTriangularTestCheck)
 		g_to);
 }
 // --------------------------------------------------------------------------------
-*/
+
 BOOST_AUTO_TEST_SUITE_END()
