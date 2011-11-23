fc68a775f3faba412bb1693e2303abf8c000e2bb
Andrey Urusov <drobus@gmail.com>
2011-11-23 20:42:54 +0000

    * revert changes from rev.5503
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5746 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/utils/branches/dev_linux/test/rdo_animation_test/main.cpp
+++ rdo/packages/utils/branches/dev_linux/test/rdo_animation_test/main.cpp
@@ -249,16 +249,13 @@ BOOST_AUTO_TEST_CASE(RDOCommonAnimationActiveElement)
 BOOST_AUTO_TEST_CASE(RDOCommonAnimationNullElement)
 {
 	rdoAnimation::RDONullElement* nullElem = new rdoAnimation::RDONullElement;
-	
 	BOOST_CHECK(nullElem);
-	
 	delete nullElem;
 }
 
 BOOST_AUTO_TEST_CASE(RDOCommonAnimationFramet)
 {
 	rdoAnimation::RDOFrame frame;
-	
 	BOOST_CHECK(!frame.hasBgImage());
 }
 
