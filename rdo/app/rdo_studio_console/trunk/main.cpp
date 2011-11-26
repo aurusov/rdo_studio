6873f927aa6142f74ba41a77dd8d68e603179cde
Andrey Urusov <drobus@gmail.com>
2011-11-26 14:34:02 +0000

    - рыба
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5761 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio_console/trunk/main.cpp
+++ rdo/app/rdo_studio_console/trunk/main.cpp
@@ -75,16 +75,15 @@ void create_description(po::options_description& description);
 
 struct rdo_studio_console_param
 {
-	rdo_studio_console_param() :
-		m_model_file_name(""),
-		autorun(true),
-		autoexit(true),
-		dont_close_if_error(false),
-		pluginstart(false),
-		pluginautoexit(false)
-	{
-	}
-	
+	rdo_studio_console_param()
+		: m_model_file_name("")
+		, autorun(true)
+		, autoexit(true)
+		, dont_close_if_error(false)
+		, pluginstart(false)
+		, pluginautoexit(false)
+	{}
+
 	tstring m_model_file_name;
 	rbool autorun;
 	rbool autoexit;
@@ -93,11 +92,177 @@ struct rdo_studio_console_param
 	rbool pluginautoexit;
 };
 
+class ModelController: public RDOThread
+{
+public:
+	ModelController();
+	virtual ~ModelController();
+
+private:
+	void proc(REF(RDOMessageInfo) msg);
+};
+
+ModelController::ModelController()
+	: RDOThread(_T("RDOThreadModelController"))
+{
+	notifies.push_back(RT_REPOSITORY_MODEL_OPEN             );
+	notifies.push_back(RT_REPOSITORY_MODEL_OPEN_GET_NAME    );
+	notifies.push_back(RT_REPOSITORY_MODEL_OPEN_ERROR       );
+	notifies.push_back(RT_REPOSITORY_MODEL_CLOSE            );
+	notifies.push_back(RT_REPOSITORY_MODEL_CLOSE_CAN_CLOSE  );
+	notifies.push_back(RT_REPOSITORY_MODEL_CLOSE_ERROR      );
+	notifies.push_back(RT_REPOSITORY_MODEL_SAVE             );
+	notifies.push_back(RT_SIMULATOR_PARSE_STRING            );
+	notifies.push_back(RT_SIMULATOR_PARSE_OK                );
+	notifies.push_back(RT_SIMULATOR_PARSE_ERROR             );
+	notifies.push_back(RT_SIMULATOR_MODEL_STOP_OK           );
+	notifies.push_back(RT_SIMULATOR_MODEL_STOP_RUNTIME_ERROR);
+	notifies.push_back(RT_RUNTIME_MODEL_START_BEFORE        );
+	notifies.push_back(RT_RUNTIME_MODEL_START_AFTER         );
+	notifies.push_back(RT_RUNTIME_MODEL_STOP_BEFORE         );
+	notifies.push_back(RT_DEBUG_STRING                      );
+	notifies.push_back(RT_RESULT_STRING                     );
+
+	after_constructor();
+}
+
+ModelController::~ModelController()
+{}
+
+void ModelController::proc(REF(RDOThread::RDOMessageInfo) msg)
+{
+	switch (msg.message)
+	{
+		case RDOThread::RT_REPOSITORY_MODEL_OPEN:
+		{
+			int i = 1;
+			//openModelFromRepository();
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_OPEN_ERROR:
+		{
+//			AfxMessageBox(rdo::format(ID_MSG_MODELOPEN_ERROR, static_cast<PTR(tstring)>(msg.param)->c_str()).c_str(), MB_ICONSTOP | MB_OK);
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_SAVE:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_OPEN_GET_NAME:
+		{
+			//msg.lock();
+			//PTR(rdoRepository::RDOThreadRepository::OpenFile) data = static_cast<PTR(rdoRepository::RDOThreadRepository::OpenFile)>(msg.param);
+			//CString filter;
+			//filter.LoadString(ID_MODEL_FILETYPE);
+			//CFileDialog dlg(true, _T(""), _T(""), 0, filter, AfxGetMainWnd());
+			//data->m_result   = dlg.DoModal() == IDOK;
+			//data->m_name     = dlg.GetPathName();
+			//data->m_readOnly = dlg.GetReadOnlyPref() == TRUE;
+			//msg.unlock();
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_CLOSE:
+		{
+//			closeModelFromRepository();
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_CLOSE_CAN_CLOSE:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_REPOSITORY_MODEL_CLOSE_ERROR:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_RUNTIME_MODEL_START_BEFORE:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_RUNTIME_MODEL_START_AFTER:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_RUNTIME_MODEL_STOP_BEFORE:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_MODEL_STOP_OK:
+		{
+			//! @todo вызывается после завершения
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_MODEL_STOP_RUNTIME_ERROR:
+		{
+//			sendMessage(kernel->simulator(), RT_SIMULATOR_GET_MODEL_EXITCODE, &m_exitCode);
+//			show_result();
+//			PTR(RDOStudioOutput) output = &studioApp.mainFrame->output;
+//			output->appendStringToDebug(rdo::format(IDS_MODEL_RUNTIMEERROR_STOPED));
+//			output->clearBuild();
+//			output->showBuild();
+//			output->appendStringToBuild(rdo::format(IDS_MODEL_RUNTIMEERROR));
+//			std::vector< RDOSyntaxError > errors;
+//			studioApp.studioGUI->sendMessage(kernel->simulator(), RDOThread::RT_SIMULATOR_GET_ERRORS, &errors);
+//			int errors_cnt   = 0;
+//			int warnings_cnt = 0;
+//			STL_FOR_ALL_CONST(errors, it)
+//			{
+//				output->appendStringToBuild(it->m_code, it->m_message, it->m_file, it->m_line, it->m_pos, it->m_warning);
+//				if (it->m_warning)
+//				{
+//					warnings_cnt++;
+//				}
+//				else
+//				{
+//					errors_cnt++;
+//				}
+//			}
+//			studioApp.autoCloseByModel();
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_PARSE_OK:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_PARSE_ERROR:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_SIMULATOR_PARSE_STRING:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_DEBUG_STRING:
+		{
+			int i = 1;
+			break;
+		}
+		case RDOThread::RT_RESULT_STRING:
+		{
+			int i = 1;
+			break;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	po::options_description description(create_description_message().c_str());
 	create_description(description);
-	
+
 	po::variables_map options;
 	try
 	{
@@ -119,13 +284,25 @@ int main(int argc, char *argv[])
 	{
 		std::cout << RDO_STUDIO_CONSOLE_PROGRAMM_NAME + NOP_STRING + VERSION_COMMAND_SHORT + RDO_STUDIO_CONSOLE_VERSION << std::endl;
 	}
-	else
+	//else
 	{
 		// Init
 		RDOKernel::init();
 		new rdoSimulator::RDOThreadSimulator();
 		new rdoRepository::RDOThreadRepository();
-		
+
+		ModelController* modelController = new ModelController();
+
+		rdoRepository::RDOThreadRepository::OpenFile data("C:\\mymodel\\mymodel.rdox");
+		modelController->broadcastMessage(RDOThread::RT_STUDIO_MODEL_OPEN, &data);
+		modelController->broadcastMessage(RDOThread::RT_STUDIO_MODEL_BUILD);
+		modelController->broadcastMessage(RDOThread::RT_STUDIO_MODEL_RUN  );
+
+		while (true)
+		{
+			kernel->idle();
+		}
+
 		// Close
 		RDOKernel::close();
 	}
