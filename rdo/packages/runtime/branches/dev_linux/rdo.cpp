1194ba0006cfc624fe48d8645f6392178da94409
Andrey Urusov <drobus@gmail.com>
2011-11-30 20:03:06 +0000

    - пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5789 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_linux/rdo.cpp
@@ -0,0 +1,21 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdo.cpp
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      11.06.2006
+  \brief     Определение интерфейса IBaseOperation
+  \indent    4T
+*/
+
+// ----------------------------------------------------------------------- PLATFORM
+#include "utils/platform.h"
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/runtime/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/rdo.h"
+// --------------------------------------------------------------------------------
+
+#ifdef COMPILER_VISUAL_STUDIO
+	#pragma warning(disable : 4786)
+#endif // COMPILER_VISUAL_STUDIO
