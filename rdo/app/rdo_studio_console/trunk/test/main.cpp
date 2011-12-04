db5c633ef9aa0f60482f451e4160c610fba18bff
Evgeny Proydakov <lord.tiran@gmail.com>
2011-12-04 20:06:12 +0000

    - added skeleton for testing framework rdo_studio_console
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5836 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/app/rdo_studio_console/trunk/test/main.cpp
@@ -0,0 +1,24 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      main.cpp
+  \author    Пройдаков Евгений (lord.tiran@gmail.com)
+  \date      10.05.2009
+  \brief     Тест приложения rdo_studio_console
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- INCLUDES
+#define BOOST_TEST_MODULE RDOStudioConsoleTest
+#include <boost/test/included/unit_test.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+// --------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(RDOStudioConsoleTest)
+
+BOOST_AUTO_TEST_CASE(RDOCommonAnimationFrame)
+{
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // RDOStudioConsoleTest
