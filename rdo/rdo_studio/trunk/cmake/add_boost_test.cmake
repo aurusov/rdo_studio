7c337d0d55589624ed86b72931c740e1b723bb7c
Andrey Urusov <drobus@gmail.com>
2011-12-11 11:26:46 +0000

    * merged with dev_linux, rev[5087-5193]  - fixed compile bugs  - updated cmake : reworked build library and executable directory  - updated cmake : added gcc 32 bit compile flag  - set build type x64  - reworked cmake : removed _nix suffix  - fixed numerous bugs and compiler warnings  - updated main cmake : renamed project  - added cmake for mbuilder  - updated cmake : added macros for add_boost_test and reworked all test cmake  - added *Copyright* in cmake build system
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5882 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/rdo_studio/trunk/cmake/add_boost_test.cmake
@@ -0,0 +1,29 @@
+#==============================================================================
+# Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
+#==============================================================================
+
+# create BOOST TEST
+MACRO(ADD_BOOST_TEST args)
+
+
+MESSAGE(STATUS "CREATE  ${ARGV0}")
+
+INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR})
+INCLUDE_DIRECTORIES(${RDO_BASE_DIRECTORY})
+
+FIND_PACKAGE(Boost COMPONENTS unit_test_framework REQUIRED)
+INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
+
+ADD_EXECUTABLE(${ARGV0} ${ARGN})
+ADD_TEST(NAME ${ARGV0} COMMAND ${EXECUTABLE_OUTPUT_PATH}/${ARGV0})
+
+IF(MSVC)
+	SET_TARGET_PROPERTIES(${ARGV0} PROPERTIES COMPILE_FLAGS "/EHa")
+ENDIF()
+
+TARGET_LINK_LIBRARIES(${ARGV0} ${Boost_LIBRARIES})
+
+SOURCE_GROUP(".test" FILES 
+	${ARGN})
+
+ENDMACRO(ADD_BOOST_TEST)
