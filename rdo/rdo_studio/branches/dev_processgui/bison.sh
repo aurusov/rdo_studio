48b14705f35c2ea2f71a2c2abad3be77a5c5763e
Andrey Urusov <drobus@gmail.com>
2011-12-11 18:24:04 +0000

    - удаление перед пересозданием
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5945 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/rdo_studio/branches/dev_processgui/bison.sh
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# Copyright (c) 2011 Evgeny Proydakov <lord.tiran@gmail.com>
-###############################################################################
-# bison files generator shell script
-###############################################################################
-
-cd simulator/compiler/parser/
-bison rdortp.y -g -v --defines=rdogramma.h -prtp -ordogramrtp.cpp
