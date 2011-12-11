ba84f576fdfea119bf3b7c4388c0096dc3a05d76
Andrey Urusov <drobus@gmail.com>
2011-12-11 17:20:06 +0000

    * merged from dev_linux, rev[5829-5864]  - fixed a link error when building boost libraries rdo_parser  - fixed lines in lower case  - updated build system to cmake library parser: added compatibility with version 2.5.4 flex  - cmake style fix
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@5908 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/compiler/parser/trunk/parser/std_fun.cpp
+++ rdo/packages/compiler/parser/trunk/parser/std_fun.cpp
@@ -114,9 +114,7 @@ public:
 void RDOParserSTDFUN::generate(CREF(tstring) name, CREF(rdoRuntime::LPRDOFunCalc) pCalc, CREF(LPRDOParam) pReturnType, CREF(ParamList) paramList) const
 {
 	tstring nameLower = name;
-#ifdef COMPILER_VISUAL_STUDIO
 	rdo::toLower(nameLower);
-#endif // COMPILER_VISUAL_STUDIO
 	generateReal(name,      pCalc, pReturnType, paramList);
 	generateReal(nameLower, pCalc, pReturnType, paramList);
 }
