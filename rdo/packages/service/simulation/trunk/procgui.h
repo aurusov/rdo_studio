03fffb746ca0d5aff368ad2bc5a21186e2bed28a
Andrey Urusov <drobus@gmail.com>
2011-12-20 20:41:06 +0000

    * temporary revert, rev[6086-6087]  - перемещение калков по отдельным файлам  - RDOCalcFunctionCall -> RDOCalcFunctionCaller  - форматирование
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6088 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/service/simulation/trunk/procgui.h
+++ rdo/packages/service/simulation/trunk/procgui.h
@@ -25,12 +25,12 @@ OPEN_RDO_SIMULATOR_NAMESPACE
 class ProcGUICalc
 {
 public:
-	rdoRuntime::LPRDOCalcConst          getConstCalc     (double arg                                       );
-	rdoRuntime::LPRDOCalcFunctionCaller getNormalCalc    (ruint base, double arg1, double arg2             );
-	rdoRuntime::LPRDOCalcFunctionCaller getUniformCalc   (ruint base, double arg1, double arg2             );
-	rdoRuntime::LPRDOCalcFunctionCaller getExpCalc       (ruint base, double arg                           );
-	rdoRuntime::LPRDOCalcFunctionCaller getTriangularCalc(ruint base, double arg1, double arg2, double arg3);
-	rdoRuntime::LPRDOCalc               getCalc();
+	rdoRuntime::LPRDOCalcConst        getConstCalc     (double arg                                       );
+	rdoRuntime::LPRDOCalcFunctionCall getNormalCalc    (ruint base, double arg1, double arg2             );
+	rdoRuntime::LPRDOCalcFunctionCall getUniformCalc   (ruint base, double arg1, double arg2             );
+	rdoRuntime::LPRDOCalcFunctionCall getExpCalc       (ruint base, double arg                           );
+	rdoRuntime::LPRDOCalcFunctionCall getTriangularCalc(ruint base, double arg1, double arg2, double arg3);
+	rdoRuntime::LPRDOCalc             getCalc();
 
 protected:
 	ProcGUICalc(CREF(rdoRuntime::LPRDORuntime) pRuntime, CREF(LPRPShapeDataBlock) pParams);
