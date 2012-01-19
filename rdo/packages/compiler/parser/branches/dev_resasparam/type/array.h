2ffcfb0c80bd78445f2bfbd4a69a1628c6ed0131
Andrey Urusov <drobus@gmail.com>
2012-01-19 07:57:56 +0000

    - бранч
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6280 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_resasparam/type/array.h
@@ -0,0 +1,45 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      simulator/compiler/parser/type/array.h
+  \author    Чирков Михаил
+  \date      
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPARSER_TYPE_ARRAY_H_
+#define _RDOPARSER_TYPE_ARRAY_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/type/info.h"
+#include "simulator/runtime/rdo_array.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOArrayType
+// --------------------------------------------------------------------------------
+class RDOArrayType: public RDOType, public RDOParserSrcInfo
+{
+DECLARE_FACTORY(RDOArrayType);
+public:
+	CREF(LPTypeInfo)           getItemType        () const;
+	rdoRuntime::LPRDOArrayType getRuntimeArrayType() const;
+	LPTypeInfo                 typeInfo           () const;
+
+private:
+	RDOArrayType         (CREF(LPTypeInfo) pItemType, CREF(RDOParserSrcInfo) src_info);
+	virtual ~RDOArrayType();
+
+	LPTypeInfo m_pItemType;
+
+	DECLARE_IType;
+	DECLARE_IModelStructure;
+};
+DECLARE_POINTER(RDOArrayType)
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif // _RDOPARSER_TYPE_ARRAY_H_
