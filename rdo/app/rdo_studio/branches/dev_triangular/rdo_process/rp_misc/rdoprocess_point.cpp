8fb4bf10adc1b85099be91f39c7136690ddd50cc
Andrey Urusov <drobus@gmail.com>
2012-01-28 10:33:14 +0000

    * больше не нужен
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6383 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/branches/dev_triangular/rdo_process/rp_misc/rdoprocess_point.cpp
+++
@@ -1,8 +0,0 @@
-#include "app/rdo_studio_mfc/rdo_process/rp_misc/stdafx.h"
-#include "app/rdo_studio_mfc/rdo_process/rp_misc/rdoprocess_point.h"
-
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
