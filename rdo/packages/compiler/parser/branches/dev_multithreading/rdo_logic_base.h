ca3184e2749386a931e6dba21103f1bd3a15ffaf
Igor Klevanec <cerevra@gmail.com>
2012-02-12 15:57:15 +0000

    - перевод RDOLogicBase на объявление через макросы
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6577 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/compiler/parser/branches/dev_multithreading/rdo_logic_base.h
+++ rdo/packages/compiler/parser/branches/dev_multithreading/rdo_logic_base.h
@@ -3,7 +3,7 @@
   \file      simulator/compiler/parser/rdo_logic_base.h
   \author    Клеванец Игорь (impus@hotbox.ru)
   \date      5.02.2012
-  \brief     
+  \brief     Базовый класс для точек принятия решений
   \indent    4T
 */
 
@@ -19,14 +19,11 @@
 
 OPEN_RDO_PARSE_NAMESPACE
 
-class RDOLogicBase: public RDOParserSrcInfo
+OBJECT(RDOLogicBase)
+	AND INSTANCE_OF (RDOParserSrcInfo)
 {
+DECLARE_FACTORY(RDOLogicBase);
 public:
-	RDOLogicBase(CREF(RDOParserSrcInfo) src_info)
-	: RDOParserSrcInfo(src_info)
-	{}
-	virtual ~RDOLogicBase()
-	{}
 
 	CREF(tstring) name              () const;
 	rbool         setPrior          (REF(LPRDOFUNArithm) pPrior);
@@ -37,6 +34,12 @@ public:
 	LPILogic      getLogic          () const;
 
 protected:
+	RDOLogicBase(CREF(RDOParserSrcInfo) src_info)
+	: RDOParserSrcInfo(src_info)
+	{}
+	virtual ~RDOLogicBase()
+	{}
+
 	LPRDOFUNLogic m_pConditon;
 	LPILogic      m_pRuntimeLogic;
 	rbool         m_multithreading;
