d8d74766433b53039d0e96e40becfadb057faad6
Andrey Urusov <drobus@gmail.com>
2012-02-23 13:44:48 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6761 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_oop/expression.h
@@ -0,0 +1,68 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      expression.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      07.03.2011
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPARSER_EXPRESSION_H_
+#define _RDOPARSER_EXPRESSION_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/type/info.h"
+#include "simulator/compiler/parser/rdo_value.h"
+#include "simulator/runtime/calc/calc_base.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- Expression
+// --------------------------------------------------------------------------------
+class ExpressionStatement;
+OBJECT(Expression) IS INSTANCE_OF(RDOParserSrcInfo)
+{
+DECLARE_FACTORY(Expression);
+friend class ExpressionStatement;
+public:
+	CREF(LPTypeInfo)             typeInfo  () const;
+	CREF(rdoRuntime::LPRDOCalc)  calc      () const;
+	virtual void                 setSrcInfo(CREF(RDOParserSrcInfo) src_info);
+
+	LPRDOValue                   constant  () const;
+
+private:
+	Expression(CREF(LPTypeInfo) pType, CREF(rdoRuntime::LPRDOCalc) pCalc, CREF(RDOParserSrcInfo) src_info);
+	Expression(CREF(LPRDOValue) pValue);
+	Expression(CREF(LPExpression) pExpression);
+	virtual ~Expression();
+
+	LPTypeInfo             m_pType;
+	LPRDOValue             m_pValue; //! < Используется или m_pValue или m_pCalc
+	rdoRuntime::LPRDOCalc  m_pCalc;  //! < Более красивое решение: завести парсеровский калк
+};
+
+// --------------------------------------------------------------------------------
+// -------------------- ExpressionStatement
+// --------------------------------------------------------------------------------
+CLASS(ExpressionStatement): INSTANCE_OF(Expression)
+{
+DECLARE_FACTORY(ExpressionStatement);
+public:
+	rbool getReturn();
+
+private:
+	ExpressionStatement(CREF(LPTypeInfo) pType, CREF(rdoRuntime::LPRDOCalc) pCalc, CREF(rdoRuntime::RDOSrcInfo) src_info);
+	ExpressionStatement(CREF(LPExpression) pExpression);
+	virtual ~ExpressionStatement();
+
+	rbool m_returnFlag;
+};
+DECLARE_POINTER(ExpressionStatement);
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif // _RDOPARSER_EXPRESSION_H_
