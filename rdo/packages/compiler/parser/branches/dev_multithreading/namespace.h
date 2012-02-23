468398ab47233685dd2ac955078e3ee56804c564
Andrey Urusov <drobus@gmail.com>
2012-02-23 13:44:22 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6759 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_multithreading/namespace.h
@@ -0,0 +1,24 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      simulator/compiler/parser/namespace.h
+  \authors   Барс Александр
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      14.02.2012
+  \brief     Пространство имен rdoParser
+  \indent    4T
+*/
+
+#ifndef _LIB_PARSER_NAMESPACE_H_
+#define _LIB_PARSER_NAMESPACE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+// --------------------------------------------------------------------------------
+
+//! \def    OPEN_RDO_PARSER_NAMESPACE
+//! \brief  Пространство имён парсера
+#define  OPEN_RDO_PARSER_NAMESPACE  namespace rdoParser {
+#define CLOSE_RDO_PARSER_NAMESPACE  };
+
+#endif // _LIB_PARSER_NAMESPACE_H_
