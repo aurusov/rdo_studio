18a429fc1501a5eeb3a4c222b46d37139bd9392b
Andrey Urusov <drobus@gmail.com>
2012-02-28 18:47:56 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@6834 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_multithreading/local_variable.cpp
@@ -0,0 +1,117 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      local_variable.cpp
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      16.10.2010
+  \brief     
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/compiler/parser/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/local_variable.h"
+#include "simulator/compiler/parser/rdoparser_error.h"
+#include "simulator/runtime/rdo_resource.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- LocalVariable
+// --------------------------------------------------------------------------------
+LocalVariable::LocalVariable(CREF(LPRDOValue) pValue, CREF(LPExpression) pExpression, CREF(LPTypeInfo) pType)
+	: m_pValue     (pValue     )
+	, m_pExpression(pExpression)
+	, m_pType      (pType      )
+{}
+
+CREF(LPRDOValue) LocalVariable::getValue() const
+{
+	return m_pValue;
+}
+
+CREF(LPExpression) LocalVariable::getExpression() const
+{
+	return m_pExpression;
+}
+
+CREF(LPTypeInfo) LocalVariable::getTypeInfo() const
+{
+	return m_pType;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- LocalVariableList
+// --------------------------------------------------------------------------------
+LocalVariableList::LocalVariableList()
+{}
+
+void LocalVariableList::append(CREF(LPLocalVariable) pVariable)
+{
+	ASSERT(pVariable);
+
+	STL_FOR_ALL_CONST(m_variableList, it)
+	{
+		if ((*it)->getValue()->value().getIdentificator() == pVariable->getValue()->value().getIdentificator())
+		{
+			rdoParser::g_error().push_only(pVariable->getValue()->src_info(), rdo::format(_T("Переменная %s уже объявлена"), pVariable->getValue()->value().getIdentificator().c_str()));
+			rdoParser::g_error().push_only((*it)->getValue()->src_info(),     _T("См. первое описание"));
+			rdoParser::g_error().push_done();
+		}
+	}
+
+	m_variableList.push_back(pVariable);
+}
+
+LPLocalVariable LocalVariableList::findLocalVariable(CREF(tstring) name) const
+{
+	STL_FOR_ALL_CONST(m_variableList, it)
+	{
+		if ((*it)->getValue()->value().getIdentificator() == name)
+		{
+			return *it;
+		}
+	}
+	return NULL;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- LocalVariableListStack
+// --------------------------------------------------------------------------------
+LocalVariableListStack::LocalVariableListStack()
+{}
+
+void LocalVariableListStack::push(CREF(LPLocalVariableList) pVariableList)
+{
+	m_pVariableListStack.push_back(pVariableList);
+}
+
+void LocalVariableListStack::pop()
+{
+	ASSERT(!m_pVariableListStack.empty());
+
+	m_pVariableListStack.pop_back();
+}
+
+void LocalVariableListStack::append(CREF(LPLocalVariable) pVariable)
+{
+	ASSERT(!m_pVariableListStack.empty());
+
+	m_pVariableListStack.back()->append(pVariable);
+}
+
+LPLocalVariable LocalVariableListStack::findLocalVariable(CREF(tstring) name) const
+{
+	STL_FOR_ALL_CONST(m_pVariableListStack, it)
+	{
+		if ((*it)->findLocalVariable(name))
+		{
+			return (*it)->findLocalVariable(name);
+		}
+	}
+	return NULL;
+}
+
+CLOSE_RDO_PARSER_NAMESPACE
---
+++ rdo/packages/compiler/parser/branches/dev_multithreading/rdoparser_lexer.h
@@ -0,0 +1,77 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      rdoparser_lexer.h
+  \authors   Барс Александр
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _RDOPARSER_LEXER_H_
+#define _RDOPARSER_LEXER_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/namespace.h"
+#include "simulator/compiler/parser/grammar/rdobison.h"
+#include "utils/smart_ptr/intrusive_ptr.h"
+
+#ifndef __FLEX_LEXER_H
+#undef yyFlexLexer
+#define yyFlexLexer yyFlexLexer
+#include <FlexLexer.h>
+#endif
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOLexer
+// --------------------------------------------------------------------------------
+PREDECLARE_POINTER(RDOParser);
+
+class RDOLexer: public yyFlexLexer
+{
+public:
+	RDOLexer(CREF(LPRDOParser) pParser, PTR(std::istream) yyin, PTR(std::ostream) yyout);
+
+	void loc_init     ();
+	void loc_action   ();
+	void loc_delta_pos(int value);
+	void setvalue     (int value);
+
+	CREF(LPRDOParser) parser() const;
+
+	void  enumBegin();
+	void  enumReset();
+	rbool enumEmpty();
+
+	void  array_cnt_pls();
+	void  array_cnt_rst();
+	rsint array_cnt_shw();
+
+	PTR(int)         m_lpval;
+	PTR(YYLTYPE)     m_lploc;
+
+protected:
+	virtual int  LexerInput (PTR(char)  buf, int max_size);
+	virtual void LexerOutput(CPTR(char) buf, int size    );
+	virtual void LexerError (CPTR(char) msg);
+
+private:
+	PTR(std::istream) m_yyin;
+	PTR(std::ostream) m_yyout;
+	LPRDOParser       m_pParser;
+	rbool             m_enumEmpty;
+	rsint             m_array_param_cnt;
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#define LEXER     reinterpret_cast<PTR(rdoParser::RDOLexer)>(lexer)
+#define LEXER_POS (*LEXER->m_lploc)
+
+#include "simulator/compiler/parser/rdoparser_lexer.inl"
+
+#endif // _RDOPARSER_LEXER_H_
