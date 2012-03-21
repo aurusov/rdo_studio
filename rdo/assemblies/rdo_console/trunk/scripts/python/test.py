3bee684142d0494238c98f636badc866c3b756da
Evgeny Proydakov <lord.tiran@gmail.com>
2012-03-21 17:02:13 +0000

    - updated python system test script
    1) system test base
    2) added work with TARGET_CONSOLE
    3) added exit code
    4) code restyle
    
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7145 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/assemblies/rdo_console/trunk/scripts/python/test.py
+++ rdo/assemblies/rdo_console/trunk/scripts/python/test.py
@@ -3,17 +3,40 @@
 ###############################################################################
 
 import os
-import xml.sax
+import sys
+import xml.dom.minidom
 
-directory = '.'
-model_directory = '.'
-expansion = '.rtestx'
-buid_dir_substr = 'build'
-rdo_ex_substr = "rdo"
-rdo_test_ex_substr = "rdo_test"
+###############################################################################
+#                                 constant                                    #
+###############################################################################
+
+directory          = '.'
+model_directory    = '.'
+test_expansion     = '.rtestx'
+trace_expansion    = '.trc'
+result_expansion   = '.pmv'
+build_dir_substr   = 'build'
+rdo_ex_substr      = 'rdo'
+rdo_test_ex_substr = 'rdo_test'
+
+dividing_line = '-------------------------------------------------------------------------------'
+
+TARGET_CONSOLE   = 'CONSOLE'
+TAGRET_CONVERTER = 'CONVERTOR'
+TARGET_GUI       = 'GUI'
+
+EXIT_CODE_TERMINATION_NORMAL = 0
+EXIT_CODE_TERMINATION_ERROR  = 1
+
+# global exit code variable
+G_EXIT_CODE = EXIT_CODE_TERMINATION_NORMAL
+
+
+###############################################################################
+#                                 functions                                   #
+###############################################################################
 
 def get_test_files(dir):
-  # get all
   dirs  = []
   nfile = []
   files = []
@@ -23,44 +46,125 @@ def get_test_files(dir):
       dirs.append(os.path.join(dirname, subdirname))
     for filename in filenames:
       files.append(os.path.join(dirname, filename))
-  nfile = filter(lambda x: x.endswith(expansion), files)
+  nfile = filter(lambda x: x.endswith(test_expansion), files)
   return nfile
+
 
 def get_executables(dir):
   # get rdo_studio_console executable and test_rdo_studio_console executable
-  rdo_ex = ""
-  test_rdo_ex = ""
+  rdo_ex = ''
+  test_rdo_ex = ''
   
   dirs = os.listdir(dir)
   fdir = ''
   
   for x in dirs:
-    if x.find(buid_dir_substr) != -1:
-      fdir = "./" + x
+    if x.find(build_dir_substr) != -1:
+      fdir = './' + x
       break
   
   if len(fdir) == 0:
-    print "Build directory not found. Critical error !!!"
+    print 'Build directory not found. Critical error !!!'
     exit(-1)
   
-  rdo_ex      = ""
-  rdo_test_ex = ""
+  rdo_ex      = ''
+  rdo_test_ex = ''
   
-  rdo_ex = fdir + "/" + rdo_ex_substr
-  rdo_test_ex = fdir + "/" + rdo_test_ex_substr
+  rdo_ex = fdir + '/' + rdo_ex_substr
+  rdo_test_ex = fdir + '/' + rdo_test_ex_substr
   
   if not os.path.exists(rdo_ex) or not os.path.exists(rdo_test_ex):
-    print "Build directory not found. Critical error !!!"
+    print 'Build directory not found. Critical error !!!'
     exit(-1)
   
   return rdo_ex, rdo_test_ex
+
+
+def get_text_from_node_list(nodelist):
+  rc = []
+  for node in nodelist:
+    if node.nodeType == node.TEXT_NODE:
+      rc.append(node.data)
+  return ''.join(rc)
 
 ###############################################################################
 #                                 main code                                   #
 ###############################################################################
 
+print "STARTED SCRIPT :", sys.argv[0]
+
+# search .rtestx files, rdo and rdo_test executables
+
 files = get_test_files(model_directory)
 executables = get_executables(directory)
 
-print 'files : ', files
-print 'executables : ', executables
+rdo_ex = executables[0]
+rdo_test_ex = executables[1]
+
+print '\nDEBUG INFO\n'
+
+print 'Find test project files : ', files
+print 'Find RDO executables    : ', executables
+
+# parse xml and start tests
+
+print '\nSTARTED TEST CYCLE'
+print dividing_line
+
+for task in files:
+  dom = xml.dom.minidom.parse(task)
+  
+  dirname = os.path.dirname(task) + '/'
+  
+  model_name_with_ex = get_text_from_node_list(dom.getElementsByTagName('model')[0].childNodes)
+  
+  target = get_text_from_node_list(dom.getElementsByTagName('target')[0].childNodes)
+  exit_code = get_text_from_node_list(dom.getElementsByTagName('exit_code')[0].childNodes)
+  etalon_trace_name = get_text_from_node_list(dom.getElementsByTagName('trace')[0].childNodes)
+  etalon_result_name =  get_text_from_node_list(dom.getElementsByTagName('result')[0].childNodes)
+  
+  print 'Project     :', task
+  print 'Model file  :', model_name_with_ex
+  print 'Target      :', target
+  print 'Exit code   :', exit_code
+  print 'Trace file  :', etalon_trace_name
+  print 'Result file :', etalon_result_name
+  
+  model = dirname + model_name_with_ex
+  etalon_trace = dirname + etalon_trace_name
+  etalon_result =  dirname + etalon_result_name
+  
+  model_name = model_name_with_ex.partition('.')[0]
+  
+  simulation_trace = dirname + model_name + trace_expansion
+  simulation_result = dirname + model_name + result_expansion
+  
+  if target == TARGET_CONSOLE:
+    simulation_code = os.system(rdo_ex + ' -i ' + model)
+    
+    print "SIMYLATION EXIT CODE   :", simulation_code
+    
+    exit_code_string = ''
+    if cmp(simulation_code, exit_code):
+      exit_code_string = "OK"
+    else:
+      exit_code_string = "ERROR"
+    print "CHECK EXIT CODE - " + exit_code_string
+    
+    if simulation_code == EXIT_CODE_TERMINATION_NORMAL:
+      test_code = os.system(rdo_test_ex + ' -T ' + etalon_trace + ' -R ' + etalon_result + ' -t ' + simulation_trace + ' -r ' + simulation_result)
+      if not test_code == EXIT_CODE_TERMINATION_NORMAL:
+        G_EXIT_CODE = EXIT_CODE_TERMINATION_ERROR
+        
+  else:
+    print 'INVALID TARGET'
+  
+  # remove temp file
+  os.remove(simulation_trace)
+  os.remove(simulation_result)
+  
+  print dividing_line
+    
+print 'G_EXIT_CODE', G_EXIT_CODE
+
+sys.exit(G_EXIT_CODE)
