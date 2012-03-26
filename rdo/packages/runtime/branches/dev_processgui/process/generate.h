1e19e81c2662dc8055ea060835b2d771d2733b12
Andrey Urusov <drobus@gmail.com>
2012-03-26 06:04:40 +0000

    * бранч
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7237 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_processgui/process/generate.h
@@ -0,0 +1,55 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      generate.h
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      12.02.2012
+  \brief     Процессный оператор GENERATE
+  \indent    4T
+*/
+
+#ifndef _LIB_RUNTIME_PROCESS_GENERATE_H_
+#define _LIB_RUNTIME_PROCESS_GENERATE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+#include <boost/noncopyable.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/rdo.h"
+#include "simulator/runtime/rdo_runtime.h"
+#include "simulator/runtime/process/rdoprocess.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+/*!
+  \class   RDOPROCGenerate
+  \brief   Процессный блок GENERATE
+*/
+class RDOPROCGenerate: public RDOPROCBlock, public IBaseOperation 
+{
+DEFINE_IFACTORY(RDOPROCGenerate);
+QUERY_INTERFACE_BEGIN
+	QUERY_INTERFACE_PARENT(RDOPROCBlock  )
+	QUERY_INTERFACE       (IBaseOperation)
+QUERY_INTERFACE_END
+
+public:
+	void calcNextTimeInterval(CREF(LPRDORuntime) pRuntime);
+
+private:
+	RDOPROCGenerate(LPIPROCProcess process, CREF(LPRDOCalc) pTime, CREF(LPRDOCalc) pCreateAndGoOnTransactCalc, int maxTransCount = 0);
+
+	double     timeNext;
+	LPRDOCalc  m_pTimeCalc;
+	LPRDOCalc  m_pCreateAndGoOnTransactCalc;
+	int        m_maxTransCount;
+	int        m_TransCount;
+
+	DECLARE_IBaseOperation;
+};
+
+CLOSE_RDO_RUNTIME_NAMESPACE
+
+#include "simulator/runtime/process/generate.inl"
+
+#endif // _LIB_RUNTIME_PROCESS_GENERATE_H_
---
+++ rdo/packages/runtime/branches/dev_processgui/process/terminate.inl
@@ -0,0 +1,28 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      terminate.inl
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      12.02.2012
+  \brief     Процессный оператор TERMINATE
+  \indent    4T
+*/
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/rdo.h"
+#include "simulator/runtime/rdo_runtime.h"
+#include "simulator/runtime/rdo_logic.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOPROCTerminate
+// --------------------------------------------------------------------------------
+inline RDOPROCTerminate::RDOPROCTerminate(CREF(LPIPROCProcess) pProcess, CREF(LPRDOCalc) pCalc)
+	: RDOPROCBlock(pProcess)
+	, pTermCalc   (pCalc   )
+{}
+
+CLOSE_RDO_RUNTIME_NAMESPACE
