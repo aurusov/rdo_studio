bc3e28e59b7fb3f5b5016abe343151b4a508cc9c
Alex Cyferov <pinometr@gmail.com>
2012-04-14 12:37:05 +0000

    *merged to rev.7508
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7539 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_processgui/calc/procedural/calc_locvar.h
+++ rdo/packages/runtime/branches/dev_processgui/calc/procedural/calc_locvar.h
@@ -23,7 +23,11 @@ CALC(RDOCalcCreateLocalVariable)
 {
 DECLARE_FACTORY(RDOCalcCreateLocalVariable)
 private:
+<<<<<<< .working
 	RDOCalcCreateLocalVariable(CREF(tstring) name, CREF(LPRDOCalc) pValueCalc);
+=======
+	RDOCalcCreateLocalVariable(CREF(tstring) name);
+>>>>>>> .merge-right.r7493
 
 	tstring    m_name;
 	LPRDOCalc  m_pValueCalc;
