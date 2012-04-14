113f102fe7c1dc4712782e163c0595f3713927cd
Andrey Urusov <drobus@gmail.com>
2012-04-14 10:23:04 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7530 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_oop/calc/procedural/calc_statement.cpp
@@ -0,0 +1,210 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      calc_statement.cpp
+  \authors   Чирков Михаил
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      16.04.2011
+  \brief     Инструкции
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/runtime/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/calc/procedural/calc_statement.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcNoChange
+// --------------------------------------------------------------------------------
+RDOCalcNoChange::RDOCalcNoChange()
+{}
+
+REF(RDOValue) RDOCalcNoChange::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	UNUSED(pRuntime);
+	m_value = RDOValue(0);
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcIf
+// --------------------------------------------------------------------------------
+RDOCalcIf::RDOCalcIf(CREF(LPRDOCalc) pCondition, CREF(LPRDOCalc) pStatement)
+	: m_pCondition(pCondition)
+	, m_pStatement(pStatement)
+{
+	ASSERT(m_pCondition);
+	ASSERT(m_pStatement);
+}
+
+REF(RDOValue) RDOCalcIf::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	m_value = RDOValue(false);
+	return (m_pCondition->calcValue(pRuntime).getAsBool()) ? m_pStatement->calcValue(pRuntime) : (m_value);
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcIfElse
+// --------------------------------------------------------------------------------
+RDOCalcIfElse::RDOCalcIfElse(CREF(LPRDOCalc) pCondition, CREF(LPRDOCalc) pIfStatement, CREF(LPRDOCalc) pElseStatement)
+	: m_pCondition    (pCondition    )
+	, m_pIfStatement  (pIfStatement  )
+	, m_pElseStatement(pElseStatement)
+{
+	ASSERT(m_pCondition    );
+	ASSERT(m_pIfStatement  );
+	ASSERT(m_pElseStatement);
+}
+
+REF(RDOValue) RDOCalcIfElse::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	return (m_pCondition->calcValue(pRuntime).getAsBool()) ? m_pIfStatement->calcValue(pRuntime) : m_pElseStatement->calcValue(pRuntime);
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcFor
+// --------------------------------------------------------------------------------
+RDOCalcFor::RDOCalcFor(CREF(LPRDOCalc) pDeclaration, CREF(LPRDOCalc) pCondition, CREF(LPRDOCalc) pExpression)
+	: m_pDeclaration(pDeclaration)
+	, m_pCondition  (pCondition  )
+	, m_pExpression (pExpression )
+{
+	ASSERT(m_pDeclaration);
+	ASSERT(m_pCondition  );
+	ASSERT(m_pExpression );
+}
+
+void RDOCalcFor::setStatement(CREF(LPRDOCalc) pStatement)
+{
+	ASSERT(pStatement);
+	m_pStatement = pStatement;
+}
+
+REF(RDOValue) RDOCalcFor::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	if (pRuntime->getFunBreakFlag() == RDORuntime::FBF_NONE)
+	{
+		m_value = m_pDeclaration->calcValue(pRuntime);
+		while (m_pCondition->calcValue(pRuntime).getAsBool())
+		{
+			m_value = m_pStatement->calcValue(pRuntime);
+			if (pRuntime->getFunBreakFlag() != RDORuntime::FBF_NONE)
+			{
+				return m_value;
+			}
+			m_pExpression->calcValue(pRuntime);
+		}
+	}
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcFunReturn
+// --------------------------------------------------------------------------------
+RDOCalcFunReturn::RDOCalcFunReturn(CREF(LPRDOCalc) pReturn)
+	: m_pReturn(pReturn)
+{}
+
+REF(RDOValue) RDOCalcFunReturn::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	m_value = m_pReturn->calcValue(pRuntime);
+	pRuntime->setFunBreakFlag(RDORuntime::FBF_RETURN);
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcFunBreak
+// --------------------------------------------------------------------------------
+RDOCalcFunBreak::RDOCalcFunBreak()
+{}
+
+REF(RDOValue) RDOCalcFunBreak::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	pRuntime->setFunBreakFlag(RDORuntime::FBF_BREAK);
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcStatementList
+// --------------------------------------------------------------------------------
+RDOCalcStatementList::RDOCalcStatementList()
+{}
+
+void RDOCalcStatementList::addCalcStatement(CREF(LPRDOCalc) pStatement)
+{
+	ASSERT(pStatement);
+	m_calcStatementList.push_back(pStatement);
+}
+
+RDOCalc::RDOCalcList RDOCalcStatementList::statementList()
+{
+	return m_calcStatementList;
+}
+
+REF(RDOValue) RDOCalcStatementList::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	STL_FOR_ALL(m_calcStatementList, calcIt)
+	{
+		if (pRuntime->getFunBreakFlag() == RDORuntime::FBF_NONE)
+		{
+			LPRDOCalc pCalc = *calcIt;
+			ASSERT(pCalc);
+			m_value = pCalc->calcValue(pRuntime);
+			if (pRuntime->getFunBreakFlag() != RDORuntime::FBF_NONE)
+			{
+				return m_value;
+			}
+		}
+	}
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcBreakCatch
+// --------------------------------------------------------------------------------
+RDOCalcBreakCatch::RDOCalcBreakCatch()
+{}
+
+void RDOCalcBreakCatch::addStatementList(CREF(LPRDOCalc) pStatementList)
+{
+	ASSERT(pStatementList);
+	m_pStatementList = pStatementList;
+}
+
+REF(RDOValue) RDOCalcBreakCatch::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	m_value = m_pStatementList->calcValue(pRuntime);
+	if (pRuntime->getFunBreakFlag() == RDORuntime::FBF_BREAK)
+	{
+		pRuntime->setFunBreakFlag(RDORuntime::FBF_NONE);
+	}
+	return m_value;
+}
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOCalcReturnCatch
+// --------------------------------------------------------------------------------
+RDOCalcReturnCatch::RDOCalcReturnCatch()
+{}
+
+void RDOCalcReturnCatch::addStatementList(CREF(LPRDOCalc) pStatementList)
+{
+	ASSERT(pStatementList);
+	m_pStatementList = pStatementList;
+}
+
+REF(RDOValue) RDOCalcReturnCatch::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	m_value = m_pStatementList->calcValue(pRuntime);
+	if (pRuntime->getFunBreakFlag() == RDORuntime::FBF_RETURN)
+	{
+		pRuntime->setFunBreakFlag(RDORuntime::FBF_NONE);
+	}
+	return m_value;
+}
+
+CLOSE_RDO_RUNTIME_NAMESPACE
---
+++ rdo/packages/runtime/branches/dev_oop/process/terminate.cpp
@@ -0,0 +1,74 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      terminate.cpp
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      12.02.2012
+  \brief     Процессный оператор TERMINATE
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/runtime/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/rdotypes.h"
+#include "utils/rdomacros.h"
+#include "simulator/runtime/process/terminate.h"
+#include "simulator/runtime/calc/calc_base.h"
+#include "simulator/runtime/calc/resource/calc_relevant.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOPROCTerminate
+// --------------------------------------------------------------------------------
+rbool RDOPROCTerminate::onCheckCondition(CREF(LPRDORuntime) pRuntime)
+{
+	UNUSED(pRuntime);
+	return !m_transacts.empty() ? true : false;
+}
+
+IBaseOperation::BOResult RDOPROCTerminate::onDoOperation(CREF(LPRDORuntime) pRuntime)
+{
+	TRACE1(_T("%7.1f TERMINATE\n"), pRuntime->getCurrentTime());
+	LPRDOPROCTransact transact = m_transacts.front();
+	ASSERT(transact);
+	transact->setState(RDOResource::CS_Erase);
+	PTR(RDOTrace) tracer = pRuntime->getTracer();
+	if (!tracer->isNull())
+	{
+		tracer->getOStream() << transact->traceResourceState('\0', pRuntime) << tracer->getEOL();
+	}
+	pRuntime->onEraseRes(transact->getTraceID(), NULL);
+	m_transacts.erase(m_transacts.begin());
+	ruint termNow = pRuntime->getCurrentTerm();
+	termNow += pTermCalc->calcValue(pRuntime).getInt();
+	pRuntime->setCurrentTerm(termNow);
+	return IBaseOperation::BOR_done;
+}
+
+void RDOPROCTerminate::onStart(CREF(LPRDORuntime) pRuntime)
+{
+	UNUSED(pRuntime);
+}
+
+void RDOPROCTerminate::onStop(CREF(LPRDORuntime) pRuntime)
+{
+	UNUSED(pRuntime);
+}
+
+void RDOPROCTerminate::onMakePlaned(CREF(LPRDORuntime) pRuntime, PTR(void) pParam)
+{
+	UNUSED(pRuntime);
+	UNUSED(pParam  );
+}
+
+IBaseOperation::BOResult RDOPROCTerminate::onContinue(CREF(LPRDORuntime) pRuntime)
+{
+	UNUSED(pRuntime);
+	return IBaseOperation::BOR_cant_run;
+}
+
+CLOSE_RDO_RUNTIME_NAMESPACE
