3d8b6313d87af150d1ab3aa6dae7ad7504ea5553
Igor Klevanec <cerevra@gmail.com>
2012-04-16 07:11:20 +0000

    - модульность
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7554 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/help/shared/trunk/scripts/navbar.js
@@ -0,0 +1,13 @@
+﻿var str = '<TABLE class="navigation" cellpadding="0"><TR class="navigation">'
+var tdi = '<TD class="navigation" align=left width="26">';
+var tdt = '<TD class="navigation" align=middle valign=middle nowrap style="padding-left: 5px; padding-right: 5px">';
+var len = document.anchors.length;
+var imgb = '<IMG class="navigation" align=middle hspace="0" vspace="0" height=18 border src="../';
+var imge = '.gif" width=26';
+str += '</TR></TABLE>';
+document.write(str);
+str = '<TABLE class="navigation" cellpadding="0"><TR class="navigation"><TD class="navigation" align=left width="26">&nbsp;</TD></TR></TABLE><DIV class="footer">';
+str += 'Справка по языку РДО';
+str += '<BR>';
+str += 'МГТУ им. Н.Э. Баумана, 2002-2012 гг.</DIV>';
+document.write(str);
