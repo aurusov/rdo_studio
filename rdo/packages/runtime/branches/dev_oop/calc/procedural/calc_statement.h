a62e869c99bf56bdaea3812620c08c1f3ac8d25e
Andrey Urusov <drobus@gmail.com>
2012-04-22 10:35:33 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7661 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/runtime/branches/dev_oop/calc/procedural/calc_statement.h
@@ -0,0 +1,141 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      calc_statement.h
+  \authors   Чирков Михаил
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \authors   Поподьянец Евгений (kurt.gigacore@gmail.com)
+  \date      16.04.2011
+  \brief     Инструкции
+  \indent    4T
+*/
+
+#ifndef _LIB_RUNTIME_CALC_PROCEDURAL_STATEMENT_H_
+#define _LIB_RUNTIME_CALC_PROCEDURAL_STATEMENT_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/calc/calc_base.h"
+#include "simulator/runtime/rdo_runtime.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+//! Пустая операция
+//! @todo заменить на CalcNop ?
+CALC(RDOCalcNoChange)
+{
+DECLARE_FACTORY(RDOCalcNoChange)
+private:
+	RDOCalcNoChange();
+	DECLARE_ICalc;
+};
+
+//! Условный оператор if () then {}
+CALC(RDOCalcIf)
+{
+DECLARE_FACTORY(RDOCalcIf)
+public:
+	void  setThenStatement(CREF(LPRDOCalc) pStatement);
+	void  setElseStatement(CREF(LPRDOCalc) pStatement);
+
+	rbool hasElse() const;
+
+private:
+	typedef  std::pair<LPRDOCalc, LPRDOCalc>  Statements;
+
+	RDOCalcIf(CREF(LPRDOCalc) pCondition);
+
+	LPRDOCalc   m_pCondition;
+	Statements  m_statements;
+
+	DECLARE_ICalc;
+};
+
+//! Оператор цикла for
+CALC(RDOCalcFor)
+{
+DECLARE_FACTORY(RDOCalcFor)
+public:
+	void setStatement(CREF(LPRDOCalc) pStatement);
+
+private:
+	RDOCalcFor(CREF(LPRDOCalc) pDeclaration, CREF(LPRDOCalc) pCondition, CREF(LPRDOCalc) pExpression);
+
+	LPRDOCalc m_pDeclaration;
+	LPRDOCalc m_pCondition;
+	LPRDOCalc m_pExpression;
+	LPRDOCalc m_pStatement;
+
+	DECLARE_ICalc;
+};
+
+//! Оператор возврата return
+CALC(RDOCalcFunReturn)
+{
+DECLARE_FACTORY(RDOCalcFunReturn)
+private:
+	RDOCalcFunReturn(CREF(LPRDOCalc) pReturn);
+
+	LPRDOCalc m_pReturn;
+	DECLARE_ICalc;
+};
+
+//! Оператор возврата break
+CALC(RDOCalcFunBreak)
+{
+DECLARE_FACTORY(RDOCalcFunBreak)
+private:
+	RDOCalcFunBreak();
+
+	DECLARE_ICalc;
+};
+
+//! Список операторов
+CALC(RDOCalcStatementList)
+{
+DECLARE_FACTORY(RDOCalcStatementList)
+public:
+	void        addCalcStatement(CREF(LPRDOCalc) pStatement);
+	RDOCalcList statementList();
+
+private:
+	RDOCalcStatementList();
+
+	RDOCalcList m_calcStatementList;
+
+	DECLARE_ICalc;
+};
+
+//! Ловушка для break
+CALC(RDOCalcBreakCatch)
+{
+DECLARE_FACTORY(RDOCalcBreakCatch)
+public:
+	void addStatementList(CREF(LPRDOCalc) pStatementList);
+
+private:
+	RDOCalcBreakCatch();
+	
+	LPRDOCalc m_pStatementList;
+
+	DECLARE_ICalc;
+};
+
+//! Ловушка для return
+CALC(RDOCalcReturnCatch)
+{
+DECLARE_FACTORY(RDOCalcReturnCatch)
+public:
+	void addStatementList(CREF(LPRDOCalc) pStatementList);
+
+private:
+	RDOCalcReturnCatch();
+	
+	LPRDOCalc m_pStatementList;
+
+	DECLARE_ICalc;
+};
+
+CLOSE_RDO_RUNTIME_NAMESPACE
+
+#endif // _LIB_RUNTIME_CALC_PROCEDURAL_STATEMENT_H_
---
+++ rdo/packages/runtime/branches/dev_oop/process/generate.inl
@@ -0,0 +1,33 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      generate.inl
+  \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
+  \date      12.02.2012
+  \brief     Процессный оператор GENERATE
+  \indent    4T
+*/
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/runtime/rdo.h"
+#include "simulator/runtime/rdo_runtime.h"
+#include "simulator/runtime/rdo_logic.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_RUNTIME_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- RDOPROCGenerate
+// --------------------------------------------------------------------------------
+inline RDOPROCGenerate::RDOPROCGenerate(LPIPROCProcess process, CREF(LPRDOCalc) pTime, CREF(LPRDOCalc) pCreateAndGoOnTransactCalc, int maxTransCount)
+	: RDOPROCBlock                (process                   )
+	, timeNext                    (0.0                       )
+	, m_pTimeCalc                 (pTime                     )
+	, m_pCreateAndGoOnTransactCalc(pCreateAndGoOnTransactCalc)
+	, m_maxTransCount             (maxTransCount             )
+{
+	m_TransCount = 0;
+}
+
+CLOSE_RDO_RUNTIME_NAMESPACE
