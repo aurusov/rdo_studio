2408617b2073a02cf127f107949f4df19a5704c1
Andrey Urusov <drobus@gmail.com>
2012-05-05 08:52:48 +0000

    - получилось воссоздать ошибку рекурсии на c++
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7739 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_oop/test/rdo_calc_test/main.cpp
+++ rdo/packages/runtime/branches/dev_oop/test/rdo_calc_test/main.cpp
@@ -15,6 +15,7 @@
 // ----------------------------------------------------------------------- SYNOPSIS
 #include "simulator/runtime/rdo_runtime.h"
 #include "simulator/runtime/calc/procedural/calc_const.h"
+#include "simulator/runtime/calc/procedural/calc_statement.h"
 // --------------------------------------------------------------------------------
 
 OPEN_RDO_RUNTIME_NAMESPACE
@@ -72,6 +73,103 @@ BOOST_AUTO_TEST_CASE(RDOCalc_MinusDouble)
 {
 	RDOValue result = calc(boost::bind(&RDOValue::operator-, _1, _2), prepair());
 	BOOST_CHECK(result.getDouble() == const1 - const2);
+}
+
+CALC(RDOCalcProxy)
+{
+DECLARE_FACTORY(RDOCalcProxy)
+public:
+	void setCalc(CREF(LPRDOCalc) pCalc)
+	{
+		ASSERT(pCalc);
+		m_pCalc = pCalc;
+	}
+
+private:
+	RDOCalcProxy()
+		: m_count(0)
+	{}
+
+	LPRDOCalc m_pCalc;
+	ruint     m_count;
+
+	DECLARE_ICalc;
+};
+
+REF(RDOValue) RDOCalcProxy::doCalc(CREF(LPRDORuntime) pRuntime)
+{
+	ASSERT(m_pCalc);
+
+	m_value = m_pCalc->calcValue(pRuntime);
+	return m_value;
+}
+
+struct RecursCalcSimulator
+{
+	struct Param
+	{
+		Param(int value)
+			: m_value(value)
+		{}
+
+		//! Важно, чтобы результат был int, а не REF(int)
+		int value() 
+		{
+			return m_value;
+		}
+
+		Param& dec()
+		{
+			--m_value;
+			return *this;
+		}
+
+	private:
+		int m_value;
+	};
+
+	int& fun1(Param& param)
+	{
+		if (param.value() == 1)
+		{
+			m_result = 1;
+		}
+		else
+		{
+			m_result = fun1(param.dec()) * param.value();
+		}
+
+		return m_result;
+	}
+
+	int& fun2(Param& param)
+	{
+		if (param.value() == 1)
+		{
+			m_result = 1;
+		}
+		else
+		{
+			m_result = param.value() * fun2(param.dec());
+		}
+
+		return m_result;
+	}
+
+	int m_result;
+};
+
+BOOST_AUTO_TEST_CASE(RDOCalc_Recurs)
+{
+	RecursCalcSimulator calc1;
+	RecursCalcSimulator calc2;
+	RecursCalcSimulator::Param param1(5);
+	RecursCalcSimulator::Param param2(5);
+
+	int result1 = calc1.fun1(param1);
+	int result2 = calc2.fun2(param2);
+	BOOST_CHECK(result1 == 1);
+	BOOST_CHECK(result2 == 120);
 }
 
 BOOST_AUTO_TEST_SUITE_END() // RDOCalc_Test
