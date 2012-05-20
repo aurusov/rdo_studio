c6786efcd44e536fc8eb9da35650ba99cf6833f8
Andrey Urusov <drobus@gmail.com>
2012-05-20 17:47:27 +0000

    - использование _set_output_format(_TWO_DIGIT_EXPONENT)
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7934 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/utils/trunk/test/real_format_test/main.cpp
+++ rdo/packages/utils/trunk/test/real_format_test/main.cpp
@@ -8,9 +8,10 @@
 */
 
 // ---------------------------------------------------------------------------- PCH
+// ----------------------------------------------------------------------- PLATFORM
+#include "utils/platform.h"
 // ----------------------------------------------------------------------- INCLUDES
 #include <iostream>
-#include <boost/format.hpp>
 #define BOOST_TEST_MODULE RDORealFormatTest
 #include <boost/test/included/unit_test.hpp>
 // ----------------------------------------------------------------------- SYNOPSIS
@@ -18,16 +19,31 @@
 
 BOOST_AUTO_TEST_SUITE(RDORealFormatTest)
 
-BOOST_AUTO_TEST_CASE(MantissaPrecision)
+BOOST_AUTO_TEST_CASE(MantissaPrecisionSet)
+{
+#ifdef COMPILER_VISUAL_STUDIO
+	_set_output_format(_TWO_DIGIT_EXPONENT);
+#endif
+}
+
+BOOST_AUTO_TEST_CASE(MantissaPrecision2)
 {
 	double value = 10e+37;
 	std::stringstream stream;
-	stream << boost::format("%1$.10E") % value;
 
-	std::string str = stream.str();
-	std::cout << str << std::endl;
+	stream << value;
 
-	BOOST_CHECK(stream.str() == "1.0000000000E+038");
+	BOOST_CHECK(stream.str() == "1e+38");
+}
+
+BOOST_AUTO_TEST_CASE(MantissaPrecision3)
+{
+	double value = 10e+237;
+	std::stringstream stream;
+
+	stream << value;
+
+	BOOST_CHECK(stream.str() == "1e+238");
 }
 
 BOOST_AUTO_TEST_SUITE_END() // RDORealFormatTest
