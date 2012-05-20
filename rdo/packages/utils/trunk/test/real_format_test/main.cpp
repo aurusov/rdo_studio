16868e4a0ca10844453100327966343d901c5e83
Andrey Urusov <drobus@gmail.com>
2012-05-20 10:31:17 +0000

    - переделан тест
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@7927 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/utils/trunk/test/real_format_test/main.cpp
+++ rdo/packages/utils/trunk/test/real_format_test/main.cpp
@@ -17,17 +17,15 @@
 
 BOOST_AUTO_TEST_SUITE(RDORealFormatTest)
 
-BOOST_AUTO_TEST_CASE(Case1)
+BOOST_AUTO_TEST_CASE(MantissaPrecision)
 {
+	double value = 10e+007;
     std::stringstream stream;
-    stream << 0.00001;
-    
-    std::string data;
-    stream >> data;
+    stream << value;
 
-    int size = data.size();
+	std::string str = stream.str();
 
-    BOOST_CHECK(size == 6);
+    BOOST_CHECK(stream.str() == "1e+008");
 }
 
 BOOST_AUTO_TEST_SUITE_END() // RDORealFormatTest
