8a5e84804527c57381a86de05e418d631448ff69
Andrey Urusov <drobus@gmail.com>
2012-06-30 08:41:15 +0000

    * перенос
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8288 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/help/rdo_help/tags/v_1_1_mfc/shared/scripts/footer_raostudio.js
@@ -0,0 +1,2 @@
+str = '<BR><TABLE class="navigation" cellpadding="0"><TR class="navigation"><TD class="navigation" align=left width="26">&nbsp;</TD></TR></TABLE><DIV class="footer">Справка по RAO-studio<BR>МГТУ им. Н.Э. Баумана, 2004-2012 гг.</DIV>';
+document.write(str);
