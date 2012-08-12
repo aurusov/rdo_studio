510640a27e70a6cd43d4050f2e69340b306a7008
Evgeny Proydakov <lord.tiran@gmail.com>
2012-08-12 15:23:16 +0000

    - updated simulator/report library : added comment.h
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8436 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/service/report/trunk/comment.h
@@ -0,0 +1,16 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      comment.h
+  \authors   Евгений Пройдаков (lord.tiran@gmail.com)
+  \date      12.08.2012
+  \brief     
+  \indent    4T
+*/
+
+#ifndef _SIMULATOR_REPORT_COMMENT_H_
+#define _SIMULATOR_REPORT_COMMENT_H_
+
+#define WARNING_STRING "предупреждение"
+#define ERROR_STRING   "ошибка"
+
+#endif // _SIMULATOR_REPORT_COMMENT_H_
