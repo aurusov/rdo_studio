4fcfa8381fd1aeb33c686291232d0a45772d9be2
Andrey Urusov <drobus@gmail.com>
2012-08-12 09:17:45 +0000

    - RDOSyntaxError -> RDOSyntaxMessage  - переставлены первые параметры конструктора
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8412 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/service/simulation/trunk/error_code.h
+++ rdo/packages/service/simulation/trunk/error_code.h
@@ -30,7 +30,7 @@ enum RDOExitCode
 };
 
 //! Синтаксическая ошибка
-struct RDOSyntaxError
+struct RDOSyntaxMessage
 {
 	//! \brief   Код ошибки
 	//! \details Сейчас вместо кода ошибки используется текстовое сообщение об ошибке
@@ -39,23 +39,23 @@ struct RDOSyntaxError
 		UNKNOWN = 1, //!< Неизвестная ошибка
 	};
 
-	ErrorCode                     m_code;    //!< Код ошибки
 	tstring                       m_message; //!< Сообщение об ошибке
+	ErrorCode                     m_code;    //!< Код ошибки
 	rdoModelObjects::RDOFileType  m_file;    //!< Файл, в котором найдена ошибка
 	ruint                         m_line;    //!< Номер строки с ошибкой
 	ruint                         m_pos;     //!< Позиция ошибки в строке
 	rbool                         m_warning; //!< Признак предупреждения (\b true - предупреждение, \b false - ошибка)
 
 	//! Конструктор ошибки
-	//! \param code    - код ошибки
 	//! \param message - сообщение об ошибке
+	//! \param code    - код ошибки
 	//! \param file    - файл, в котором найдена ошибка
 	//! \param line    - номер строки с ошибкой
 	//! \param pos     - позиция ошибки в строке
 	//! \param warning - признак предупреждения, может отсутствовать. Значение по умолчанию \b false
-	RDOSyntaxError(ErrorCode code, CREF(tstring) message, ruint line, ruint pos, rdoModelObjects::RDOFileType file, rbool warning = false)
-		: m_code   (code   )
-		, m_message(message)
+	RDOSyntaxMessage(CREF(tstring) message, ErrorCode code, ruint line, ruint pos, rdoModelObjects::RDOFileType file, rbool warning = false)
+		: m_message(message)
+		, m_code   (code   )
 		, m_line   (line   )
 		, m_pos    (pos    )
 		, m_file   (file   )
