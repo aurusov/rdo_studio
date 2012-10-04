e4ca756b30466edf6f8fe3a9dc5df3637de89fb6
Yaroslav Romanov <robot.xet@gmail.com>
2012-10-04 20:24:04 +0000

    - added GetNearestWords() in WordListUtil
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8753 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/WordListUtil.cxx
+++ rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/WordListUtil.cxx
@@ -1,24 +1,73 @@
-// Scintilla source code edit control
-/** @file WordListUtil.cxx
- ** Util for WordList.
- ** Written by Yaroslav Romanov.
- **/
-// Copyright 1998-2012 by Neil Hodgson <neilh@scintilla.org>
-// The License.txt file describes the conditions under which this software may be distributed.
+/*!
+  \copyright (c) RDO-Team, 2003-2012
+  \file      WordListUtil.cpp
+  \author    Романов Ярослав (robot.xet@gmail.com)
+  \date      29.09.2012
+  \brief     
+  \indent    4T
+*/
+
+#include <vector>
+#include <map>
+#include <boost\algorithm\string.hpp>
 
 #include "utils\rdotypes.h"
+
+#include "WordList.h"
 #include "WordListUtil.h"
 
-WordListUtil::WordListUtil(const WordList wordlist)
+WordListUtil::WordListUtil(const WordList& wordlist)
 	:wl(wordlist)
-{}
-
-tstring *WordListUtil::GetNearestWord (const char *wordStart) const
 {
-
 }
+//tstring WordListUtil::GetNearestWord (const char *wordStart, int searchLen) const
+//{
+//}
 
-std::vector<tstring> *WordListUtil::GetNearestWords(const char *wordStart) const
+std::vector<tstring> WordListUtil::GetNearestWords(const char *wordStart) const
 {
+	struct keywords
+	{
+		tstring keyword;
+		int priority;
 
+		bool operator<(const keywords& k)
+		{
+			return priority < k.priority;
+		}
+	};
+	keywords key;
+	int start = 0;
+	int end = wl.len - 1;
+	std::vector<tstring> res;
+	std::vector<keywords> pres;
+	if (0 == wl.words)
+	{
+		res.push_back("");
+		return res;
+	}
+	if( boost::iequals(wordStart, ""))
+	{
+		for(int i = start; i < end; i++)
+		{
+			res.push_back(wl.words[i]);
+		}
+		return res;
+	}
+	for (int i = start; i < end; i++)
+	{
+		if(boost::ifind_first(wl.words[i], wordStart))
+		{
+			key.keyword = wl.words[i];
+			key.priority = tstring(wl.words[i]).length();
+			pres.push_back(key);
+		}
+	}
+	std::sort(pres.begin(), pres.end());
+	std::vector<keywords>::iterator it;
+	for(it = pres.begin(); it != pres.end(); ++it)
+	{
+		res.push_back((*it).keyword);
+	}
+	return res;
 }
--- rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/WordListUtil.h
+++ rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/WordListUtil.h
@@ -1,23 +1,21 @@
-// Scintilla source code edit control
-/** @file WordListUtil.h
- ** Util for WordList.
- ** Written by Yaroslav Romanov.
- **/
-// Copyright 1998-2012 by Neil Hodgson <neilh@scintilla.org>
-// The License.txt file describes the conditions under which this software may be distributed.
-#include <string.h>
-#include <vector>
-#include "WordList.h"
+/*!
+  \copyright (c) RDO-Team, 2003-2012
+  \file      WordListUtil.h
+  \author    Романов Ярослав (robot.xet@gmail.com)
+  \date      29.09.2012
+  \brief     
+  \indent    4T
+*/
 
-class WordListUtil {
-
+class WordListUtil
+{
 public:
-	WordListUtil(const WordList wordlist);
-	~WordListUtil();
-	
-	tstring *GetNearestWord (const char *wordStart) const;
-	std::vector<tstring> *GetNearestWords(const char *wordStart) const;
+	WordListUtil(const WordList& wordlist);
+	//~WordListUtil();
+	WordListUtil& operator=( const WordListUtil&) {}
+	//tstring GetNearestWord(const char *wordStart, int searchLen) const;
+	std::vector<tstring> GetNearestWords(const char *wordStart) const;
 
 private:
-	const WordList wl;
-}
+	const WordList& wl;
+};
