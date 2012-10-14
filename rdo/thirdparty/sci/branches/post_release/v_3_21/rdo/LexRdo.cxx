96e78dd39114b4af67a42045f3812c36bdda77f6
Andrey Urusov <drobus@gmail.com>
2012-10-14 17:15:27 +0000

    * бранч для использования сантиллы в режиме Qt
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8947 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/LexRdo.cxx
@@ -0,0 +1,159 @@
+// Scintilla source code edit control
+/** @file LexRdo.cxx
+ ** Lexer for RDO.
+ ** Written by Andrey Urusov.
+ **/
+// Copyright 1998-2001 by Neil Hodgson <neilh@scintilla.org>
+// The License.txt file describes the conditions under which this software may be distributed.
+
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <assert.h>
+
+#include "LexRdo.h"
+
+#include "Platform.h"
+
+#include "WordList.h"
+#include "LexAccessor.h"
+#include "Accessor.h"
+#include "StyleContext.h"
+#include "Scintilla.h"
+#include "SciLexer.h"
+#include "CharacterSet.h"
+
+static void lexerRDOSyntaxColor( unsigned int startPos, int length, int initStyle, WordList *keywordlists[], Accessor &styler )
+{
+	WordList& keywords  = *keywordlists[0];
+	WordList& functions = *keywordlists[1];
+	WordList& traces    = *keywordlists[2];
+	WordList& colors    = *keywordlists[3];
+
+	styler.StartAt( startPos );
+
+	StyleContext sc( startPos, length, initStyle, styler );
+
+	bool flag = sc.More();
+	for ( ; flag; sc.Forward() ) {
+
+		if ( sc.state == SCE_RDO_OPERATOR ) {
+			sc.SetState( SCE_RDO_DEFAULT );
+		} else if ( sc.state == SCE_RDO_IDENTIFIER ) {
+			if ( !isRDOLexerIdentifier(sc.ch) ) {
+				char s[100];
+				sc.GetCurrent( s, sizeof(s) );
+				if ( keywords.InList(s) ) {
+					sc.ChangeState( SCE_RDO_KEYWORD );
+				} else if ( functions.InList(s) ) {
+					sc.ChangeState( SCE_RDO_FUNCTION );
+				} else if ( traces.InList(s) ) {
+					sc.ChangeState( SCE_RDO_TRACE );
+				} else if ( colors.InList(s) ) {
+					sc.ChangeState( SCE_RDO_FRAME_COLOR );
+				}
+				sc.SetState( SCE_RDO_DEFAULT );
+			}
+		} else if ( sc.state == SCE_RDO_NUMBER ) {
+			if ( !isdigit(sc.ch) && sc.ch != '.' && 
+			     !(sc.ch == 'e' || sc.ch == 'E') && 
+			     !((sc.ch == '+' || sc.ch == '-') && (sc.chPrev == 'e' || sc.chPrev == 'E'))) {
+				sc.SetState( SCE_RDO_DEFAULT );
+			}
+		} else if ( sc.state == SCE_RDO_STRING ) {
+			if ( sc.ch == '\'' ) {
+				sc.ForwardSetState( SCE_RDO_DEFAULT );
+			}
+		} else if ( sc.state == SCE_RDO_COMMENT_CPP ) {
+			if ( sc.Match('*', '/') ) {
+				sc.Forward();
+				sc.ForwardSetState( SCE_RDO_DEFAULT );
+			}
+		} else if ( sc.state == SCE_RDO_COMMENT_LINE ) {
+			if ( sc.ch == '\r' || sc.ch == '\n' ) {
+				sc.ForwardSetState( SCE_RDO_DEFAULT );
+			}
+		}
+
+		if ( sc.state == SCE_RDO_DEFAULT ) {
+			if ( sc.ch == '\'' ) {
+				sc.SetState( SCE_RDO_STRING );
+			} else if ( sc.Match('/', '*') ) {
+				sc.SetState( SCE_RDO_COMMENT_CPP );
+				sc.Forward();	// Eat the * so it isn't used for the end of the comment
+			} else if ( sc.Match('/', '/') ) {
+				sc.SetState( SCE_RDO_COMMENT_LINE );
+			} else if ( isdigit(sc.ch) || ((sc.ch == '-' || sc.ch == '+') && isdigit(sc.chNext)) ) {
+				sc.SetState( SCE_RDO_NUMBER );
+			} else if ( isRDOLexerOperator(sc.ch) ) {
+				sc.SetState( SCE_RDO_OPERATOR );
+			} else if ( isRDOLexerIdentifier(sc.ch) )
+				sc.SetState( SCE_RDO_IDENTIFIER );
+		}
+
+		flag = sc.More();
+	}
+	sc.Complete();
+}
+
+static void lexerRDOSyntaxFold( unsigned int startPos, int length, int initStyle, WordList *[], Accessor &styler )
+{
+	unsigned int endPos = startPos + length;
+	int visibleChars = 0;
+	int lineCurrent = styler.GetLine( startPos );
+	int levelPrev = styler.LevelAt( lineCurrent ) & SC_FOLDLEVELNUMBERMASK;
+	int levelCurrent = levelPrev;
+	char chNext = styler[startPos];
+	int styleNext = styler.StyleAt( startPos );
+	int style = initStyle;
+	tstring str = "";
+	for ( unsigned int i = startPos; i < endPos; i++ ) {
+		char ch = chNext;
+		chNext = styler.SafeGetCharAt(i + 1);
+		int stylePrev = style;
+		style = styleNext;
+		styleNext = styler.StyleAt(i + 1);
+		bool atEOL = ( ch == '\r' && chNext != '\n') || (ch == '\n' );
+		if ( style == SCE_RDO_COMMENT_CPP && styler.GetPropertyInt( "CommentFold" ) != 0 ) {
+			if ( style != stylePrev ) {
+				levelCurrent++;
+			} else if ( (style != styleNext) && !atEOL ) {
+				levelCurrent--;
+			}
+		}
+		if ( style == SCE_RDO_KEYWORD ) {
+			str += ch;
+		} else {
+			if ( str.length() ) {
+				if (
+					str == "$Pattern" || str == "$Resource_type" || str == "$Resources" || str == "$Operations" || str == "$Frame" || str == "$Constant" || str == "$Sequence"  || str == "$Function" || str == "$Decision_point" || str == "$Process" || str == "$Results" ||
+					str == "$pattern" || str == "$resource_type" || str == "$resources" || str == "$operations" || str == "$frame" || str == "$constant" || str == "$sequence"  || str == "$function" || str == "$decision_point" || str == "$Process" || str == "$results" )
+				{
+					levelCurrent++;
+				} else if ( str == "$End" ) {
+					levelCurrent--;
+				}
+				str = "";
+			}
+		}
+		if ( atEOL ) {
+			int lev = levelPrev;
+			if ( (levelCurrent > levelPrev) && (visibleChars > 0) )
+				lev |= SC_FOLDLEVELHEADERFLAG;
+			if ( lev != styler.LevelAt(lineCurrent) ) {
+				styler.SetLevel(lineCurrent, lev);
+			}
+			lineCurrent++;
+			levelPrev = levelCurrent;
+			visibleChars = 0;
+		}
+		if ( !isspacechar(ch) ) visibleChars++;
+	}
+	// Fill in the real level of the next line, keeping the current flags as they will be filled in later
+	int flagsNext = styler.LevelAt(lineCurrent) & ~SC_FOLDLEVELNUMBERMASK;
+	styler.SetLevel(lineCurrent, levelPrev | flagsNext);
+}
+
+LexerModule lexerRDOSyntax(SCLEX_RDO, lexerRDOSyntaxColor, "rdo", lexerRDOSyntaxFold);
---
+++ rdo/thirdparty/sci/branches/post_release/v_3_21/rdo/WordListUtil.cxx
@@ -0,0 +1,90 @@
+/*!
+  \copyright (c) RDO-Team, 2003-2012
+  \file      WordListUtil.cpp
+  \author    Романов Ярослав (robot.xet@gmail.com)
+  \date      29.09.2012
+  \brief     
+  \indent    4T
+*/
+
+// ----------------------------------------------------------------------- INCLUDES
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "utils/rdotypes.h"
+#include "thirdparty/sci/rdo/WordListUtil.h"
+// --------------------------------------------------------------------------------
+
+WordListUtil::WordListUtil(const WordList& wordlist)
+	: wl(wordlist)
+{}
+
+std::vector<tstring> WordListUtil::getNearestWords(const tstring& userPattern) const
+{
+	struct PriorityResultItem
+	{
+		tstring  value;
+		float    priority;
+
+		PriorityResultItem()
+			: priority(0.0)
+		{}
+		PriorityResultItem(const tstring& value, float priority)
+			: value   (value   )
+			, priority(priority)
+		{}
+
+		rbool operator< (const PriorityResultItem& item) const
+		{
+			return priority == item.priority
+				? value < item.value
+				: priority > item.priority;
+		}
+	};
+
+	typedef  std::vector<tstring>  result_type;
+	result_type result;
+
+	if (wl.words == 0)
+		return result;
+
+	if (userPattern.empty())
+	{
+		for (int i = 0; i < wl.len; ++i)
+		{
+			result.push_back(wl.words[i]);
+		}
+		return result;
+	}
+
+	std::vector<PriorityResultItem> priorityResult;
+	for (int i = 0; i < wl.len; ++i)
+	{
+		boost::iterator_range<char*> findPatternIt = boost::ifind_first(wl.words[i], userPattern);
+		if (!findPatternIt.empty())
+		{
+			boost::iterator_range<char*> fullKeywordIt(wl.words[i], wl.words[i] + strlen(wl.words[i]));
+			ruint position = findPatternIt.begin() - fullKeywordIt.begin();
+			ruint diff = position + (fullKeywordIt.end() - findPatternIt.end());
+			ruint wLen = fullKeywordIt.end() - fullKeywordIt.begin();
+			float positionPart = float(position) / float(wLen);
+			float diffPart = float(diff) / float(wLen);
+			float priority = 1 - (positionPart + diffPart) / 2;
+			priorityResult.push_back(PriorityResultItem(wl.words[i], priority));
+		}
+		else
+		{
+			priorityResult.push_back(PriorityResultItem(wl.words[i], 0.0));
+		}
+	}
+	std::sort(priorityResult.begin(), priorityResult.end());
+	float const minPriority = 0.3f;
+
+	BOOST_FOREACH(const PriorityResultItem& item, priorityResult)
+	{
+		if(item.priority >= minPriority)
+			result.push_back(item.value);
+	}
+	return result;
+}
