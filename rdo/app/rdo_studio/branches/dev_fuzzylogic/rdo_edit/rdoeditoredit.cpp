d01cea9ccda78a327c2c20967339c0153c720d9b
Ulya Yadgarova <y.v.yadgarova@gmail.com>
2012-10-20 22:25:40 +0000

    - восстановление rev. 6713
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8992 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/branches/dev_fuzzylogic/rdo_edit/rdoeditoredit.cpp
+++ rdo/app/rdo_studio/branches/dev_fuzzylogic/rdo_edit/rdoeditoredit.cpp
@@ -654,7 +654,7 @@ void RDOEditorEdit::OnInsertCommand( UINT nID )
 		case ID_INSERT_ALGO_FOR             : incPos = 5;  break;
 		case ID_INSERT_ALGO_IF              :
 		case ID_INSERT_ALGO_IF_ELSE         : incPos = 4;  break;
-		case ID_INSERT_PRC_TEMPL_PROCESS    : incPos = 9;  break;
+		case ID_INSERT_PRC_TEMPL_PROCESS    : incPos = 10;  break;
 	}
 
 	replaceCurrent( static_cast<LPCTSTR>(s), incPos );
