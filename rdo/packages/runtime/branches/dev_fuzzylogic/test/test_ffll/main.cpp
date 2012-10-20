45f005aa1aa78b4c91c246b174b3171eb7f27576
Ulya Yadgarova <y.v.yadgarova@gmail.com>
2012-10-20 20:09:01 +0000

    * revert changes from rev.8979
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@8988 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_fuzzylogic/test/test_ffll/main.cpp
+++ rdo/packages/runtime/branches/dev_fuzzylogic/test/test_ffll/main.cpp
@@ -1,38 +1,6 @@
 
-#include <iostream>
 
-
-
-#include <FFLLAPI.h>
-#include <FFLLBase.h>
-#include <FuzzyModelBase.h>
-#include <FuzzyVariableBase.h>
-#include <FuzzyOutVariable.h>
-
-int main( int argc, char* argv[] )
+int main(int argc, char **argv)
 {
-
-
-// создаем пустую модель
-//
-//int model_ent = ffll_new_model(); // создается ModelContainer (empty), model = null;
-//// may init by *ffll_load_fcl_file()*
-//model_ent.init(); // init the model
-
-FuzzyModelBase model;// = new FuzzyModelBase();
-model = FuzzyModelBase();
-model.init();
-
-FuzzyModelBase* pmodel = &model;
-
-std::wstring name(L"Steve Nash");
-const wchar_t* szName = name.c_str();
-
-
-FuzzyVariableBase* inputVar1 = new FuzzyVariableBase(pmodel);
-//FuzzyVariableBase* inputVar2 = new FuzzyVariableBase(model);
-int i = inputVar1->init(szName, 34.5, 45.3, true);//left x value, right x value? 	// if no name is passed in name it "Variable 0"
-//int k = inputVar2->init("Variable2", true);
-return 0;
-
+	return 0;
 }
