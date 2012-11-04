d2b4eaaca958720a599abded9acaba60433e93e9
Yaroslav Romanov <robot.xet@gmail.com>
2012-11-04 15:58:24 +0000

    - todo qt для рдо-сигналов
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@9163 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/branches/dev_qt_migration/rdo_edit/rdoeditoredit.cpp
+++ rdo/app/rdo_studio/branches/dev_qt_migration/rdo_edit/rdoeditoredit.cpp
@@ -141,6 +141,9 @@ RDOEditorEdit::RDOEditorEdit(PTR(QWidget) pParent, PTR(RDOStudioEditBaseView) pV
 	QObject::connect(this, SIGNAL(ScintillaEditBase::modified(int, int, int, int, const QByteArray&, int, int, int)), this, SLOT(catchModified(int, int, int, int, const QByteArray&, int, int, int)));
 	QObject::connect(this, SIGNAL(ScintillaEditBase::marginClicked()), this, SLOT(catchMarginClick()));
 	QObject::connect(this, SIGNAL(ScintillaEditBase::charAdded()), this, SLOT(catchCharAdded()));
+	//! @todo qt - обработать сигналы SCI_RDOPOSCHANGED, SCI_RDOCLICK
+	//QObject::connect(this, SIGNAL(), this, SLOT(catchRdoPosChanged()));
+	//QObject::connect(this, SIGNAL(), this, SLOT(catchRdoClick()));
 }
 
 RDOEditorEdit::~RDOEditorEdit()
