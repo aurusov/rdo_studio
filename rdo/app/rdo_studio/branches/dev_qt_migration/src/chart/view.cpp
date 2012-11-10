83d04be67f7edf3354577f0d25599b77abd7e043
Andrey Urusov <drobus@gmail.com>
2012-11-10 05:55:27 +0000

    - патч конфликта после мержа
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@9302 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/branches/dev_qt_migration/src/chart/view.cpp
+++ rdo/app/rdo_studio/branches/dev_qt_migration/src/chart/view.cpp
@@ -173,16 +173,18 @@ int RDOStudioChartView::OnCreate( LPCREATESTRUCT lpCreateStruct )
 			rbool maximized = studioApp.getIMainWnd()->isMDIMaximazed();
 			int delta = maximized ? 1 : 0;
 
-	appendMenu( mainMenu->GetSubMenu( 1 + delta ), 4, &popupMenu );
-	popupMenu.AppendMenu( MF_SEPARATOR );
-	appendMenu( mainMenu->GetSubMenu( 3 + delta ), 6, &popupMenu );
-	appendMenu( mainMenu->GetSubMenu( 3 + delta ), 7, &popupMenu );
-	appendMenu( mainMenu->GetSubMenu( 3 + delta ), 8, &popupMenu );
-	appendMenu( mainMenu->GetSubMenu( 3 + delta ), 9, &popupMenu );
-	popupMenu.AppendMenu( MF_SEPARATOR );
-	appendMenu( mainMenu->GetSubMenu( 6 + delta ), 4, &popupMenu );
-	popupMenu.AppendMenu( MF_SEPARATOR );
-	appendMenu( mainMenu->GetSubMenu( 6 + delta ), 6, &popupMenu );
+			appendMenu( mainMenu->GetSubMenu( 1 + delta ), 4, &popupMenu );
+			popupMenu.AppendMenu( MF_SEPARATOR );
+			appendMenu( mainMenu->GetSubMenu( 3 + delta ), 6, &popupMenu );
+			appendMenu( mainMenu->GetSubMenu( 3 + delta ), 7, &popupMenu );
+			appendMenu( mainMenu->GetSubMenu( 3 + delta ), 8, &popupMenu );
+			appendMenu( mainMenu->GetSubMenu( 3 + delta ), 9, &popupMenu );
+			popupMenu.AppendMenu( MF_SEPARATOR );
+			appendMenu( mainMenu->GetSubMenu( 6 + delta ), 4, &popupMenu );
+			popupMenu.AppendMenu( MF_SEPARATOR );
+			appendMenu( mainMenu->GetSubMenu( 6 + delta ), 6, &popupMenu );
+		}
+	}
 
 	attachToDoc();
 
