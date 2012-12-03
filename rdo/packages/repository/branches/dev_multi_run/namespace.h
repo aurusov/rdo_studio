1fe795ca3b2172dabad6d012c0f793eb993d6d1f
Dmitry Luschan <dluschan@gmail.com>
2012-12-03 19:03:37 +0000

    - branche to mulri_run
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@9620 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/repository/branches/dev_multi_run/namespace.h
@@ -0,0 +1,22 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      repository/namespace.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      08.04.2012
+  \brief     Пространство имен rdo::repository
+  \indent    4T
+*/
+
+#ifndef _REPOSITORY_NAMESPACE_H_
+#define _REPOSITORY_NAMESPACE_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+// --------------------------------------------------------------------------------
+
+//! \namespace  rdo::repository
+//! \brief      Пространство имён репозитария
+#define  OPEN_RDO_REPOSITORY_NAMESPACE  namespace rdo { namespace repository {
+#define CLOSE_RDO_REPOSITORY_NAMESPACE  }};
+
+#endif // _REPOSITORY_NAMESPACE_H_
