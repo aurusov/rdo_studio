32ac918a1d7ca17b0beceee4843695bf9640632b
Andrey Urusov <drobus@gmail.com>
2012-12-30 08:33:19 +0000

    * reverted changes from rev.9947
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@9948 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/trunk/test/calc/main.cpp
+++ rdo/packages/runtime/trunk/test/calc/main.cpp
@@ -143,8 +143,8 @@ BOOST_AUTO_TEST_CASE(RDOCalc_RecursSimulator)
 
 	int resultError = calc1.funError(param1);
 	int resultOk    = calc2.funOk   (param2);
-	BOOST_CHECK_MESSAGE(resultError == 1,   resultError);
-	BOOST_CHECK_MESSAGE(resultOk    == 120, resultOk   );
+	BOOST_CHECK(resultError == 1);
+	BOOST_CHECK(resultOk    == 120);
 }
 
 BOOST_AUTO_TEST_CASE(RDOCalc_Recurs)
