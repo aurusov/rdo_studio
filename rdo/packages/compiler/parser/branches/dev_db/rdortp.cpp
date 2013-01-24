36f3cf53382729870737e5692d98f1828b920970
Igor Klevanec <cerevra@gmail.com>
2013-01-24 19:22:07 +0000

    - рефакторинг: перенос кода из inl в cpp
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10383 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/compiler/parser/branches/dev_db/rdortp.cpp
+++ rdo/packages/compiler/parser/branches/dev_db/rdortp.cpp
@@ -3,6 +3,7 @@
   \file      rdortp.cpp
   \authors   Барс Александр
   \authors   Урусов Андрей (rdo@rk9.bmstu.ru)
+  \authors   Лущан Дмитрий (dluschan@rk9.bmstu.ru)
   \date      11.06.2006
   \brief     Типы ресурсов
   \indent    4T
@@ -36,6 +37,62 @@ void rtperror(PTR(char) message)
 // --------------------------------------------------------------------------------
 // -------------------- RDORTPResType
 // --------------------------------------------------------------------------------
+rsint RDORTPResType::getNumber() const
+{
+	return m_number;
+}
+
+rbool RDORTPResType::isPermanent() const
+{
+	return m_permanent;
+}
+
+rbool RDORTPResType::isTemporary() const
+{
+	return !m_permanent;
+}
+
+CREF(RDORTPResType::ParamList) RDORTPResType::getParams() const
+{
+	return m_params;
+}
+
+CREF(rdo::runtime::LPIResourceType) RDORTPResType::getRuntimeResType() const
+{
+	ASSERT(m_pRuntimeResType);
+	return m_pRuntimeResType;
+}
+
+void RDORTPResType::setType(TypeRDOResType type)
+{
+	//! \todo вывести ошибку вместо ASSERT()
+	ASSERT(!(m_type == procRes && type == procTran));
+	ASSERT(!(m_type == procTran && type == procRes));
+	m_type = type;
+}
+
+void RDORTPResType::end()
+{
+	switch (m_type)
+	{
+	case simple:
+		m_pRuntimeResType = rdo::Factory<rdo::runtime::RDOResourceType>::create(m_number).interface_cast<rdo::runtime::IResourceType>();
+		break;
+	case procRes:
+		m_pRuntimeResType = rdo::Factory<rdo::runtime::RDOResourceTypeProccess>::create(m_number).interface_cast<rdo::runtime::IResourceType>();
+		break;
+	case procTran:
+		m_pRuntimeResType = rdo::Factory<rdo::runtime::RDOResourceTypeTransact>::create(m_number).interface_cast<rdo::runtime::IResourceType>();
+		break;
+	default:
+		NEVER_REACH_HERE;
+	}
+	ASSERT(m_pRuntimeResType);
+	m_pType = m_pRuntimeResType;
+	ASSERT(m_pType);
+}
+
+
 RDORTPResType::RDORTPResType(CREF(LPRDOParser) pParser, CREF(RDOParserSrcInfo) src_info, rbool permanent, TypeRDOResType type)
 	: RDOParserSrcInfo(src_info            )
 	, m_number        (pParser->getRTP_id())
