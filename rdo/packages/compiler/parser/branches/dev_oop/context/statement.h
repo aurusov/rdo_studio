5bae000e7fb5b3417cec9cce445befe29cb1b042
Andrey Urusov <drobus@gmail.com>
2013-02-11 05:22:43 +0000

    * пересоздание бранча
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10639 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/compiler/parser/branches/dev_oop/context/statement.h
@@ -0,0 +1,74 @@
+/*!
+  \copyright (c) RDO-Team, 2012
+  \file      statement.h
+  \authors   Поподьянец Евгений (kurt.gigacore@gmail.com)
+  \date      18.04.2012
+  \brief     Контекст выражений
+  \indent    4T
+*/
+
+#ifndef _CONTEXT_STATEMENT_H_
+#define _CONTEXT_STATEMENT_H_
+
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/context/context.h"
+#include "simulator/compiler/parser/rdoparser.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+// --------------------------------------------------------------------------------
+// -------------------- ContextStatementBase
+// --------------------------------------------------------------------------------
+CLASS(ContextStatementBase):
+	             INSTANCE_OF (Context)
+{
+DECLARE_FACTORY(ContextStatementBase);
+protected:
+	ContextStatementBase();
+};
+DECLARE_POINTER(ContextStatementBase);
+
+// --------------------------------------------------------------------------------
+// -------------------- ContextBreakable
+// --------------------------------------------------------------------------------
+CLASS(ContextBreakable):
+	         INSTANCE_OF (ContextStatementBase)
+{
+DECLARE_FACTORY(ContextBreakable);
+protected:
+	ContextBreakable();
+};
+DECLARE_POINTER(ContextBreakable);
+
+// --------------------------------------------------------------------------------
+// -------------------- ContextReturnable
+// --------------------------------------------------------------------------------
+PREDECLARE_POINTER(ContextReturnable);
+CLASS(ContextReturnable):
+	         INSTANCE_OF (ContextStatementBase)
+{
+DECLARE_FACTORY(ContextReturnable);
+public:
+
+	typedef std::vector<LPContextReturnable> ContextReturnableList;
+	void addContext(REF(LPContextReturnable) pContext);
+
+	void setReturnFlag();
+	bool returnFlag();
+
+private:
+	bool checkChildFlags();
+
+protected:
+	ContextReturnable();
+
+	ContextReturnableList m_contextReturnableList;
+	bool                  m_returnFlag;
+
+};
+
+CLOSE_RDO_PARSER_NAMESPACE
+
+#endif // _CONTEXT_STATEMENT_H_
---
+++ rdo/packages/compiler/parser/branches/dev_oop/rdo_array.cpp
@@ -0,0 +1,90 @@
+/*!
+  \copyright (c) RDO-Team, 2011
+  \file      simulator/compiler/parser/rdo_array.cpp
+  \author    Чирков Михаил
+  \date      
+  \brief     
+  \indent    4T
+*/
+
+// ---------------------------------------------------------------------------- PCH
+#include "simulator/compiler/parser/pch.h"
+// ----------------------------------------------------------------------- INCLUDES
+// ----------------------------------------------------------------------- SYNOPSIS
+#include "simulator/compiler/parser/rdo_array.h"
+#include "simulator/compiler/parser/rdoparser.h"
+#include "simulator/compiler/parser/rdoparser_error.h"
+#include "simulator/compiler/parser/rdoparser_lexer.h"
+// --------------------------------------------------------------------------------
+
+OPEN_RDO_PARSER_NAMESPACE
+
+//----------------------------------------------------------------------------
+//---------- RDOArrayValue
+//----------------------------------------------------------------------------
+RDOArrayValue::RDOArrayValue(CREF(LPRDOArrayType) pArrayType)
+	: m_pArrayType(pArrayType)
+{}
+
+RDOArrayValue::~RDOArrayValue()
+{}
+
+void RDOArrayValue::insertItem(CREF(LPRDOValue) pValue)
+{
+	ASSERT(pValue);
+
+	m_pArrayType->getItemType()->type()->type_cast(pValue->typeInfo()->type(), pValue->src_info(), m_pArrayType->src_info(), pValue->src_info());
+	LPRDOValue pItemValue = m_pArrayType->getItemType()->type()->value_cast(pValue, m_pArrayType->src_info(), pValue->src_info());
+	ASSERT(pItemValue);
+	m_container.push_back(pItemValue);
+}
+
+CREF(LPRDOArrayType) RDOArrayValue::getArrayType() const
+{
+	return m_pArrayType;
+}
+
+REF(LPRDOArrayType) RDOArrayValue::getArrayType()
+{
+	return m_pArrayType;
+}
+
+rdo::runtime::RDOValue RDOArrayValue::getRArray() const
+{
+	return rdo::runtime::RDOValue(m_pArrayType->getRuntimeArrayType(), createRuntimeValue());
+}
+
+rdo::runtime::LPRDOArrayValue RDOArrayValue::createRuntimeValue() const
+{
+	rdo::runtime::LPRDOArrayValue pArrayValue = rdo::Factory<rdo::runtime::RDOArrayValue>::create(m_pArrayType->getRuntimeArrayType());
+	ASSERT(pArrayValue);
+	STL_FOR_ALL_CONST(m_container, it)
+	{
+		pArrayValue->push_back((*it)->value());
+	}
+	return pArrayValue;
+}
+
+tstring RDOArrayValue::getAsString() const
+{
+	tstring arrayValue;
+	STL_FOR_ALL_CONST(m_container, it)
+	{
+		if (it == m_container.begin())
+		{
+			arrayValue = rdo::format(_T("[%s"), (*it)->value().getAsString().c_str());
+		}
+		else
+		{
+			arrayValue = rdo::format(_T("%s, %s"), arrayValue.c_str(), (*it)->value().getAsString().c_str());
+		}
+	}
+	return rdo::format(_T("%s]"), arrayValue.c_str());
+}
+
+CREF(RDOArrayValue::Container) RDOArrayValue::getContainer() const
+{
+	return m_container;
+}
+
+CLOSE_RDO_PARSER_NAMESPACE
