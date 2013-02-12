c10613f80a0867dfae8726b31ae7ae622ae1614b
Andrey Urusov <drobus@gmail.com>
2013-02-12 18:18:35 +0000

    - перенос внутри файла
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10651 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/trunk/src/model/model.h
+++ rdo/app/rdo_studio/trunk/src/model/model.h
@@ -39,6 +39,50 @@ Q_OBJECT
 
 friend class RDOThreadStudioGUI;
 
+public:
+	RDOStudioModel();
+	virtual ~RDOStudioModel();
+
+	rbool openModel (CREF(tstring) modelName = _T(""));
+	rbool runModel  ();
+	rbool closeModel();
+	void  update    ();
+
+	void resetView();
+
+	CREF(QString) getName    () const;
+	void          setName    (CREF(QString) str);
+	QString       getFullName() const;
+
+	rbool  isRunning     () const;
+	rbool  isFrmDescribed() const;
+	double getTimeNow    () const;
+
+	rdo::simulation::report::RDOExitCode getExitCode   () const;
+	rdo::runtime::RunTimeMode            getRuntimeMode() const;
+	void    setRuntimeMode       (const rdo::runtime::RunTimeMode value);
+	tstring getLastBreakPointName();
+	double  getShowRate          () const;
+	void    setShowRate          (double value);
+
+	int        getFrameCount   () const;
+	CPTR(char) getFrameName    (int index) const;
+	void       showFrame       (int index);
+	void       closeAllFrame   ();
+	rbool      hasModel        () const;
+
+	 PTR(rdoEditor::RDOEditorTabCtrl) getTab();
+	CPTR(rdoEditor::RDOEditorTabCtrl) getTab() const;
+
+	void  updateStyleOfAllModel() const;
+	rbool isPrevModelClosed    () const;
+
+	REF(RDOStudioFrameManager) getFrameManager();
+	void onChangeFrame(ruint index);
+
+protected:
+	virtual void proc(REF(RDOThread::RDOMessageInfo) msg);
+
 private:
 	enum BuildState
 	{
@@ -119,50 +163,6 @@ private:
 	void createView();
 
 	DECLARE_IInit;
-
-protected:
-	virtual void proc(REF(RDOThread::RDOMessageInfo) msg);
-
-public:
-	RDOStudioModel();
-	virtual ~RDOStudioModel();
-
-	rbool openModel (CREF(tstring) modelName = _T(""));
-	rbool runModel  ();
-	rbool closeModel();
-	void  update    ();
-
-	void resetView();
-
-	CREF(QString) getName    () const;
-	void          setName    (CREF(QString) str);
-	QString       getFullName() const;
-
-	rbool  isRunning     () const;
-	rbool  isFrmDescribed() const;
-	double getTimeNow    () const;
-
-	rdo::simulation::report::RDOExitCode getExitCode   () const;
-	rdo::runtime::RunTimeMode            getRuntimeMode() const;
-	void    setRuntimeMode       (const rdo::runtime::RunTimeMode value);
-	tstring getLastBreakPointName();
-	double  getShowRate          () const;
-	void    setShowRate          (double value);
-
-	int        getFrameCount   () const;
-	CPTR(char) getFrameName    (int index) const;
-	void       showFrame       (int index);
-	void       closeAllFrame   ();
-	rbool      hasModel        () const;
-
-	 PTR(rdoEditor::RDOEditorTabCtrl) getTab();
-	CPTR(rdoEditor::RDOEditorTabCtrl) getTab() const;
-
-	void  updateStyleOfAllModel() const;
-	rbool isPrevModelClosed    () const;
-
-	REF(RDOStudioFrameManager) getFrameManager();
-	void onChangeFrame(ruint index);
 
 private slots:
 	void onFileNew    ();
