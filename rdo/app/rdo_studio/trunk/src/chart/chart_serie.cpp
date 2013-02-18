9132c657318d775e45a6734d825d44634c3fa2c2
Andrey Urusov <drobus@gmail.com>
2013-02-18 18:39:02 +0000

    - чистка кода
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10780 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/trunk/src/chart/chart_serie.cpp
+++ rdo/app/rdo_studio/trunk/src/chart/chart_serie.cpp
@@ -120,7 +120,7 @@ QSize ChartSerie::getLegendSize(const QFontMetrics& fm, const QRect& rect) const
 	{
 		size.setHeight(m_options.markerSize * 2);
 	}
-	size.setWidth(tmprect.right() - rect.left());
+	size.setWidth(tmprect.width());
 	size.setHeight(size.height() + 2);
 	return size;
 }
