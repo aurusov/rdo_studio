24845e952e5bcc3b10de7207d81340b467d08af8
Andrey Urusov <drobus@gmail.com>
2013-02-25 18:29:22 +0000

    - чистка кода
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10895 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/ui/qt/trunk/headers/int_validator/int_validator.h
+++ rdo/packages/ui/qt/trunk/headers/int_validator/int_validator.h
@@ -1,6 +1,6 @@
 /*!
-  \copyright (c) RDO-Team, 2012-2012
-  \file      ui/qt/headers/int_validator/int_validator.h
+  \copyright (c) RDO-Team, 2013
+  \file      int_validator.h
   \author    Романов Ярослав (rdo@rk9.bmstu.ru)
   \date      25.02.2013
   \brief     QIntValidator для настроек
@@ -11,25 +11,26 @@
 #define _UI_QT_INTVALIDATOR_H_
 
 // ----------------------------------------------------------------------- INCLUDES
-#include <QtGui/qpainter.h>
-#include <QtGui/qpixmap.h>
+#include <QtGui/qvalidator>
 // ----------------------------------------------------------------------- SYNOPSIS
-#include "ui/abstract/headers/memdc/memdc.h"
 // --------------------------------------------------------------------------------
 
-class IntValidator : public QIntValidator
+class IntValidator: public QIntValidator
 {
-	public:
-		IntValidator(int bottom, int top, PTR(QObject) pParent)
-			: QIntValidator(bottom, top, pParent)
-		{}
+public:
+	IntValidator(int bottom, int top, PTR(QObject) pParent)
+		: QIntValidator(bottom, top, pParent)
+	{}
 
-		QValidator::State validate(QString& input, int& pos) const
+	virtual QValidator::State validate(QString& input, int& pos) const
+	{
+		QIntValidator::State result(QIntValidator::validate(input, pos));
+		if (result == QValidator::Intermediate)
 		{
-			QIntValidator::State result(QIntValidator::validate(input, pos));
-			if (result == QValidator::Intermediate) result = QValidator::Invalid;
-			return result;
+			result = QValidator::Invalid;
 		}
+		return result;
+	}
 };
 
 #endif // _UI_QT_INTVALIDATOR_H_
