059bac562ac20508d38a76d525c8af6091995a22
Andrey Urusov <drobus@gmail.com>
2013-03-02 14:01:30 +0000

    - добавлено подавление ворнингов
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@10987 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/utils/trunk/warning_disable.h
@@ -0,0 +1,21 @@
+/*!
+  \copyright (c) RDO-Team, 2013
+  \file      warning_disable.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      02.03.2013
+  \brief     
+  \indent    4T
+*/
+
+#include "utils/platform.h"
+
+#ifdef COMPILER_VISUAL_STUDIO
+	#pragma warning(push)
+	#pragma warning(disable: 4100)
+	#pragma warning(disable: 4127)
+	#pragma warning(disable: 4231)
+	#pragma warning(disable: 4244)
+	#pragma warning(disable: 4251)
+	#pragma warning(disable: 4512)
+	#pragma warning(disable: 4800)
+#endif
---
+++ rdo/packages/utils/trunk/warning_enable.h
@@ -0,0 +1,14 @@
+/*!
+  \copyright (c) RDO-Team, 2013
+  \file      warning_enable.h
+  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \date      02.03.2013
+  \brief     
+  \indent    4T
+*/
+
+#include "utils/platform.h"
+
+#ifdef COMPILER_VISUAL_STUDIO
+	#pragma warning(pop)
+#endif
