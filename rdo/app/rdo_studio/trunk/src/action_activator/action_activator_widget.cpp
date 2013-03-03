26e4a1d7e36f1be6ddd98d2de24f15b968af15ba
Andrey Urusov <drobus@gmail.com>
2013-03-03 15:33:34 +0000

    - перевод на юникод
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11040 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/app/rdo_studio/trunk/src/action_activator/action_activator_widget.cpp
+++ rdo/app/rdo_studio/trunk/src/action_activator/action_activator_widget.cpp
@@ -1,7 +1,7 @@
 /*!
   \copyright (c) RDO-Team, 2013
   \file      action_activator_widget.cpp
-  \author    Урусов Андрей (rdo@rk9.bmstu.ru)
+  \author    РЈСЂСѓСЃРѕРІ РђРЅРґСЂРµР№ (rdo@rk9.bmstu.ru)
   \date      01.01.2013
   \brief     
   \indent    4T
