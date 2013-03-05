0f57b864952340f584ec3eb9ccefd03ea944b3bb
Igor Klevanec <cerevra@gmail.com>
2013-03-05 17:58:01 +0000

    - доработка теста
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11072 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/runtime/branches/dev_db/test/unit/value/main.cpp
+++ rdo/packages/runtime/branches/dev_db/test/unit/value/main.cpp
@@ -12,8 +12,6 @@
 
 // ---------------------------------------------------------------------------- PCH
 // ----------------------------------------------------------------------- INCLUDES
-#include <iostream>
-#include <boost/bind.hpp>
 #define BOOST_TEST_MODULE RDOValue_Test
 #include <boost/test/included/unit_test.hpp>
 // ----------------------------------------------------------------------- SYNOPSIS
@@ -23,25 +21,28 @@
 
 OPEN_RDO_RUNTIME_NAMESPACE
 
-RDOValue foo()
+#define CREATE_VALUE\
+	LPRDOEnumType pRDOEnumType = rdo::Factory<RDOEnumType>::create();\
+	pRDOEnumType->add(_T("first_record"));\
+	pRDOEnumType->add(_T("second_record"));\
+	rdo::runtime::RDOValue tempObject = rdo::runtime::RDOValue(pRDOEnumType,_T("second_record"));
+
+rdo::runtime::RDOValue foo()
 {
-	LPRDOEnumType pRDOEnumType = rdo::Factory<RDOEnumType>::create();
-
-	pRDOEnumType->add(_T("first_record"));
-	pRDOEnumType->add(_T("second_record"));
-
-	RDOValue tempObject = RDOValue(pRDOEnumType,_T("second_record"));
+	CREATE_VALUE;
 	return tempObject;
 }
 
 BOOST_AUTO_TEST_SUITE(RDOValue_Test)
 
-BOOST_AUTO_TEST_CASE(RDOValue_String)
+BOOST_AUTO_TEST_CASE(RDOValue)
 {
-	RDOValue a = foo();
-	int i = 1;
-}
+	CREATE_VALUE;
+	rdo::runtime::RDOValue a = foo();
 
+	BOOST_CHECK(tempObject.getAsString() == a.getAsString());
+	BOOST_CHECK(tempObject.getEnum()->getValues() == a.getEnum()->getValues());
+}
 
 BOOST_AUTO_TEST_SUITE_END() // RDOValue_Test
 
