57f4cfcd4adb420e67ad285b6fab8be3d8da738e
Evgeny Proydakov <lord.tiran@gmail.com>
2013-03-05 16:22:05 +0000

    - added compare.trace in python
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11066 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/scripts/trunk/python/compare.py
+++ rdo/packages/scripts/trunk/python/compare.py
@@ -7,7 +7,7 @@
 import sys
 import utils
 
-def read_trace_data(name):
+def read_result_data(name):
     file_data_temp = open(name, u'r').readlines()
     utils.cut_slash(file_data_temp)
     
@@ -22,6 +22,23 @@ def read_trace_data(name):
 
     return file_data
 
+def read_trace_data(name):
+    file_data_temp = open(name, u'r').readlines()
+    utils.cut_slash(file_data_temp)
+
+    file_data = []
+
+    push = False
+    for line in file_data_temp:
+        if(line.find('DPS_MM') != -1):
+            break;
+        if(push):
+            file_data.append(line)
+        if(line.find('$Changes') != -1):
+            push = True
+
+    return file_data
+    
 def full(file1, file2):
     file1_data = open(file1, 'r').readlines()
     file2_data = open(file2, 'r').readlines()
@@ -35,8 +52,8 @@ def full(file1, file2):
     return False
 
 def result(file1, file2):
-    file1_data = read_trace_data(file1)
-    file2_data = read_trace_data(file2)
+    file1_data = read_result_data(file1)
+    file2_data = read_result_data(file2)
     
     if cmp(file1_data, file2_data) == 0:
         return True
@@ -44,12 +61,11 @@ def result(file1, file2):
     return False
 
 def trace(file1, file2):
-    file1_data = open(file1, 'r').readlines()
-    file2_data = open(file2, 'r').readlines()
+    file1_data = read_trace_data(file1)
+    file2_data = read_trace_data(file2)
 
-    utils.cut_slash(file1_data)
-    utils.cut_slash(file2_data)
-
+    print "size: ", len(file1_data), " : ", len(file2_data)
+    
     if cmp(file1_data, file2_data) == 0:
         return True
 
