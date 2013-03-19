efd31d8dc6b3f1ace15293d08f7bbbb85633b7a1
Andrey Urusov <drobus@gmail.com>
2013-03-19 19:36:50 +0000

    - добавлена генерация версии билда
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11361 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/scripts/trunk/svn/generate_version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+file_name="../../app/rdo_studio/res/build_version.h"
+
+echo "#ifndef _RDO_STUDIO_RES_BUILD_VERSION_H_" > $file_name
+echo "#define _RDO_STUDIO_RES_BUILD_VERSION_H_" >> $file_name
+echo "" >> $file_name
+echo -n "static const QString g_buildVersion(\"svn-" >> $file_name
+svnversion -n ./../ >> $file_name
+echo "\");" >> $file_name
+echo "" >> $file_name
+echo "#endif // _RDO_STUDIO_RES_BUILD_VERSION_H_" >> $file_name
