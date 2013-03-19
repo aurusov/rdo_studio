0afd0ef0751d6138d6610fb29854e592675e87f9
Andrey Urusov <drobus@gmail.com>
2013-03-19 19:39:17 +0000

    - номер ревизии от корня сборки
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11362 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/scripts/trunk/svn/generate_version.sh
+++ rdo/packages/scripts/trunk/svn/generate_version.sh
@@ -6,7 +6,7 @@ echo "#ifndef _RDO_STUDIO_RES_BUILD_VERSION_H_" > $file_name
 echo "#define _RDO_STUDIO_RES_BUILD_VERSION_H_" >> $file_name
 echo "" >> $file_name
 echo -n "static const QString g_buildVersion(\"svn-" >> $file_name
-svnversion -n ./../ >> $file_name
+svnversion -n ./../../ >> $file_name
 echo "\");" >> $file_name
 echo "" >> $file_name
 echo "#endif // _RDO_STUDIO_RES_BUILD_VERSION_H_" >> $file_name
