71a74ff4a3d49ecc0c4eb3d5d28238dbf4ebd902
Andrey Urusov <drobus@gmail.com>
2013-04-21 08:29:27 +0000

    - новый футер
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11755 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/help/shared/trunk/scripts/footer_raostudio.js
+++ rdo/packages/help/shared/trunk/scripts/footer_raostudio.js
@@ -1,2 +1,2 @@
-str = '<BR><TABLE class="navigation" cellpadding="0"><TR class="navigation"><TD class="navigation" align=left width="26">&nbsp;</TD></TR></TABLE><DIV class="footer">Справка по RAO-studio<BR>МГТУ им. Н.Э. Баумана, 2004-2012 гг.</DIV>';
+str = '<BR><TABLE class="navigation" cellpadding="0"><TR class="navigation"><TD class="navigation" align=left width="26">&nbsp;</TD></TR></TABLE><DIV class="footer">Справка по RAO-studio<BR>МГТУ им. Н.Э. Баумана, 2003-2013 гг.</DIV>';
 document.write(str);
