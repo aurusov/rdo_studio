409b7957e6f35550dff31336dfc9164dc553d20c
Evgeny Proydakov <lord.tiran@gmail.com>
2013-04-23 15:29:58 +0000

    - updated python executor_of_system_tests.py : fixed print on jenkins error
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@11809 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/scripts/trunk/python/executor_of_system_tests.py
+++ rdo/packages/scripts/trunk/python/executor_of_system_tests.py
@@ -247,7 +247,7 @@ for task in files:
             # run rdo_console app on test model
 
             command = (rdo_ex + u' -i ' + utils.wrap_the_string_in_quotes(model_file))
-            print 'Run:', utils.safe_encode(command), '\n'
+            print 'Run:', utils.safe_encode(command, 'UTF-8'), '\n'
             simulation_code = subprocess.call(utils.safe_encode(command, CONSOLE_PARAM_ENCODING), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
             print u'SIMYLATION EXIT CODE :', simulation_code
@@ -309,7 +309,7 @@ for task in files:
                 
                 model_file = temp_directory_name + model['name']
                 command = (rdo_ex + u' -i ' + utils.wrap_the_string_in_quotes(model_file) + u' -c')
-                print 'Run:', utils.safe_encode(command), '\n'
+                print 'Run:', utils.safe_encode(command, 'UTF-8'), '\n'
                 convertor_exit_code = subprocess.call(utils.safe_encode(command, 'UTF-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 
                 print u'CONVERT EXIT CODE :', convertor_exit_code, u'\n'
