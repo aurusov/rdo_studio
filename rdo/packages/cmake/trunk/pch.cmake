527e2fdf0113f0fb2e78df8d2dc997a2309a7406
Andrey Urusov <drobus@gmail.com>
2013-09-17 06:42:35 +0000

    - space to tab
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12232 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/cmake/trunk/pch.cmake
+++ rdo/packages/cmake/trunk/pch.cmake
@@ -3,19 +3,19 @@
 #==============================================================================
 
 MACRO(ADD_PCH SOURCE_FILES NO_PCH_FILES PCH_FILE PCH_PATH)
-    FOREACH(source_file ${SOURCE_FILES})
-        GET_FILENAME_COMPONENT(CPP ${source_file} EXT)
-        IF (${CPP} STREQUAL ".cpp")
-            LIST(FIND NO_PCH_FILES ${source_file} no_pch_index)
-            IF (${no_pch_index} EQUAL -1)
-                SET_SOURCE_FILES_PROPERTIES(${source_file}
-                    PROPERTIES COMPILE_FLAGS "/Yu${PCH_PATH}/${PCH_FILE}.h"
-                )
-            ENDIF (${no_pch_index} EQUAL -1)
-        ENDIF (${CPP} STREQUAL ".cpp")
-    ENDFOREACH(source_file)
+	FOREACH(source_file ${SOURCE_FILES})
+		GET_FILENAME_COMPONENT(CPP ${source_file} EXT)
+		IF (${CPP} STREQUAL ".cpp")
+			LIST(FIND NO_PCH_FILES ${source_file} no_pch_index)
+			IF (${no_pch_index} EQUAL -1)
+				SET_SOURCE_FILES_PROPERTIES(${source_file}
+					PROPERTIES COMPILE_FLAGS "/Yu${PCH_PATH}/${PCH_FILE}.h"
+				)
+			ENDIF (${no_pch_index} EQUAL -1)
+		ENDIF (${CPP} STREQUAL ".cpp")
+	ENDFOREACH(source_file)
 
-    SET_SOURCE_FILES_PROPERTIES(${PCH_FILE}.cpp
-        PROPERTIES COMPILE_FLAGS "/Yc${PCH_PATH}/${PCH_FILE}.h"
-    )
+	SET_SOURCE_FILES_PROPERTIES(${PCH_FILE}.cpp
+		PROPERTIES COMPILE_FLAGS "/Yc${PCH_PATH}/${PCH_FILE}.h"
+	)
 ENDMACRO(ADD_PCH)
