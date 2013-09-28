34a6fc7ba0eb63ffd2a478c825794bc8e9938b07
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2013-09-28 09:10:40 +0000

    - базовая мультиплатформенность в скрипте запуска бизона
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12356 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/scripts/trunk/python/run-bison.py
+++ rdo/packages/scripts/trunk/python/run-bison.py
@@ -1,5 +1,29 @@
 import subprocess,os,argparse,re,sys
 
+def run_bison(yxPath, yPath, cppPath, iswin):
+    if iswin:
+        procname = "set CYGWIN=nodosfilewarning& set BISON_PKGDATADIR=" + os.environ["BISON_PKGDATADIR"] + "&" + os.environ["BISON_PATH"] + " -g -pdpt \"" + yPath + "\" -o\"" + cppPath + "\""
+    else:
+        procname = "set BISON_PKGDATADIR=" + os.environ["BISON_PKGDATADIR"] + ";" + os.environ["BISON_PATH"] + " -g -pdpt \"" + yPath + "\" -o\"" + cppPath + "\""
+
+    output, errr = subprocess.Popen(procname,stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True).communicate()
+
+    errr = errr.decode(codepage)
+
+    errr = errr.replace(yPath,yxPath)
+    
+    if iswin:
+        yPath = yPath.replace("\\","\\\\")   # ну это какой-то грязный хак, конечно
+        yxPath = yxPath.replace("\\","\\\\")
+        errr = errr.replace(yPath,yxPath)
+
+    strGRAM = open(cppPath, encoding = codepage).read()
+
+    fin = open(cppPath, 'w', encoding = codepage)
+    fin.write( strGRAM.replace(yPath,yxPath) )
+    fin.close()
+
+    return re.sub(r"^([^:]*:)((\\\\?[^:\\]*)*):(([0-9]*).([0-9]*)-[0-9.]*)(.*)$", r"\1\2(\5,\6)\7", errr, flags=re.MULTILINE)
 
 def main():
     parser = argparse.ArgumentParser(usage = argparse.SUPPRESS, description = "run bison twice for multipass compiler grammar files")
@@ -15,74 +39,34 @@ def main():
     parser.add_argument('-o2', type = str, default = '', help =\
                         "2nd output cpp file", required = True)
 
+    parser.add_argument('-windows', action = "store_true", help =\
+                        "win or linux")
+
     args = parser.parse_args()
 
-    print("Executing bison...")
-    print("STEP1")
+    print(toolname + ": " + "Executing bison...")
     cppPath = os.path.abspath(args.o1)
-    yPath = os.path.abspath(args.y1)
-    yxPath = os.path.abspath(args.inputFile)
-        
-    procname = "\
-set CYGWIN=nodosfilewarning\
-&set BISON_PKGDATADIR=" + os.environ["BISON_PKGDATADIR"] + "\
-&" + os.environ["BISON_PATH"] + " -g -pdpt \"" + yPath + "\" -o\"" + cppPath + "\""
-
-    output, errr = subprocess.Popen(procname,stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True).communicate()
-
-    errr = errr.decode('utf-8')
-
-    errr = errr.replace(yPath,yxPath)
+    yPath   = os.path.abspath(args.y1)
+    yxPath  = os.path.abspath(args.inputFile)
+    print(toolname + ": " + "parsing " + yPath)
     
-    yPath = yPath.replace("\\","\\\\")   # ну это какой-то грязный хак, конечно
-    yxPath = yxPath.replace("\\","\\\\")
-    
-    errr = errr.replace(yPath,yxPath)
-    
-    out1 = re.sub(r"^([^:]*:)((\\\\?[^:\\]*)*):(([0-9]*).([0-9]*)-[0-9.]*)(.*)$", r"\1\2(\5,\6)\7", errr, flags=re.MULTILINE) # треш, да, а что поделать
+    out1 = run_bison(yxPath, yPath, cppPath, args.windows)
 
-    strGRAM = open(cppPath, encoding = 'utf-8').read()
-
-    fin = open(cppPath, 'w', encoding = 'utf-8')
-    fin.write( strGRAM.replace(yPath,yxPath) )
-    fin.close()
-
-    print("STEP2")
     cppPath = os.path.abspath(args.o2)
-    yPath = os.path.abspath(args.y2)
-    yxPath = os.path.abspath(args.inputFile)
+    yPath   = os.path.abspath(args.y2)
+    yxPath  = os.path.abspath(args.inputFile)
+    print(toolname + ": " + "parsing " + yPath)
 
-    procname = "\
-set CYGWIN=nodosfilewarning\
-&set BISON_PKGDATADIR=" + os.environ["BISON_PKGDATADIR"] + "\
-&" + os.environ["BISON_PATH"] + " -g -pdpt \"" + yPath + "\" -o\"" + cppPath + "\""
-    
-    output, errr = subprocess.Popen(procname,stdout = subprocess.PIPE, stderr= subprocess.PIPE, shell = True).communicate()
+    out2 = run_bison(yxPath, yPath, cppPath, args.windows)
 
-    errr = errr.decode('utf-8')
-
-    errr = errr.replace(yPath,yxPath)
-    
-    yPath = yPath.replace("\\","\\\\")   # ну это какой-то грязный хак, конечно
-    yxPath = yxPath.replace("\\","\\\\")
-    
-    errr = errr.replace(yPath,yxPath)
-    
-    out2 = re.sub(r"^([^:]*:)((\\\\?[^:\\]*)*):(([0-9]*).([0-9]*)-[0-9.]*)(.*)$", r"\1\2(\5,\6)\7", errr, flags=re.MULTILINE) # треш, да, а что поделать
-
-    strGRAM = open(cppPath, encoding = 'utf-8').read()
-
-    fin = open(cppPath, 'w', encoding = 'utf-8')
-    fin.write( strGRAM.replace(yPath,yxPath) )
-    fin.close()
-
-    if out1==out2:
+    if out1 == out2:
+        print(toolname + ": " + "bison output:")
         print(out1)
         sys.exit(0)
     else:
-        sys.exit(yxPath+": error : bison console output doesn't match for grammar files")
+        sys.exit(yxPath + ": error : bison console output doesn't match for grammar files")
 
-toolname = "split-bison"
+toolname = "run-bison"
 codepage = "utf-8"
 
 if __name__ == '__main__':
