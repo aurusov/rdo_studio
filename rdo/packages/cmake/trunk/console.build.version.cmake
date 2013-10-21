89d87c71420b5d449a818a1cb72e95d1d7ec2021
Andrey Urusov <drobus@gmail.com>
2013-10-21 18:14:00 +0000

    - ошибка компиляции хедеров
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12440 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/cmake/trunk/console.build.version.cmake
+++ rdo/packages/cmake/trunk/console.build.version.cmake
@@ -1,10 +1,11 @@
 // ----------------------------------------------------------------------- INCLUDES
+#include <string>
 // ----------------------------------------------------------------------- SYNOPSIS
 // --------------------------------------------------------------------------------
 
 #ifndef _RDO_CONSOLE_BUILD_VERSION_H_
 #define _RDO_CONSOLE_BUILD_VERSION_H_
 
-static const tstring PROGRAM_VERSION = "${CMAKE_SVN_VERSION}";
+static const std::string PROGRAM_VERSION = "${CMAKE_SVN_VERSION}";
 
 #endif // _RDO_CONSOLE_BUILD_VERSION_H_
