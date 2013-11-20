a534b46fc6978df3566032377b7112e4455b408f
Dmitry Luschan <dluschan@gmail.com>
2013-11-20 21:15:28 +0000

    * merged revision(s) 12134-12574 from rdo/assemblies/rdo_studio/trunk
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12635 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/assemblies/rdo_studio/branches/dev_smr_proc_lang/simulator/compiler/compiler.cmake
@@ -0,0 +1,6 @@
+#==============================================================================
+# Copyright (c) 2013 Andrey Urusov <drobus@gmail.com>
+#==============================================================================
+
+INCLUDE(${PROJECT_SOURCE_DIR}/simulator/simulator.cmake)
+SET(PROJECT_FOLDER ${PROJECT_FOLDER}/.compiler)
