d1c1b71c09ea7359745331cd2c8ad2d3f3c822d8
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2013-11-25 13:17:08 +0000

    - добавлен скрипт перебора кодировок
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12654 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

---
+++ rdo/packages/scripts/trunk/python/encoding_tester.py
@@ -0,0 +1,29 @@
+import sys
+
+enclist = sys.argv[1:]
+
+testPhrase = \
+"""\
+========================
+  это успех, например
+  qqq 123 !@#$%^&*()_+
+========================
+"""
+
+failPhrase = \
+"""\
+========================
+        LOL NOPE!
+========================
+"""
+
+for left in enclist:
+    for right in enclist:
+        if left != right:
+            print("========================")
+            print(" TRYING ", left, ">", right)
+            try:
+                print(testPhrase.encode(left).decode(right))
+            except Exception as ex: 
+                print(failPhrase)
+
