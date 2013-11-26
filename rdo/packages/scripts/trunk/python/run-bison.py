d1aad11d951adb95b38e69a716a1eb02071c6194
Kirill Alexandrovsky <k.alexandrovsky@gmail.com>
2013-11-26 17:26:20 +0000

    - улучшен скрипт запуска бизона - более корректное форматирование вывода, возможность создавать bison defines
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12659 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/scripts/trunk/python/run-bison.py
+++ rdo/packages/scripts/trunk/python/run-bison.py
@@ -1,12 +1,12 @@
-import subprocess, os, argparse, re, sys
+import subprocess, os, argparse, re, sys, difflib
 
-def run_bison(yxPath, yPath, cppPath, name, bison):
-    procname = os.path.abspath(bison) + " -p" + name + " \"" + yPath + "\" -o\"" + cppPath + "\""
+def run_bison(yxPath, yPath, cppPath, name, bison, defines):
+    procname = os.path.abspath(bison)+ (" --defines=\"" + defines + "\" ")*(len(defines) and True) + " -p" + name + " \"" + yPath + "\" -o\"" + cppPath + "\""
     
     output, errr = subprocess.Popen(procname,stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True).communicate()
 
-    errr = errr.decode(codepage)
-
+    errr = errr.decode(sys.stdout.encoding)
+    
     errr = errr.replace(yPath,yxPath)
     
     if sys.platform == 'win32':
@@ -22,9 +22,9 @@ def run_bison(yxPath, yPath, cppPath, name, bison):
             fin.close()
 
     if sys.platform == 'win32': 
-        return re.sub(r"^(.*)(\.yx)(:)([0-9]*)\.([0-9]*)(-[0-9]*)?(:.*)$", r"\1\2(\4)\7",    errr, flags=re.MULTILINE)
+        return re.sub(r"^(.*)(\.yx)(:)([0-9]*)\.([0-9]*)(-[0-9]*(\.[0-9]*)?)?(:.*)$", r"\1\2(\4)\8",    errr, flags=re.MULTILINE)
     else:
-        return re.sub(r"^(.*)(\.yx)(:)([0-9]*)\.([0-9]*)(-[0-9]*)?(:.*)$", r"\1\2\3\4:\5\7", errr, flags=re.MULTILINE)
+        return re.sub(r"^(.*)(\.yx)(:)([0-9]*)\.([0-9]*)(-[0-9]*(\.[0-9]*)?)?(:.*)$", r"\1\2\3\4:\5\8", errr, flags=re.MULTILINE)
 
 def main():
     parser = argparse.ArgumentParser(usage = argparse.SUPPRESS, description = "run bison twice for multipass compiler grammar files")
@@ -38,30 +38,33 @@ def main():
     parser.add_argument('-o1', type = str, default = '', help = "1st output cpp file", required = True)
     parser.add_argument('-o2', type = str, default = '', help = "2nd output cpp file", required = True)
     parser.add_argument('-bison', type = str, default = '', help = "bison path", required = True)
+    parser.add_argument('-defines', type = str, default = '', help = "bison defines")
 
     args = parser.parse_args()
 
-    print(toolname + ": " + "Executing bison...")
+    defines = os.path.abspath(args.defines)
+    
+    print(toolname + ": " + "Executing bison...", file=sys.stderr)
     cppPath = os.path.abspath(args.o1)
     if os.path.exists(cppPath):
         os.remove(cppPath)
     yPath   = os.path.abspath(args.y1)
     yxPath  = os.path.abspath(args.inputFile)
-    print(toolname + ": " + "parsing " + yPath)
-    
-    out1 = run_bison(yxPath, yPath, cppPath, args.n1, args.bison)
+    print(toolname + ": " + "parsing " + yPath, file=sys.stderr)
+
+    out1 = run_bison(yxPath, yPath, cppPath, args.n1, args.bison, defines)
 
     cppPath = os.path.abspath(args.o2)
     if os.path.exists(cppPath):
         os.remove(cppPath)
     yPath   = os.path.abspath(args.y2)
     yxPath  = os.path.abspath(args.inputFile)
-    print(toolname + ": " + "parsing " + yPath)
-
-    out2 = run_bison(yxPath, yPath, cppPath, args.n2, args.bison)
+    print(toolname + ": " + "parsing " + yPath, file=sys.stderr)
+    
+    out2 = run_bison(yxPath, yPath, cppPath, args.n2, args.bison, defines)
 
     if out1 == out2:
-        print(toolname + ": " + "bison output:")
+        print(toolname + ": " + "bison output:", file=sys.stderr)
         con = out1.find(": conflicts:")
         if con > -1:
 
@@ -85,6 +88,15 @@ def main():
         print(out1, file=sys.stderr)
         sys.exit(0)
     else:
+        text1_lines = out1.split('\n')
+        text2_lines = out2.split('\n')
+
+        diff1 = difflib.unified_diff(text1_lines, text2_lines, lineterm='')
+
+        print("Difference in bison output:")
+        for d in list(diff1):
+            print(d)
+
         sys.exit(yxPath + ": error : bison console output doesn't match for grammar files")
 
 toolname = "run-bison"
