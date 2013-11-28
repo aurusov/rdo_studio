db2195404da14ea832b60a8e8706572a2eb7f7d9
Andrey Urusov <drobus@gmail.com>
2013-11-28 20:52:33 +0000

    * merged revision(s)  from rdo/packages/compiler/parser/branches/dev_parser  - слияние rdortp.y с rdoevn.yx, удаление rdortp.y  - удалено неиспользуемое
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root@12678 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- rdo/packages/compiler/parser/trunk/rdoparser_base.h
+++ rdo/packages/compiler/parser/trunk/rdoparser_base.h
@@ -49,93 +49,23 @@ public:
 	t_flex_lexer_fun  m_lexer_fun;
 
 	virtual void parse(CREF(LPRDOParser) pParser) = 0;
-	virtual void parse(CREF(LPRDOParser) pParser, REF(std::istream) in_stream)
-	{
-		UNUSED(pParser  );
-		UNUSED(in_stream);
-	};
+	virtual void parse(CREF(LPRDOParser) pParser, REF(std::istream) in_stream);
 
-	virtual ruint lexer_loc_line() { return ruint(rdo::runtime::RDOSrcInfo::Position::UNDEFINE_LINE); };
-	virtual ruint lexer_loc_pos () { return 0; };
+	virtual ruint lexer_loc_line();
+	virtual ruint lexer_loc_pos ();
 
 protected:
-	RDOParserItem()
-		: m_type      (rdoModelObjects::PAT)
-		, m_parser_fun(NULL                )
-		, m_error_fun (NULL                )
-		, m_lexer_fun (NULL                )
-		, m_from      (sf_repository       )
-	{}
-	RDOParserItem(rdoModelObjects::RDOFileType type, t_bison_parse_fun parser_fun, t_bison_error_fun error_fun, t_flex_lexer_fun lexer_fun, StreamFrom from = sf_repository)
-		: m_type      (type      )
-		, m_parser_fun(parser_fun)
-		, m_error_fun (error_fun )
-		, m_lexer_fun (lexer_fun )
-		, m_from      (from      )
-	{}
-	virtual ~RDOParserItem()
-	{}
+	RDOParserItem();
+	RDOParserItem(
+		rdoModelObjects::RDOFileType type,
+		t_bison_parse_fun            parser_fun,
+		t_bison_error_fun            error_fun,
+		t_flex_lexer_fun             lexer_fun,
+		StreamFrom                   from = sf_repository
+	);
+	virtual ~RDOParserItem();
 
 	StreamFrom m_from;
-};
-
-// --------------------------------------------------------------------------------
-// -------------------- RDOParserContainer
-// --------------------------------------------------------------------------------
-OBJECT(RDOParserContainer)
-{
-DECLARE_FACTORY(RDOParserContainer);
-public:
-	typedef std::map<ruint, LPRDOParserItem> List;
-	typedef List::iterator                   Iterator;
-
-	static const ruint UNDEFINED_ID = ruint(~0);
-
-	Iterator begin()            { return m_list.begin();     }
-	Iterator end  ()            { return m_list.end();       }
-	Iterator find (ruint index) { return m_list.find(index); }
-	void     clear();
-
-	static void getMinMax(rdoModelObjects::RDOParseType type, REF(ruint) min, REF(ruint) max);
-
-protected:
-	RDOParserContainer();
-	virtual ~RDOParserContainer();
-
-	ruint insert(rdoModelObjects::RDOParseType type, CREF(LPRDOParserItem) pParser);
-
-private:
-	List m_list;
-};
-
-// --------------------------------------------------------------------------------
-// -------------------- RDOParserContainerModel
-// --------------------------------------------------------------------------------
-class RDOParserContainerModel: public RDOParserContainer
-{
-DECLARE_FACTORY(RDOParserContainerModel);
-private:
-	RDOParserContainerModel();
-};
-
-// --------------------------------------------------------------------------------
-// -------------------- RDOParserContainerSMRInfo
-// --------------------------------------------------------------------------------
-class RDOParserContainerSMRInfo: public RDOParserContainer
-{
-DECLARE_FACTORY(RDOParserContainerSMRInfo);
-private:
-	RDOParserContainerSMRInfo();
-};
-
-// --------------------------------------------------------------------------------
-// -------------------- RDOParserContainerCorba
-// --------------------------------------------------------------------------------
-class RDOParserContainerCorba: public RDOParserContainer
-{
-DECLARE_FACTORY(RDOParserContainerCorba);
-private:
-	RDOParserContainerCorba();
 };
 
 CLOSE_RDO_PARSER_NAMESPACE
