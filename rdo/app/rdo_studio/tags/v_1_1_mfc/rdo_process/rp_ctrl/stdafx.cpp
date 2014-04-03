b89d7e223f6c14b34368554371838ac61ffb17c1
Andrey Urusov <drobus@gmail.com>
2014-04-03 20:06:13 +0400

    Remove rdo_studio/tags/v_1_1_mfc

--- rdo/app/rdo_studio/tags/v_1_1_mfc/rdo_process/rp_ctrl/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	rdo_process.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "app/rdo_studio_mfc/rdo_process/rp_ctrl/stdafx.h"
